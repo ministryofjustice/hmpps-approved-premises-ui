ec8aace959dc1078af37d3778ffcacbb7689e94d
Bryan Gale <bryan.gale@dxw.com>
2022-09-29 15:27:56 +0100

    Add stub Temporary Accommodation router

---
+++ server/routes/temporary-accommodation/index.ts
@@ -0,0 +1,20 @@
+/* istanbul ignore file */
+
+import { Router } from 'express'
+
+import type { Controllers } from '../../controllers'
+import actions from '../utils'
+
+import manageRoutes from './manage'
+
+export default function routes(controllers: Controllers): Router {
+  const router = Router()
+
+  const { applicationController } = controllers
+
+  const { get } = actions(router)
+
+  get('/', applicationController.index())
+
+  return router
+}
