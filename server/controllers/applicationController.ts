7fb57ec3a681dd189e64752f5da1d183be3d3c99
Bryan Gale <bryan.gale@dxw.com>
2022-09-29 15:38:58 +0100

    Redirect to differing start page depending on service

--- server/controllers/applicationController.ts
+++ server/controllers/applicationController.ts
@@ -1,9 +1,18 @@
 import type { Response, Request, RequestHandler } from 'express'
+import apManagePaths from '../paths/approved-premises/manage'
+import taManagePaths from '../paths/temporary-accommodation/manage'
+import { getService } from '../utils/applicationUtils'
 
 export default class ApplicationController {
   index(): RequestHandler {
-    return (_req: Request, res: Response) => {
-      res.redirect('/premises')
+    return (req: Request, res: Response) => {
+      const service = getService(req)
+
+      if (service === 'approved-premises') {
+        res.redirect(apManagePaths.premises.index({}))
+      } else {
+        res.redirect(taManagePaths.premises.new({}))
+      }
     }
   }
 }
