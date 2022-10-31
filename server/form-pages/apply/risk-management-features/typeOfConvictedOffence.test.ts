016a845957b5b967098fb40afa212f0fccd2d73b
rich <rich.james@dxw.com>
2022-10-31 16:47:01 +0000

    Create page object for TypeOfConvictedOffence

---
+++ server/form-pages/apply/risk-management-features/typeOfConvictedOffence.test.ts
@@ -0,0 +1,71 @@
+import { itShouldHaveNextValue, itShouldHavePreviousValue } from '../../shared-examples'
+
+import applicationFactory from '../../../testutils/factories/application'
+import personFactory from '../../../testutils/factories/person'
+import TypeOfConvictedOffence, { offences } from './typeOfConvictedOffence'
+import { convertKeyValuePairToCheckBoxItems } from '../../../utils/formUtils'
+
+jest.mock('../../../utils/formUtils')
+
+describe('TypeOfConvictedOffence', () => {
+  const person = personFactory.build({ name: 'John Wayne' })
+  const application = applicationFactory.build({ person })
+
+  describe('body', () => {
+    it('should strip unknown attributes from the body', () => {
+      const page = new TypeOfConvictedOffence({ offenceConvictions: 'some offence', something: 'else' }, application)
+
+      expect(page.body).toEqual({ offenceConvictions: 'some offence' })
+    })
+  })
+
+  itShouldHavePreviousValue(new TypeOfConvictedOffence({}, application), 'convicted-offences')
+
+  itShouldHaveNextValue(new TypeOfConvictedOffence({}, application), 'date-of-offence')
+
+  describe('errors', () => {
+    it('should return an empty object if the offenceConvictions are populated', () => {
+      const page = new TypeOfConvictedOffence({ offenceConvictions: 'some offence' }, application)
+      expect(page.errors()).toEqual({})
+    })
+
+    it('should return an error if the offenceConvictions are not populated', () => {
+      const page = new TypeOfConvictedOffence({}, application)
+      expect(page.errors()).toEqual({
+        offenceConvictions: 'You must specify at least one type of offence',
+      })
+    })
+  })
+
+  describe('response', () => {
+    describe('should return a translated version of the response', () => {
+      it('When there are multiple convictions', () => {
+        const page = new TypeOfConvictedOffence(
+          { offenceConvictions: ['arson', 'sexualOffence', 'hateCrimes', 'childNonSexualOffence'] },
+          application,
+        )
+
+        expect(page.response()).toEqual({
+          'What type of offending has John Wayne been convicted of?': {
+            Offences: 'Arson, Sexual Offence, Hate Crimes, Non-sexual offences against children',
+          },
+        })
+      })
+      it('When there is a single conviction', () => {
+        const page = new TypeOfConvictedOffence({ offenceConvictions: 'arson' }, application)
+
+        expect(page.response()).toEqual({
+          'What type of offending has John Wayne been convicted of?': {
+            Offences: 'Arson',
+          },
+        })
+      })
+    })
+  })
+  describe('items', () => {
+    it('calls convertKeyValuePairToCheckBoxItems with the correct arguments', () => {
+      new TypeOfConvictedOffence({ offenceConvictions: 'arson' }, application).items()
+      expect(convertKeyValuePairToCheckBoxItems).toHaveBeenCalledWith(offences, 'arson')
+    })
+  })
+})
