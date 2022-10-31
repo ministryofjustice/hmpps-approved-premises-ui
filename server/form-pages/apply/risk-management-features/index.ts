746459a2e9b332f955cd9d8e4e4b2a3f82039803
rich <rich.james@dxw.com>
2022-10-31 15:15:35 +0000

    Create page object

--- server/form-pages/apply/risk-management-features/index.ts
+++ server/form-pages/apply/risk-management-features/index.ts
@@ -1,7 +1,9 @@
-import DateOfOffence from './dateOfOffence'
 import RiskManagementFeatures from './riskManagementFeatures'
+import DateOfOffence from './dateOfOffence'
+import ConvictedOffences from './convictedOffences'
 
 export default {
   'risk-management-features': RiskManagementFeatures,
   'date-of-offence': DateOfOffence,
+  'convicted-offences': ConvictedOffences,
 }
