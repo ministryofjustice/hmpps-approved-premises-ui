f2b8ca544420ad07b4466650c450a38173d6b70d
pezholio <pezholio@gmail.com>
2022-11-09 14:30:54 +0000

    Add a Catering page class

--- server/form-pages/apply/further-considerations/index.ts
+++ server/form-pages/apply/further-considerations/index.ts
@@ -4,12 +4,14 @@ import RoomSharing from './roomSharing'
 import Vulnerability from './vulnerability'
 import PreviousPlacements from './previousPlacements'
 import ComplexCaseBoard from './complexCaseBoard'
+import Catering from './catering'
 
 const pages = {
   'room-sharing': RoomSharing,
   vulnerability: Vulnerability,
   'previous-placements': PreviousPlacements,
   'complex-case-board': ComplexCaseBoard,
+  catering: Catering,
 }
 
 export default pages
