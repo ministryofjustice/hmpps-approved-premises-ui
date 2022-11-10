0cc5d67291a3fabc490fb6b57838b2db7b86cd34
rich <rich.james@dxw.com>
2022-11-10 13:42:08 +0000

    Add page class for relocationRegion

--- server/form-pages/apply/move-on/index.ts
+++ server/form-pages/apply/move-on/index.ts
@@ -1,9 +1,11 @@
 /* istanbul ignore file */
 
+import RelocationRegion from './relocationRegion'
 import PlacementDuration from './placementDuration'
 
 const pages = {
   'placement-duration': PlacementDuration,
+  'relocation-region': RelocationRegion,
 }
 
 export default pages
---
+++ server/form-pages/apply/move-on/relocationRegion.test.ts
@@ -0,0 +1,50 @@
+import { itShouldHaveNextValue, itShouldHavePreviousValue } from '../../shared-examples'
+
+import RelocationRegion from './relocationRegion'
+import applicationFactory from '../../../testutils/factories/application'
+import personFactory from '../../../testutils/factories/person'
+
+describe('RelocationRegion', () => {
+  const person = personFactory.build({ name: 'John Wayne' })
+  const application = applicationFactory.build({ person })
+
+  describe('title', () => {
+    expect(new RelocationRegion({}, application).title).toBe('Placement duration and move on')
+  })
+
+  describe('body', () => {
+    it('should strip unknown attributes from the body', () => {
+      const page = new RelocationRegion({ postcodeArea: 'some code', something: 'else' }, application)
+      expect(page.body).toEqual({ postcodeArea: 'some code' })
+    })
+  })
+
+  itShouldHaveNextValue(new RelocationRegion({}, application), '')
+  itShouldHavePreviousValue(new RelocationRegion({}, application), 'placement-duration')
+
+  describe('errors', () => {
+    it('returns an error if not passed any input for the postcode', () => {
+      const page = new RelocationRegion({}, application)
+
+      expect(page.errors()).toEqual({
+        postcodeArea: 'You must enter a postcode region',
+      })
+    })
+
+    it('returns an error if passed an invalid postcode', () => {
+      const page = new RelocationRegion({ postcodeArea: 'foo' }, application)
+
+      expect(page.errors()).toEqual({
+        postcodeArea: 'You must enter a valid postcode region',
+      })
+    })
+  })
+
+  describe('response', () => {
+    const page = new RelocationRegion({ postcodeArea: 'XX1 1XX' }, application)
+
+    expect(page.response()).toEqual({
+      'Where is John Wayne most likely to live when they move on from the AP?': 'XX1 1XX',
+    })
+  })
+})
