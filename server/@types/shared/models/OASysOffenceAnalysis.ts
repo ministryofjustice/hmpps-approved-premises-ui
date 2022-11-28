88325ea42cc93959cb2f4032f24a1570c2ba8f11
brownbearsoftware <brownbearsoftware@users.noreply.github.com>
2022-11-28 13:50:58 +0000

    Updating hmpps-approved-premises-api models from OpenAPI specification

--- server/@types/shared/models/OASysOffenceAnalysis.ts
+++ server/@types/shared/models/OASysOffenceAnalysis.ts
@@ -8,11 +8,9 @@ import type { OASysAssessmentState } from './OASysAssessmentState';
 
 export type OASysOffenceAnalysis = {
     assessmentId: OASysAssessmentId;
-    assessmentState?: OASysAssessmentState;
-    /**
-     * If the assessment is completed this is the date it was completed, otherwise it is the date it was initiated
-     */
-    date?: string;
+    assessmentState: OASysAssessmentState;
+    dateStarted: string;
+    dateCompleted?: string;
     offenceAnalysis: ArrayOfOASysOffenceAnalysisQuestions;
 };
 
