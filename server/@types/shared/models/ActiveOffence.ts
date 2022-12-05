4e46937af68aa669d3ec1cc22d33fdd496254515
brownbearsoftware <brownbearsoftware@users.noreply.github.com>
2022-12-05 14:06:15 +0000

    Updating hmpps-approved-premises-api models from OpenAPI specification

---
+++ server/@types/shared/models/ActiveOffence.ts
@@ -0,0 +1,11 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type ActiveOffence = {
+    deliusEventNumber: string;
+    offenceDescription: string;
+    offenceId: string;
+    convictionId: number;
+};
+
