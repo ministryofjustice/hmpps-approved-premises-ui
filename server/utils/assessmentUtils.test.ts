87b3d6961a3f7ec7b46cbad06c5012b3e4abbb71
pezholio <pezholio@gmail.com>
2022-12-07 16:25:45 +0000

    Helpers to return assessments awaiting assessment
    This includes a bunch of other helper methods that have been exported to
    allow ease of testing.

---
+++ server/utils/assessmentUtils.test.ts
@@ -0,0 +1,113 @@
+import { AssessmentWithRisks } from '@approved-premises/ui'
+import {
+  daysSinceReceived,
+  getStatus,
+  formattedArrivalDate,
+  awaitingAssessmentTableRows,
+  formatDays,
+  daysUntilDue,
+} from './assessmentUtils'
+
+import * as personUtils from './personUtils'
+import * as applicationUtils from './applicationUtils'
+
+import assessmentFactory from '../testutils/factories/assessment'
+import risksFactory from '../testutils/factories/risks'
+
+jest.mock('./applicationUtils')
+jest.mock('./personUtils')
+
+describe('assessmentUtils', () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+  })
+
+  describe('daysSinceReceived', () => {
+    it('returns the difference in days since the assessment has been received', () => {
+      const assessment = assessmentFactory.createdXDaysAgo(10).build()
+
+      expect(daysSinceReceived(assessment)).toEqual(10)
+    })
+  })
+
+  describe('formatDays', () => {
+    it('returns the singular form if there is 1 day', () => {
+      expect(formatDays(1)).toEqual('1 Day')
+    })
+
+    it('returns the plural form if there is more than 1 day', () => {
+      expect(formatDays(22)).toEqual('22 Days')
+    })
+
+    it('returns N/A if the day in undefined', () => {
+      expect(formatDays(undefined)).toEqual('N/A')
+    })
+  })
+
+  describe('getStatus', () => {
+    it('returns Not started for an assessment without data', () => {
+      const assessment = assessmentFactory.build({ data: undefined })
+
+      expect(getStatus(assessment)).toEqual('<strong class="govuk-tag govuk-tag--grey">Not started</strong>')
+    })
+
+    it('returns In Progress for an assessment with data and no decision', () => {
+      const assessment = assessmentFactory.build({ decision: undefined })
+
+      expect(getStatus(assessment)).toEqual('<strong class="govuk-tag govuk-tag--blue">In progress</strong>')
+    })
+
+    it('returns Completed for an assessment with data and a decision', () => {
+      const assessment = assessmentFactory.build({ decision: 'accepted' })
+
+      expect(getStatus(assessment)).toEqual('<strong class="govuk-tag govuk-tag">Completed</strong>')
+    })
+  })
+
+  describe('daysUntilDue', () => {
+    it('returns the days until the assessment is due', () => {
+      const assessment = assessmentFactory.createdXDaysAgo(2).build()
+
+      expect(daysUntilDue(assessment)).toEqual(7)
+    })
+  })
+
+  describe('formattedArrivalDate', () => {
+    it('returns the formatted arrival date from the application', () => {
+      const assessment = assessmentFactory.build()
+      const getDateSpy = jest.spyOn(applicationUtils, 'getArrivalDate').mockReturnValue('2022-01-01')
+
+      expect(formattedArrivalDate(assessment)).toEqual('1 Jan 2022')
+      expect(getDateSpy).toHaveBeenCalledWith(assessment.application)
+    })
+  })
+
+  describe('awaitingAssessmentTableRows', () => {
+    it('returns table rows for the assessments', () => {
+      const assessment = assessmentFactory.build()
+      const risks = risksFactory.build()
+      const assessmentWithRisks = {
+        ...assessment,
+        application: { person: { ...assessment.application.person, risks } },
+      } as AssessmentWithRisks
+
+      jest.spyOn(applicationUtils, 'getArrivalDate').mockReturnValue('2022-01-01')
+
+      const tierBadgeSpy = jest.spyOn(personUtils, 'tierBadge').mockReturnValue('TIER_BADGE')
+
+      expect(awaitingAssessmentTableRows([assessmentWithRisks])).toEqual([
+        [
+          { html: `<a href="#">${assessment.application.person.name}</a>` },
+          { html: assessment.application.person.crn },
+          { html: 'TIER_BADGE' },
+          { text: formattedArrivalDate(assessment) },
+          { text: assessment.application.person.prisonName },
+          { text: formatDays(daysUntilDue(assessment)) },
+          { html: getStatus(assessment) },
+        ],
+      ])
+
+      expect(tierBadgeSpy).toHaveBeenCalledWith(risks.tier.value.level)
+    })
+  })
+})
---
+++ server/utils/assessmentUtils.ts
@@ -0,0 +1,76 @@
+import { TableRow, AssessmentWithRisks } from '@approved-premises/ui'
+import { format, differenceInDays, add } from 'date-fns'
+
+import { Assessment } from '@approved-premises/api'
+import { tierBadge } from './personUtils'
+import { DateFormats } from './dateUtils'
+import { getArrivalDate } from './applicationUtils'
+
+const awaitingAssessmentTableRows = (assessments: Array<AssessmentWithRisks>): Array<TableRow> => {
+  const rows = [] as Array<TableRow>
+
+  assessments.forEach(assessment => {
+    rows.push([
+      {
+        html: `<a href="#">${assessment.application.person.name}</a>`,
+      },
+      {
+        html: assessment.application.person.crn,
+      },
+      {
+        html: tierBadge(assessment.application.person.risks.tier.value.level),
+      },
+      {
+        text: formattedArrivalDate(assessment),
+      },
+      {
+        text: assessment.application.person.prisonName,
+      },
+      {
+        text: formatDays(daysUntilDue(assessment)),
+      },
+      {
+        html: getStatus(assessment),
+      },
+    ])
+  })
+
+  return rows
+}
+
+const formattedArrivalDate = (assessment: Assessment): string => {
+  const arrivalDate = getArrivalDate(assessment.application)
+  return format(DateFormats.isoToDateObj(arrivalDate), 'd MMM yyyy')
+}
+
+const formatDays = (days: number): string => {
+  if (days === undefined) {
+    return 'N/A'
+  }
+  return `${days} Day${days > 1 ? 's' : ''}`
+}
+
+const daysUntilDue = (assessment: Assessment): number => {
+  const receivedDate = DateFormats.isoToDateObj(assessment.createdAt)
+  const dueDate = add(receivedDate, { days: 10 })
+
+  return differenceInDays(dueDate, new Date())
+}
+
+const getStatus = (assessment: Assessment): string => {
+  if (assessment.data) {
+    if (!assessment.decision) {
+      return `<strong class="govuk-tag govuk-tag--blue">In progress</strong>`
+    }
+    return `<strong class="govuk-tag govuk-tag">Completed</strong>`
+  }
+  return `<strong class="govuk-tag govuk-tag--grey">Not started</strong>`
+}
+
+const daysSinceReceived = (assessment: Assessment): number => {
+  const receivedDate = DateFormats.isoToDateObj(assessment.createdAt)
+
+  return differenceInDays(new Date(), receivedDate)
+}
+
+export { awaitingAssessmentTableRows, getStatus, daysSinceReceived, formattedArrivalDate, formatDays, daysUntilDue }
