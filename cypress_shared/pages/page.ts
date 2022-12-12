25066db1b151b18986829a6b9ce4a6b515461fda
rich <rich.james@dxw.com>
2022-12-12 17:35:28 +0000

    Move riskWidget assertions on to the pageObjects

--- cypress_shared/pages/page.ts
+++ cypress_shared/pages/page.ts
@@ -1,3 +1,4 @@
+import { PersonRisksUI } from '../../server/@types/ui'
 import errorLookups from '../../server/i18n/en/errors.json'
 import { DateFormats } from '../../server/utils/dateUtils'
 
@@ -87,5 +88,43 @@ export default abstract class Page {
 
   clickBack(): void {
     cy.get('a').contains('Back').click()
+  }
+
+  shouldShowMappa = (): void => {
+    cy.get('h3').contains('MAPPA')
+    cy.get('h3').contains('CAT 2 / LEVEL 1')
+  }
+
+  shouldShowRosh = (risks: PersonRisksUI['roshRisks']): void => {
+    cy.get('h3').contains(`${risks.overallRisk.toLocaleUpperCase()} RoSH`)
+    cy.get('p').contains(`Last updated: ${risks.lastUpdated}`)
+
+    cy.get('.rosh-widget__table').within($row => {
+      cy.wrap($row).get('th').contains('Children').get('td').contains(risks.riskToChildren, { matchCase: false })
+      cy.wrap($row).get('th').contains('Public').get('td').contains(risks.riskToPublic, { matchCase: false })
+      cy.wrap($row).get('th').contains('Known adult').get('td').contains(risks.riskToKnownAdult, { matchCase: false })
+      cy.wrap($row).get('th').contains('Staff').get('td').contains(risks.riskToStaff, { matchCase: false })
+    })
+  }
+
+  shouldShowTier = (tier: PersonRisksUI['tier']): void => {
+    cy.get('h3').contains(`TIER ${tier.level}`)
+    cy.get('p').contains(`Last updated: ${tier.lastUpdated}`)
+  }
+
+  shouldShowDeliusRiskFlags = (flags: Array<string>): void => {
+    cy.get('h3').contains(`Delius risk flags (registers)`)
+    cy.get('.risk-flag-widget > ul').within($item => {
+      flags.forEach(flag => {
+        cy.wrap($item).get('li').should('contain', flag)
+      })
+    })
+  }
+
+  shouldShowRiskWidgets(risks: PersonRisksUI): void {
+    this.shouldShowMappa()
+    this.shouldShowRosh(risks.roshRisks)
+    this.shouldShowTier(risks.tier)
+    this.shouldShowDeliusRiskFlags(risks.flags)
   }
 }
