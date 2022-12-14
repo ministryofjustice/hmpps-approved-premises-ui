26cd56053201105bee6c5ae7772bc64111cc25c9
pezholio <pezholio@gmail.com>
2022-12-14 13:53:59 +0000

    Remove risk API stubbing
    We don’t need this anymore, as the risks come directly from the
    application.

--- integration_tests/tests/apply/list.cy.ts
+++ integration_tests/tests/apply/list.cy.ts
@@ -1,7 +1,6 @@
 import { StartPage, ListPage } from '../../../cypress_shared/pages/apply'
 
 import applicationFactory from '../../../server/testutils/factories/application'
-import risksFactory from '../../../server/testutils/factories/risks'
 import Page from '../../../cypress_shared/pages/page'
 
 context('Applications dashboard', () => {
@@ -19,17 +18,11 @@ context('Applications dashboard', () => {
     const applications = applicationFactory.withReleaseDate().buildList(5)
     cy.task('stubApplications', applications)
 
-    // And there are risks for the persons related to these applications
-    const risks = risksFactory.buildList(5)
-    applications.forEach((application, i) => {
-      cy.task('stubPersonRisks', { person: application.person, risks: risks[i] })
-    })
-
     // When I visit the Previous Applications page
     const page = ListPage.visit()
 
     // Then I should see all of the application summaries listed
-    page.shouldShowApplications(applications, risks)
+    page.shouldShowApplications(applications)
 
     // And I the button should take me to the application start page
     page.clickSubmit()
