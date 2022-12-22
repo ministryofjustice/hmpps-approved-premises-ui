479cc12b4aaab98eb979d9c4424334b5c2e1f508
rich <rich.james@dxw.com>
2022-12-22 09:49:23 +0000

    Add 'initialize' and 'save' methods to Assessment service
    These will be used for setting up the Page object and saving the assessment.
    Both methods are very similar to the Application equivalents

--- server/services/assessmentService.test.ts
+++ server/services/assessmentService.test.ts
@@ -1,10 +1,18 @@
+import { DeepMocked, createMock } from '@golevelup/ts-jest'
+import { Request } from 'express'
+
 import { AssessmentClient } from '../data'
 import AssessmentService from './assessmentService'
-
 import assessmentFactory from '../testutils/factories/assessment'
+
+import { getBody } from '../form-pages/utils'
+import TasklistPage from '../form-pages/tasklistPage'
+import { TaskListErrors } from '../@types/ui'
+import { ValidationError } from '../utils/errors'
 
 jest.mock('../data/assessmentClient.ts')
 jest.mock('../data/personClient.ts')
+jest.mock('../form-pages/utils')
 
 describe('AssessmentService', () => {
   const assessmentClient = new AssessmentClient(null) as jest.Mocked<AssessmentClient>
@@ -44,5 +52,86 @@ describe('AssessmentService', () => {
     expect(result).toEqual(assessment)
 
     expect(assessmentClient.find).toHaveBeenCalledWith(assessment.id)
+  })
+
+  describe('initializePage', () => {
+    let request: DeepMocked<Request>
+
+    const assessment = assessmentFactory.build()
+    const Page = jest.fn()
+
+    beforeEach(() => {
+      request = createMock<Request>({
+        params: { id: assessment.id, task: 'my-task', page: 'first' },
+        session: { application: assessment, previousPage: '' },
+        user: { token: 'some-token' },
+      })
+    })
+
+    it('should fetch the assessment from the API', async () => {
+      assessmentClient.find.mockResolvedValue(assessment)
+      ;(getBody as jest.Mock).mockReturnValue(request.body)
+
+      const result = await service.initializePage(Page, request, {})
+
+      expect(result).toBeInstanceOf(Page)
+
+      expect(Page).toHaveBeenCalledWith(request.body, assessment, '')
+      expect(assessmentClient.find).toHaveBeenCalledWith(request.params.id)
+    })
+  })
+
+  describe('save', () => {
+    const assessment = assessmentFactory.build()
+
+    const token = 'some-token'
+    const request = createMock<Request>({
+      params: { id: assessment.id, task: 'some-task', page: 'some-page' },
+      user: { token },
+    })
+
+    describe('when there are no validation errors', () => {
+      let page: DeepMocked<TasklistPage>
+
+      beforeEach(() => {
+        page = createMock<TasklistPage>({
+          errors: () => {
+            return {} as TaskListErrors<TasklistPage>
+          },
+          body: { foo: 'bar' },
+        })
+
+        assessmentClient.find.mockResolvedValue(assessment)
+      })
+
+      it('does not throw an error', () => {
+        expect(async () => {
+          await service.save(page, request)
+        }).not.toThrow(ValidationError)
+      })
+
+      it('saves data to the api', async () => {
+        await service.save(page, request)
+
+        expect(assessmentClientFactory).toHaveBeenCalledWith(token)
+        expect(assessmentClient.update).toHaveBeenCalledWith(assessment)
+      })
+    })
+
+    describe('When there validation errors', () => {
+      it('throws an error if there is a validation error', async () => {
+        const errors = createMock<TaskListErrors<TasklistPage>>({ knowOralHearingDate: 'error' })
+        const page = createMock<TasklistPage>({
+          errors: () => errors,
+        })
+
+        expect.assertions(1)
+        try {
+          await service.save(page, request)
+        } catch (e) {
+          expect(e).toEqual(new ValidationError(errors))
+        }
+      })
+    })
   })
 })
--- server/services/assessmentService.ts
+++ server/services/assessmentService.ts
@@ -1,7 +1,11 @@
+import type { Request } from 'express'
 import { ApprovedPremisesAssessment as Assessment } from '@approved-premises/api'
 import type { GroupedAssessments } from '@approved-premises/ui'
 
 import type { RestClientBuilder, AssessmentClient } from '../data'
+import TasklistPage, { TasklistPageInterface } from '../form-pages/tasklistPage'
+import { getBody, getPageName, getTaskName } from '../form-pages/utils'
+import { ValidationError } from '../utils/errors'
 
 export default class AssessmentService {
   constructor(private readonly assessmentClientFactory: RestClientBuilder<AssessmentClient>) {}
@@ -26,5 +30,33 @@ export default class AssessmentService {
     const assessment = await client.find(id)
 
     return assessment
+  }
+
+  async initializePage(Page: TasklistPageInterface, request: Request, userInput?: Record<string, unknown>) {
+    const assessment = await this.findAssessment(request.user.token, request.params.id)
+    const body = getBody(Page, assessment, request, userInput)
+
+    return new Page(body, assessment, request.session.previousPage)
+  }
+
+  async save(page: TasklistPage, request: Request) {
+    const errors = page.errors()
+
+    if (Object.keys(errors).length) {
+      throw new ValidationError<typeof page>(errors)
+    } else {
+      const assessment = await this.findAssessment(request.user.token, request.params.id)
+
+      const pageName = getPageName(page.constructor)
+      const taskName = getTaskName(page.constructor)
+
+      assessment.data = assessment.data || {}
+      assessment.data[taskName] = assessment.data[taskName] || {}
+      assessment.data[taskName][pageName] = page.body
+
+      const client = this.assessmentClientFactory(request.user.token)
+
+      await client.update(assessment)
+    }
   }
 }
