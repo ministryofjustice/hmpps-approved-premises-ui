2db45879bfaef413e43570d94efcee11c07fad1f
pezholio <pezholio@gmail.com>
2023-01-06 09:15:19 +0000

    Add e2e tests for Apply
    This uses the helper in the e2e tests.
    
    The confirmation screen doesn’t currently work, as we have no staff
    in the database to allocate an application to, so is commented out with
    a todo.
    
    I’ve also added a person fixture, which contains all the information we
    expect the user with the CRN `D002399` to have.

---
+++ e2e/tests/stepDefinitions/apply.ts
@@ -0,0 +1,38 @@
+import { Given, Then } from '@badeball/cypress-cucumber-preprocessor'
+
+import ApplyHelper from '../../../cypress_shared/helpers/apply'
+
+import applicationFactory from '../../../server/testutils/factories/application'
+import personFactory from '../../../server/testutils/factories/person'
+
+import personData from '../../../cypress_shared/fixtures/person.json'
+import applicationData from '../../../cypress_shared/fixtures/applicationData.json'
+
+const person = personFactory.build(personData)
+const application = applicationFactory.build({ person, data: applicationData })
+
+Given('I start a new application', () => {
+  const apply = new ApplyHelper(application, person, 'e2e')
+  apply.startApplication()
+})
+
+Given('I fill in and complete an application', () => {
+  cy.url().then(url => {
+    const id = url.match(/applications\/(.+)\/tasks/)[1]
+    application.id = id
+
+    const apply = new ApplyHelper(application, person, 'e2e')
+
+    apply.completeApplication()
+  })
+})
+
+Then('I should see a confirmation screen', () => {
+  // TODO: add expectations when we have enough information to submit an application
+  // // And I should be taken to the confirmation page
+  // const confirmationPage = new SubmissionConfirmation()
+  // // When I click 'Back to dashboard'
+  // confirmationPage.clickBackToDashboard()
+  // // Then I am taken back to the dashboard
+  // Page.verifyOnPage(ListPage)
+})
