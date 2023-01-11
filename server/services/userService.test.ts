03e9cd7b4eef93b64d383e150c4e3a9cb0e9fe14
pezholio <pezholio@gmail.com>
2023-01-11 09:11:35 +0000

    Add service method to get a user
    The UserService already exists to get user details from HMPPS Auth.
    I’ve updated this to add the userClientFactory to fetch user details
    from our API too

--- server/services/userService.test.ts
+++ server/services/userService.test.ts
@@ -1,19 +1,31 @@
 import UserService from './userService'
 import HmppsAuthClient, { User } from '../data/hmppsAuthClient'
+import { UserClient } from '../data'
+
+import userFactory from '../testutils/factories/user'
 
 jest.mock('../data/hmppsAuthClient')
+jest.mock('../data/userClient')
 
 const token = 'some token'
 
 describe('User service', () => {
+  const userClient: jest.Mocked<UserClient> = new UserClient(null) as jest.Mocked<UserClient>
+  const userClientFactory = jest.fn()
+
   let hmppsAuthClient: jest.Mocked<HmppsAuthClient>
   let userService: UserService
 
+  beforeEach(() => {
+    jest.resetAllMocks()
+
+    hmppsAuthClient = new HmppsAuthClient(null) as jest.Mocked<HmppsAuthClient>
+    userClientFactory.mockReturnValue(userClient)
+
+    userService = new UserService(hmppsAuthClient, userClientFactory)
+  })
+
   describe('getUser', () => {
-    beforeEach(() => {
-      hmppsAuthClient = new HmppsAuthClient(null) as jest.Mocked<HmppsAuthClient>
-      userService = new UserService(hmppsAuthClient)
-    })
     it('Retrieves and formats user name', async () => {
       hmppsAuthClient.getUser.mockResolvedValue({ name: 'john smith' } as User)
 
@@ -21,10 +33,26 @@ describe('User service', () => {
 
       expect(result.displayName).toEqual('John Smith')
     })
+
     it('Propagates error', async () => {
       hmppsAuthClient.getUser.mockRejectedValue(new Error('some error'))
 
       await expect(userService.getUser(token)).rejects.toEqual(new Error('some error'))
+    })
+  })
+
+  describe('getUserById', () => {
+    it('returns a user by ID', async () => {
+      const id = 'SOME_ID'
+      const user = userFactory.build()
+
+      userClient.getUser.mockResolvedValue(user)
+
+      const result = await userService.getUserById(token, id)
+
+      expect(result).toEqual(user)
+
+      expect(userClient.getUser).toHaveBeenCalledWith(id)
     })
   })
 })
--- server/services/userService.ts
+++ server/services/userService.ts
@@ -1,3 +1,5 @@
+import { User } from '@approved-premises/api'
+import { RestClientBuilder, UserClient } from '../data'
 import { convertToTitleCase } from '../utils/utils'
 import type HmppsAuthClient from '../data/hmppsAuthClient'
 
@@ -7,10 +9,19 @@ interface UserDetails {
 }
 
 export default class UserService {
-  constructor(private readonly hmppsAuthClient: HmppsAuthClient) {}
+  constructor(
+    private readonly hmppsAuthClient: HmppsAuthClient,
+    private readonly userClientFactory: RestClientBuilder<UserClient>,
+  ) {}
 
   async getUser(token: string): Promise<UserDetails> {
     const user = await this.hmppsAuthClient.getUser(token)
     return { ...user, displayName: convertToTitleCase(user.name) }
+  }
+
+  async getUserById(token: string, id: string): Promise<User> {
+    const client = this.userClientFactory(token)
+
+    return client.getUser(id)
   }
 }
