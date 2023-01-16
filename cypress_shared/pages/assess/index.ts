0edcd27aa4f4380b0444c0411790569ce3d0f816
rich <rich.james@dxw.com>
2023-01-16 17:00:48 +0000

    Rename review > reviewPage
    In order for it to be consistent with the other pages in this folder

--- cypress_shared/pages/assess/index.ts
+++ cypress_shared/pages/assess/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import ListPage from './listPage'
 import TaskListPage from './taskListPage'
-import ReviewPage from './review'
+import ReviewPage from './reviewPage'
 import SufficientInformationPage from './sufficientInformationPage'
 
 export { ListPage, TaskListPage, ReviewPage, SufficientInformationPage }
