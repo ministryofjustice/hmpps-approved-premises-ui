e51ba653dfd1ea2977a9aab17c16bc43df640453
rich <rich.james@dxw.com>
2023-01-18 11:06:21 +0000

    Add view for RequiredActions page
    Similarly this is very similar to the SuitabilityAssessment view with
    some additional complexity around the questions that show if the user
    answers ‘yes’ to the ‘concernsOfUnmanagableRisk’ questions

--- cypress_shared/pages/assess/index.ts
+++ cypress_shared/pages/assess/index.ts
@@ -3,5 +3,7 @@ import ListPage from './listPage'
 import TaskListPage from './taskListPage'
 import ReviewPage from './reviewPage'
 import SufficientInformationPage from './sufficientInformationPage'
+import SuitabilityAssessmentPage from './suitabilityAssessmentPage'
+import RequiredActionsPage from './requiredActionsPage'
 
-export { ListPage, TaskListPage, ReviewPage, SufficientInformationPage }
+export { ListPage, TaskListPage, ReviewPage, SufficientInformationPage, SuitabilityAssessmentPage, RequiredActionsPage }
