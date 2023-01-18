25ce61977661562a51d0738e091734fdbc661e28
pezholio <pezholio@gmail.com>
2023-01-18 13:07:21 +0000

    Add a stub requestInformation page
    This will just fetch user details and render a page. The page will then
    have a form which POSTs to the controller action we created in the last
    commit

--- server/form-pages/assess/reviewApplication/sufficientInformation/index.ts
+++ server/form-pages/assess/reviewApplication/sufficientInformation/index.ts
@@ -1,10 +1,11 @@
 import { Task } from '../../../utils/decorators'
 
 import SufficentInformationPage from './sufficientInformation'
+import RequestInformation from './requestInformation'
 
 @Task({
   slug: 'sufficient-information',
   name: 'Check there is sufficient information to make a decision',
-  pages: [SufficentInformationPage],
+  pages: [SufficentInformationPage, RequestInformation],
 })
 export default class SufficientInformation {}
