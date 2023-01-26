dd8ec5be35be7e7b60c843321d5c0d81bfbd6363
rich <rich.james@dxw.com>
2023-01-26 10:52:04 +0000

    Extract shouldShowTitle
    We can use this for checking Assess CYA titles too so we extract it
    to the Page object

--- cypress_shared/pages/page.ts
+++ cypress_shared/pages/page.ts
@@ -158,4 +158,10 @@ export default abstract class Page {
       cy.readFile(downloadedFilename, 'binary', { timeout: 300 })
     })
   }
+
+  shouldShowCyaTitle(taskName: string, taskTitle: string) {
+    cy.get(`[data-cy-check-your-answers-section="${taskName}"]`).within(() => {
+      cy.get('.box-title').should('contain', taskTitle)
+    })
+  }
 }
