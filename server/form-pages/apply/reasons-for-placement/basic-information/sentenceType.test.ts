e504959fa0e8b73cc1251c5ea0c8ce42e8a0b98c
pezholio <pezholio@gmail.com>
2023-01-31 17:20:59 +0000

    sentenceType conditionally shows back link
    If we got to the sentence type page via the exception details
    page, then we’ll want to show a back link to that previous page,
    therefor we’ll check for the presence of that section in the
    application data and return `exception-details` if we find it.

--- server/form-pages/apply/reasons-for-placement/basic-information/sentenceType.test.ts
+++ server/form-pages/apply/reasons-for-placement/basic-information/sentenceType.test.ts
@@ -1,11 +1,14 @@
+import applicationFactory from '../../../../testutils/factories/application'
 import { itShouldHavePreviousValue } from '../../../shared-examples'
 
 import SentenceType from './sentenceType'
 
 describe('SentenceType', () => {
+  const application = applicationFactory.build()
+
   describe('body', () => {
     it('should set the body', () => {
-      const page = new SentenceType({ sentenceType: 'standardDeterminate' })
+      const page = new SentenceType({ sentenceType: 'standardDeterminate' }, application)
 
       expect(page.body).toEqual({ sentenceType: 'standardDeterminate' })
     })
@@ -13,53 +16,65 @@ describe('SentenceType', () => {
 
   describe('next', () => {
     it('should return release-type for a standardDeterminate sentence', () => {
-      const page = new SentenceType({ sentenceType: 'standardDeterminate' })
+      const page = new SentenceType({ sentenceType: 'standardDeterminate' }, application)
       expect(page.next()).toEqual('release-type')
     })
 
     it('should return situation for a communityOrder sentence', () => {
-      const page = new SentenceType({ sentenceType: 'communityOrder' })
+      const page = new SentenceType({ sentenceType: 'communityOrder' }, application)
       expect(page.next()).toEqual('situation')
     })
 
     it('should return situation for a bailPlacement sentence', () => {
-      const page = new SentenceType({ sentenceType: 'bailPlacement' })
+      const page = new SentenceType({ sentenceType: 'bailPlacement' }, application)
       expect(page.next()).toEqual('situation')
     })
 
     it('should return release-type for an extendedDeterminate sentence', () => {
-      const page = new SentenceType({ sentenceType: 'extendedDeterminate' })
+      const page = new SentenceType({ sentenceType: 'extendedDeterminate' }, application)
       expect(page.next()).toEqual('release-type')
     })
 
     it('should return release-type for an ipp sentence', () => {
-      const page = new SentenceType({ sentenceType: 'ipp' })
+      const page = new SentenceType({ sentenceType: 'ipp' }, application)
       expect(page.next()).toEqual('release-type')
     })
 
     it('should return release-type for a life sentence', () => {
-      const page = new SentenceType({ sentenceType: 'life' })
+      const page = new SentenceType({ sentenceType: 'life' }, application)
       expect(page.next()).toEqual('release-type')
     })
   })
 
-  itShouldHavePreviousValue(new SentenceType({}), '')
+  describe('when the exception-details step was not completed', () => {
+    itShouldHavePreviousValue(new SentenceType({}, application), '')
+  })
+
+  describe('when the exception-details step was completed', () => {
+    itShouldHavePreviousValue(
+      new SentenceType(
+        {},
+        { ...application, data: { 'basic-information': { 'exception-details': { agreedCaseWithManager: 'yes' } } } },
+      ),
+      'exception-details',
+    )
+  })
 
   describe('errors', () => {
     it('should return an empty object if the sentence type is populated', () => {
-      const page = new SentenceType({ sentenceType: 'life' })
+      const page = new SentenceType({ sentenceType: 'life' }, application)
       expect(page.errors()).toEqual({})
     })
 
     it('should return an errors if the sentence type is not populated', () => {
-      const page = new SentenceType({})
+      const page = new SentenceType({}, application)
       expect(page.errors()).toEqual({ sentenceType: 'You must choose a sentence type' })
     })
   })
 
   describe('items', () => {
     it('marks an option as selected when the sentenceType is set', () => {
-      const page = new SentenceType({ sentenceType: 'life' })
+      const page = new SentenceType({ sentenceType: 'life' }, application)
 
       const selectedOptions = page.items().filter(item => item.checked)
 
@@ -68,7 +83,7 @@ describe('SentenceType', () => {
     })
 
     it('marks no options selected when the sentenceType is not set', () => {
-      const page = new SentenceType({})
+      const page = new SentenceType({}, application)
 
       const selectedOptions = page.items().filter(item => item.checked)
 
@@ -78,7 +93,7 @@ describe('SentenceType', () => {
 
   describe('response', () => {
     it('should return a translated version of the response', () => {
-      const page = new SentenceType({ sentenceType: 'life' })
+      const page = new SentenceType({ sentenceType: 'life' }, application)
 
       expect(page.response()).toEqual({ [page.title]: 'Life sentence' })
     })
