9558385b8eb2e875f97d4612b0e40e9414671f41
rich <rich.james@dxw.com>
2023-02-06 17:40:04 +0000

    Update adjudicationsFromAssessment to handle undefined case-notes object

--- server/utils/assessments/utils.ts
+++ server/utils/assessments/utils.ts
@@ -331,7 +331,7 @@ const confirmationPageResult = (assessment: Assessment) => {
 }
 
 const adjudicationsFromAssessment = (assessment: Assessment) =>
-  assessment.application?.data?.['prison-information']?.['case-notes'].adjudications || ''
+  assessment.application?.data?.['prison-information']?.['case-notes']?.adjudications || ''
 
 const caseNotesFromAssessment = (assessment: Assessment) =>
   assessment.application?.data?.['prison-information']?.['case-notes']?.selectedCaseNotes || ''
