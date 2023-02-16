6648816b678dd849efe4c53071ede0eb206fb380
pezholio <pezholio@gmail.com>
2023-02-16 11:40:28 +0000

    Add a util to get the type of an application
    If the arrivalDate is:
    
    - less than 7 days away, this will be an emergency
    application
    - less than 4 months away, this will be a short_term application
    - otherwise it will be standard

---
+++ server/utils/applications/noticeTypeFromApplication.test.ts
@@ -0,0 +1,40 @@
+import { add } from 'date-fns'
+
+import { DateFormats } from '../dateUtils'
+import { getArrivalDate } from './utils'
+import { noticeTypeFromApplication } from './noticeTypeFromApplication'
+
+import applicationFactory from '../../testutils/factories/application'
+
+jest.mock('./utils')
+
+describe('noticeTypeFromApplication', () => {
+  const application = applicationFactory.build({})
+
+  it('returns emergency if the arrival date is less than seven days away', () => {
+    const date = add(new Date(), { days: 4 })
+    const arrivalDate = DateFormats.dateObjToIsoDate(date)
+
+    ;(arrivalDateFromApplication as jest.Mock).mockReturnValue(arrivalDate)
+
+    expect(noticeTypeFromApplication(application)).toEqual('emergency')
+  })
+
+  it('returns short_notice if the arrival date is less than 4 months away', () => {
+    const date = add(new Date(), { months: 3 })
+    const arrivalDate = DateFormats.dateObjToIsoDate(date)
+
+    ;(arrivalDateFromApplication as jest.Mock).mockReturnValue(arrivalDate)
+
+    expect(noticeTypeFromApplication(application)).toEqual('short_notice')
+  })
+
+  it('returns standard if the arrival date is more than 4 months away', () => {
+    const date = add(new Date(), { months: 6 })
+    const arrivalDate = DateFormats.dateObjToIsoDate(date)
+
+    ;(arrivalDateFromApplication as jest.Mock).mockReturnValue(arrivalDate)
+
+    expect(noticeTypeFromApplication(application)).toEqual('standard')
+  })
+})
