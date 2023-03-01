0fb8f299a10e409e7d7598a0bd827ede8835f348
pezholio <pezholio@gmail.com>
2023-03-01 10:52:32 +0000

    Point to correct locations for applicationVersion
    The generated build-info.json and package.json files are in the parent
    directory.

--- server/applicationVersion.ts
+++ server/applicationVersion.ts
@@ -2,10 +2,13 @@
 /* istanbul ignore file */
 
 import fs from 'fs'
+import path from 'path'
+
+const buildInfoPath = path.resolve(__dirname, '../build-info.json')
 
 const packageData = JSON.parse(fs.readFileSync('./package.json').toString())
-const { buildNumber, gitRef } = fs.existsSync('./build-info.json')
-  ? JSON.parse(fs.readFileSync('./build-info.json').toString())
+const { buildNumber, gitRef } = fs.existsSync(buildInfoPath)
+  ? JSON.parse(fs.readFileSync('../build-info.json').toString())
   : {
       buildNumber: packageData.version,
       gitRef: 'unknown',
