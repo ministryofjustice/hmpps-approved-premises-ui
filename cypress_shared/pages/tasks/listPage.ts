7b9a77fde83078fcdefde9f5242bc375c89e82aa
rich <rich.james@dxw.com>
2023-03-10 10:39:51 +0000

    Add view for allocated and unallocated tasks
    Now we have the util functions this is straight forward.

---
+++ cypress_shared/pages/tasks/listPage.ts
@@ -0,0 +1,29 @@
+import Page from '../page'
+import paths from '../../../server/paths/tasks'
+
+import { allocatedTableRows, unallocatedTableRows } from '../../../server/utils/tasks/table'
+
+import { Task } from '../../../server/@types/shared'
+import { shouldShowTableRows } from '../../helpers'
+
+export default class ListPage extends Page {
+  constructor(private readonly allocatedTasks: Array<Task>, private readonly unallocatedTasks: Array<Task>) {
+    super('Tasks')
+    this.allocatedTasks = allocatedTasks
+    this.unallocatedTasks = unallocatedTasks
+  }
+
+  static visit(allocatedTasks: Array<Task>, unallocatedTasks: Array<Task>): ListPage {
+    cy.visit(paths.index({}))
+    return new ListPage(allocatedTasks, unallocatedTasks)
+  }
+
+  shouldShowAllocatedTasks(): void {
+    shouldShowTableRows(this.allocatedTasks, allocatedTableRows)
+  }
+
+  shouldShowUnallocatedTasks(): void {
+    cy.get('a').contains('Unallocated').click()
+    shouldShowTableRows(this.unallocatedTasks, unallocatedTableRows)
+  }
+}
