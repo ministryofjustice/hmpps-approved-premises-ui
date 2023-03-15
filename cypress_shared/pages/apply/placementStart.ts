9bac47f7abd18115c9452b0bfdcfbedc74852a2f
pezholio <pezholio@gmail.com>
2023-03-15 08:12:37 +0000

    Use the new method in the PlacementStart helper

--- cypress_shared/pages/apply/placementStart.ts
+++ cypress_shared/pages/apply/placementStart.ts
@@ -1,15 +1,12 @@
 import { ApprovedPremisesApplication } from '@approved-premises/api'
 import ApplyPage from './applyPage'
 import { DateFormats } from '../../../server/utils/dateUtils'
-import { retrieveQuestionResponseFromApplication } from '../../../server/utils/utils'
+import { retrieveQuestionResponseFromApplicationOrAssessment } from '../../../server/utils/retrieveQuestionResponseFromApplicationOrAssessment'
+import ReleaseDate from '../../../server/form-pages/apply/reasons-for-placement/basic-information/releaseDate'
 
 export default class PlacementStartPage extends ApplyPage {
   constructor(application: ApprovedPremisesApplication) {
-    const releaseDate = retrieveQuestionResponseFromApplication(
-      application,
-      'basic-information',
-      'releaseDate',
-    ) as string
+    const releaseDate = retrieveQuestionResponseFromApplicationOrAssessment(application, ReleaseDate) as string
 
     super(
       `Is ${DateFormats.isoDateToUIDate(releaseDate)} the date you want the placement to start?`,
