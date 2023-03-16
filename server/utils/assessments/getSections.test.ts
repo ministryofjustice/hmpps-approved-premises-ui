72a05a3ae3f460af505cb6e5dbfc64f72e55f774
pezholio <pezholio@gmail.com>
2023-03-16 11:13:02 +0000

    Remove matching section when not approved
    When an assessment has a decision and that decision is not a positive
    one, we don’t need to ask for matching information, so we remove that
    section

--- server/utils/assessments/getSections.test.ts
+++ server/utils/assessments/getSections.test.ts
@@ -1,12 +1,17 @@
 import Assess from '../../form-pages/assess'
 import assessmentFactory from '../../testutils/factories/assessment'
 import getSections from './getSections'
+import informationSetAsNotReceived from './informationSetAsNotReceived'
+import { applicationAccepted, decisionFromAssessment } from './decisionUtils'
+
+jest.mock('./informationSetAsNotReceived')
+jest.mock('./decisionUtils')
 
 describe('getSections', () => {
   const assessment = assessmentFactory.build({ status: 'pending' })
 
-  it('returns all sections if informationReceived is yes', () => {
-    assessment.data = { 'sufficient-information': { 'information-received': { informationReceived: 'yes' } } }
+  it('returns all sections if informationSetAsNotReceived is false', () => {
+    ;(informationSetAsNotReceived as jest.Mock).mockReturnValue(false)
 
     const sections = getSections(assessment)
     const sectionNames = sections.map(s => s.name)
@@ -15,23 +20,50 @@ describe('getSections', () => {
     expect(sectionNames).toContain('AssessApplication')
   })
 
-  it('returns all sections if informationReceived is not yet answered', () => {
-    assessment.data = {}
-
-    const sections = getSections(assessment)
-    const sectionNames = sections.map(s => s.name)
-
-    expect(sections.length).toEqual(Assess.sections.length)
-    expect(sectionNames).toContain('AssessApplication')
-  })
-
-  it('removes the assess section if informationReceived is no', () => {
-    assessment.data = { 'sufficient-information': { 'information-received': { informationReceived: 'no' } } }
+  it('removes the assess section if informationSetAsNotReceived is true', () => {
+    ;(informationSetAsNotReceived as jest.Mock).mockReturnValue(true)
 
     const sections = getSections(assessment)
     const sectionNames = sections.map(s => s.name)
 
     expect(sections.length).toEqual(Assess.sections.length - 1)
+    expect(sectionNames).not.toContain('AssessApplication')
+  })
+
+  it('removes the matching information section if the application has not been accepted', () => {
+    ;(informationSetAsNotReceived as jest.Mock).mockReturnValue(false)
+    ;(decisionFromAssessment as jest.Mock).mockReturnValue('decision')
+    ;(applicationAccepted as jest.Mock).mockReturnValue(false)
+
+    const sections = getSections(assessment)
+    const sectionNames = sections.map(s => s.name)
+
+    expect(sections.length).toEqual(Assess.sections.length - 1)
+    expect(sectionNames).not.toContain('MatchingInformation')
+  })
+
+  it('removes the matching information section if the application has not been accepted', () => {
+    ;(informationSetAsNotReceived as jest.Mock).mockReturnValue(false)
+    ;(decisionFromAssessment as jest.Mock).mockReturnValue('decision')
+    ;(applicationAccepted as jest.Mock).mockReturnValue(true)
+
+    const sections = getSections(assessment)
+    const sectionNames = sections.map(s => s.name)
+
+    expect(sections.length).toEqual(Assess.sections.length)
+    expect(sectionNames).toContain('MatchingInformation')
+  })
+
+  it('removes the assess and matching information section if informationSetAsNotReceived is true and the application has not been accepted', () => {
+    ;(informationSetAsNotReceived as jest.Mock).mockReturnValue(true)
+    ;(decisionFromAssessment as jest.Mock).mockReturnValue('decision')
+    ;(applicationAccepted as jest.Mock).mockReturnValue(false)
+
+    const sections = getSections(assessment)
+    const sectionNames = sections.map(s => s.name)
+
+    expect(sections.length).toEqual(Assess.sections.length - 2)
+    expect(sectionNames).not.toContain('MatchingInformation')
     expect(sectionNames).not.toContain('AssessApplication')
   })
 })
