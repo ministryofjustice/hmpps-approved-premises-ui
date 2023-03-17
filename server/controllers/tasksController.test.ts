9a83540e0b2a3cc35a0d839c50360ccf5d68e2fc
rich <rich.james@dxw.com>
2023-03-17 15:49:04 +0000

    Add show method to task controller
    This is almost identical to the allocations controller show method.
    We need to add the User and Application Service into the controller which
    requires a bit of work

--- server/controllers/tasksController.test.ts
+++ server/controllers/tasksController.test.ts
@@ -2,28 +2,42 @@ import type { NextFunction, Request, Response } from 'express'
 import { DeepMocked, createMock } from '@golevelup/ts-jest'
 
 import TasksController from './tasksController'
-import TaskService from '../services/taskService'
 import taskFactory from '../testutils/factories/task'
 import { groupByAllocation } from '../utils/tasks'
+import { ApplicationService, TaskService, UserService } from '../services'
+import { getQualificationsForApplication } from '../utils/applications/getQualificationsForApplication'
+import userFactory from '../testutils/factories/user'
+import applicationFactory from '../testutils/factories/application'
+import assessmentFactory from '../testutils/factories/assessment'
+import { catchValidationErrorOrPropogate, fetchErrorsAndUserInput } from '../utils/validation'
+import { ErrorsAndUserInput } from '../@types/ui'
+import paths from '../paths/tasks'
+
+jest.mock('../utils/applications/getQualificationsForApplication')
+jest.mock('../utils/validation')
 
 describe('TasksController', () => {
   const request: DeepMocked<Request> = createMock<Request>({})
   const response: DeepMocked<Response> = createMock<Response>({})
   const next: DeepMocked<NextFunction> = createMock<NextFunction>({})
 
-  const tasksService = createMock<TaskService>({})
+  const applicationService = createMock<ApplicationService>({})
+  const taskService = createMock<TaskService>({})
+  const userService = createMock<UserService>({})
 
   let tasksController: TasksController
 
+  const token = 'SOME_TOKEN'
+
   beforeEach(() => {
     jest.resetAllMocks()
-    tasksController = new TasksController(tasksService)
+    tasksController = new TasksController(taskService, applicationService, userService)
   })
 
   describe('index', () => {
     it('should render the tasks template', async () => {
       const tasks = taskFactory.buildList(1)
-      tasksService.getAll.mockResolvedValue(tasks)
+      taskService.getAll.mockResolvedValue(tasks)
 
       const requestHandler = tasksController.index()
 
@@ -32,6 +46,54 @@ describe('TasksController', () => {
       expect(response.render).toHaveBeenCalledWith('tasks/index', {
         pageHeading: 'Tasks',
         tasks: groupByAllocation(tasks),
+      })
+    })
+  })
+
+  describe('show', () => {
+    const application = applicationFactory.build()
+    const users = userFactory.buildList(3)
+    const qualifications = ['foo', 'bar']
+
+    beforeEach(() => {
+      applicationService.findApplication.mockResolvedValue(application)
+      userService.getUsers.mockResolvedValue(users)
+      ;(getQualificationsForApplication as jest.Mock).mockReturnValue(qualifications)
+    })
+
+    it('fetches the application and a list of qualified users', async () => {
+      ;(fetchErrorsAndUserInput as jest.Mock).mockReturnValue({ errors: {}, errorSummary: [], userInput: {} })
+
+      const requestHandler = tasksController.show()
+
+      await requestHandler(request, response, next)
+
+      expect(response.render).toHaveBeenCalledWith('tasks/allocations/show', {
+        pageHeading: `Task for allocation`,
+        application,
+        users,
+        errors: {},
+        errorSummary: [],
+      })
+
+      expect(applicationService.findApplication).toHaveBeenCalledWith(request.user.token, request.params.id)
+      expect(userService.getUsers).toHaveBeenCalledWith(request.user.token, ['assessor'], qualifications)
+    })
+
+    it('renders the form with errors and user input if an error has been sent to the flash', async () => {
+      const errorsAndUserInput = createMock<ErrorsAndUserInput>()
+      ;(fetchErrorsAndUserInput as jest.Mock).mockReturnValue(errorsAndUserInput)
+
+      const requestHandler = tasksController.show()
+      await requestHandler(request, response, next)
+
+      expect(response.render).toHaveBeenCalledWith('tasks/allocations/show', {
+        pageHeading: `Task for allocation`,
+        application,
+        users,
+        errors: errorsAndUserInput.errors,
+        errorSummary: errorsAndUserInput.errorSummary,
+        ...errorsAndUserInput.userInput,
       })
     })
   })
--- server/controllers/tasksController.ts
+++ server/controllers/tasksController.ts
@@ -1,15 +1,43 @@
 import type { Request, Response, TypedRequestHandler } from 'express'
-import TaskService from '../services/taskService'
+import paths from '../paths/tasks'
+import { ApplicationService, TaskService, UserService } from '../services'
+import { getQualificationsForApplication } from '../utils/applications/getQualificationsForApplication'
 import { groupByAllocation } from '../utils/tasks'
+import { catchValidationErrorOrPropogate, fetchErrorsAndUserInput } from '../utils/validation'
 
 export default class TasksController {
-  constructor(private readonly taskService: TaskService) {}
+  constructor(
+    private readonly taskService: TaskService,
+    private readonly applicationService: ApplicationService,
+    private readonly userService: UserService,
+  ) {}
 
   index(): TypedRequestHandler<Request, Response> {
     return async (req: Request, res: Response) => {
       const tasks = await this.taskService.getAll(req.user.token)
 
       res.render('tasks/index', { pageHeading: 'Tasks', tasks: groupByAllocation(tasks) })
+    }
+  }
+
+  show(): TypedRequestHandler<Request, Response> {
+    return async (req: Request, res: Response) => {
+      const application = await this.applicationService.findApplication(req.user.token, req.params.id)
+      const users = await this.userService.getUsers(
+        req.user.token,
+        ['assessor'],
+        getQualificationsForApplication(application),
+      )
+      const { errors, errorSummary, userInput } = fetchErrorsAndUserInput(req)
+
+      res.render('tasks/allocations/show', {
+        pageHeading: `Task for allocation`,
+        application,
+        users,
+        errors,
+        errorSummary,
+        ...userInput,
+      })
     }
   }
 }
