c3e6cea4b0f670aad5daf6ff4fc25fc0774d6e9c
pezholio <pezholio@gmail.com>
2023-03-20 15:50:44 +0000

    Add `describeClient` helper for our contract tests
    This adds a new helper that we’ll use in our contract tests, this will
    set up some sensible defaults, as well as check at the end of each test
    that our contacts match up with what is in our OpenAPI spec.

---
+++ server/testutils/describeClient.ts
@@ -0,0 +1,26 @@
+import { pactWith } from 'jest-pact'
+import { Pact } from '@pact-foundation/pact'
+
+import config from '../config'
+
+const describeClient = (consumer: string, fn: (provider: Pact) => void) => {
+  const provider = 'API'
+  const dir = '/tmp/pacts'
+
+  describe(consumer, () => {
+    pactWith({ consumer, provider, dir }, pact => {
+      beforeEach(() => {
+        config.apis.approvedPremises.url = pact.mockService.baseUrl
+      })
+
+      fn(pact)
+    })
+
+    it('meets the contract for the service', () => {
+      const pactPath = `${dir}/${consumer}-${provider}.json`
+      expect(pactPath).toMatchOpenAPISpec()
+    })
+  })
+}
+
+export default describeClient
--- server/testutils/jest.setup.ts
+++ server/testutils/jest.setup.ts
@@ -1,10 +1,13 @@
 /* eslint-disable @typescript-eslint/no-namespace */
+import { execSync } from 'child_process'
+
 export {}
 
 declare global {
   namespace jest {
     interface Matchers<R> {
       toMatchStringIgnoringWhitespace(expected: string): R
+      toMatchOpenAPISpec(): R
     }
   }
 }
@@ -18,6 +21,28 @@ expect.extend({
       message: pass
         ? () => `expected ${received} not to match ${expected}`
         : () => `expected ${received} to match ${expected}`,
+    }
+  },
+  toMatchOpenAPISpec(pactPath) {
+    const openAPIUrl =
+      'https://raw.githubusercontent.com/ministryofjustice/hmpps-approved-premises-api/main/src/main/resources/static/api.yml'
+
+    try {
+      execSync(`
+        if [ ! -f /tmp/api.yml ]; then
+          curl -s "${openAPIUrl}" > /tmp/api.yml
+        fi
+      `)
+      execSync(`npx swagger-mock-validator /tmp/api.yml ${pactPath}`)
+      return {
+        message: () => `Swagger mock validator for ${pactPath} did not fail`,
+        pass: true,
+      }
+    } catch (err) {
+      return {
+        message: () => err.output.toString(),
+        pass: false,
+      }
     }
   },
 })
