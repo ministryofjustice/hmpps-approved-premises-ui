ae9008b442c37c7c5daee488ae6df27511114730
pezholio <pezholio@gmail.com>
2023-03-21 14:05:49 +0000

    Use contract tests for the PremisesClient

--- server/data/premisesClient.test.ts
+++ server/data/premisesClient.test.ts
@@ -1,41 +1,38 @@
-import nock from 'nock'
-
 import premisesFactory from '../testutils/factories/premises'
 import PremisesClient from './premisesClient'
-import config from '../config'
 import paths from '../paths/api'
 import dateCapacityFactory from '../testutils/factories/dateCapacity'
 import staffMemberFactory from '../testutils/factories/staffMember'
+import describeClient from '../testutils/describeClient'
 
-describe('PremisesClient', () => {
-  let fakeApprovedPremisesApi: nock.Scope
+describeClient('PremisesClient', provider => {
   let premisesClient: PremisesClient
 
   const token = 'token-1'
 
   beforeEach(() => {
-    config.apis.approvedPremises.url = 'http://localhost:8080'
-    fakeApprovedPremisesApi = nock(config.apis.approvedPremises.url)
     premisesClient = new PremisesClient(token)
-  })
-
-  afterEach(() => {
-    if (!nock.isDone()) {
-      nock.cleanAll()
-      throw new Error('Not all nock interceptors were used!')
-    }
-    nock.abortPendingRequests()
-    nock.cleanAll()
   })
 
   describe('all', () => {
     const premises = premisesFactory.buildList(5)
 
     it('should get all premises', async () => {
-      fakeApprovedPremisesApi
-        .get(paths.premises.index({}))
-        .matchHeader('authorization', `Bearer ${token}`)
-        .reply(200, premises)
+      provider.addInteraction({
+        state: 'Server is healthy',
+        uponReceiving: 'A request to get all premises',
+        withRequest: {
+          method: 'GET',
+          path: paths.premises.index({}),
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        },
+        willRespondWith: {
+          status: 200,
+          body: premises,
+        },
+      })
 
       const output = await premisesClient.all()
       expect(output).toEqual(premises)
@@ -46,10 +43,21 @@ describe('PremisesClient', () => {
     const premises = premisesFactory.build()
 
     it('should get a single premises', async () => {
-      fakeApprovedPremisesApi
-        .get(paths.premises.show({ premisesId: premises.id }))
-        .matchHeader('authorization', `Bearer ${token}`)
-        .reply(200, premises)
+      provider.addInteraction({
+        state: 'Server is healthy',
+        uponReceiving: 'A request to get a single premises',
+        withRequest: {
+          method: 'GET',
+          path: paths.premises.show({ premisesId: premises.id }),
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        },
+        willRespondWith: {
+          status: 200,
+          body: premises,
+        },
+      })
 
       const output = await premisesClient.find(premises.id)
       expect(output).toEqual(premises)
@@ -58,16 +66,27 @@ describe('PremisesClient', () => {
 
   describe('capacity', () => {
     const premisesId = 'premisesId'
-    const premisesCapacityItem = dateCapacityFactory.build()
+    const premisesCapacityItems = dateCapacityFactory.buildList(5)
 
     it('should get the capacity of a premises for a given date', async () => {
-      fakeApprovedPremisesApi
-        .get(paths.premises.capacity({ premisesId }))
-        .matchHeader('authorization', `Bearer ${token}`)
-        .reply(200, premisesCapacityItem)
+      provider.addInteraction({
+        state: 'Server is healthy',
+        uponReceiving: 'A request to get the capacity for a single premises',
+        withRequest: {
+          method: 'GET',
+          path: paths.premises.capacity({ premisesId }),
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        },
+        willRespondWith: {
+          status: 200,
+          body: premisesCapacityItems,
+        },
+      })
 
       const output = await premisesClient.capacity(premisesId)
-      expect(output).toEqual(premisesCapacityItem)
+      expect(output).toEqual(premisesCapacityItems)
     })
   })
 
@@ -76,10 +95,21 @@ describe('PremisesClient', () => {
     const staffMembers = staffMemberFactory.buildList(5)
 
     it('should return a list of staff members', async () => {
-      fakeApprovedPremisesApi
-        .get(paths.premises.staffMembers.index({ premisesId: premises.id }))
-        .matchHeader('authorization', `Bearer ${token}`)
-        .reply(200, staffMembers)
+      provider.addInteraction({
+        state: 'Server is healthy',
+        uponReceiving: 'A request to get staff members for a single premises',
+        withRequest: {
+          method: 'GET',
+          path: paths.premises.staffMembers.index({ premisesId: premises.id }),
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        },
+        willRespondWith: {
+          status: 200,
+          body: staffMembers,
+        },
+      })
 
       const output = await premisesClient.getStaffMembers(premises.id)
       expect(output).toEqual(staffMembers)
