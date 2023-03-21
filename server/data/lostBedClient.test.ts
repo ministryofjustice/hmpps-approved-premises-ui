346782d23d55f16fe166bf02020ee4b4a3d45a10
pezholio <pezholio@gmail.com>
2023-03-21 14:05:28 +0000

    Use contract tests for the LostBedClient
    In the body, we have a nullable cancellation object, so in the
    interests of expediance, we return a cancellation object too, so the
    contract matches.

--- server/data/lostBedClient.test.ts
+++ server/data/lostBedClient.test.ts
@@ -1,45 +1,44 @@
-import nock from 'nock'
-
 import LostBedClient from './lostBedClient'
-import config from '../config'
 import lostBedFactory from '../testutils/factories/lostBed'
 import newLostBedFactory from '../testutils/factories/newLostBed'
+import describeClient from '../testutils/describeClient'
 
-describe('LostBedClient', () => {
-  let fakeApprovedPremisesApi: nock.Scope
+describeClient('LostBedClient', provider => {
   let lostBedClient: LostBedClient
 
   const token = 'token-1'
 
   beforeEach(() => {
-    config.apis.approvedPremises.url = 'http://localhost:8080'
-    fakeApprovedPremisesApi = nock(config.apis.approvedPremises.url)
     lostBedClient = new LostBedClient(token)
-  })
-
-  afterEach(() => {
-    if (!nock.isDone()) {
-      nock.cleanAll()
-      throw new Error('Not all nock interceptors were used!')
-    }
-    nock.abortPendingRequests()
-    nock.cleanAll()
   })
 
   describe('create', () => {
     it('should create a lostBed', async () => {
-      const lostBed = lostBedFactory.build()
+      const lostBed = lostBedFactory.build({
+        cancellation: {},
+      })
       const newLostBed = newLostBedFactory.build()
 
-      fakeApprovedPremisesApi
-        .post(`/premises/premisesId/lost-beds`, newLostBed)
-        .matchHeader('authorization', `Bearer ${token}`)
-        .reply(201, lostBed)
+      provider.addInteraction({
+        state: 'Server is healthy',
+        uponReceiving: 'A request to create a lost bed',
+        withRequest: {
+          method: 'POST',
+          path: `/premises/premisesId/lost-beds`,
+          body: newLostBed,
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        },
+        willRespondWith: {
+          status: 200,
+          body: lostBed,
+        },
+      })
 
       const result = await lostBedClient.create('premisesId', newLostBed)
 
       expect(result).toEqual(lostBed)
-      expect(nock.isDone()).toBeTruthy()
     })
   })
 })
