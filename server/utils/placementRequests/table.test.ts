ac2d51e6c378c71889ad9affbb19a2c72403249e
rich <rich.james@dxw.com>
2023-03-21 08:52:12 +0000

    Add utils to create table view for placement requests

---
+++ server/utils/placementRequests/table.test.ts
@@ -0,0 +1,51 @@
+import { add } from 'date-fns'
+import placementRequestFactory from '../../testutils/factories/placementRequest'
+import { DateFormats } from '../dateUtils'
+import { mentalHealthSupportCell, placementCriteriaClasses, tableRows } from './table'
+import { sentenceCase } from '../utils'
+
+describe('placementRequestUtils', () => {
+  describe('tableRows', () => {
+    it('returns the table rows', () => {
+      const placementRequests = placementRequestFactory.buildList(1)
+
+      expect(tableRows(placementRequests)).toEqual([
+        [
+          { text: placementRequests[0].person.name },
+          { text: placementRequests[0].person.crn },
+          {
+            text: DateFormats.differenceInDays(
+              DateFormats.isoToDateObj(placementRequests[0].expectedArrival),
+              add(DateFormats.isoToDateObj(placementRequests[0].expectedArrival), { days: 7 }),
+            ).ui,
+          },
+          { text: DateFormats.isoDateToUIDate(placementRequests[0].expectedArrival) },
+          { text: placementRequests[0].location },
+          {
+            html: `<ul class=${placementCriteriaClasses}>${placementRequests[0].essentialCriteria
+              .map(placementNeed => `<li>${sentenceCase(placementNeed)}</li>`)
+              .join('')}</ul>`,
+          },
+          {
+            html: `<ul class=${placementCriteriaClasses}>${placementRequests[0].desirableCriteria
+              .map(placementNeed => `<li>${sentenceCase(placementNeed)}</li>`)
+              .join('')}</ul>`,
+          },
+        ],
+      ])
+    })
+  })
+
+  describe('mentalHealthSupportCell', () => {
+    it('returns Yes if the value is true', () => {
+      const placementRequest = placementRequestFactory.build({ mentalHealthSupport: true })
+
+      expect(mentalHealthSupportCell(placementRequest)).toEqual({ text: 'Yes' })
+    })
+    it('returns No if the value is false', () => {
+      const placementRequest = placementRequestFactory.build({ mentalHealthSupport: false })
+
+      expect(mentalHealthSupportCell(placementRequest)).toEqual({ text: 'No' })
+    })
+  })
+})
