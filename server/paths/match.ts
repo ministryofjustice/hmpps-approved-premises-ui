6dc84bdaf0fb5288601844f770a37eac5cc461e4
rich <rich.james@dxw.com>
2023-03-23 14:32:00 +0000

    Add bedsController
    Rather than accept all the details of a Person through the query string
    I thought it better to just accept their CRN and perform a lookup
    using that. Otherwise we could end up with very, very long query strings

--- server/paths/match.ts
+++ server/paths/match.ts
@@ -1,9 +1,13 @@
 import { path } from 'static-path'
 
 const placementRequestsPath = path('/placement-requests')
+const bedsPath = path('/beds')
 
 export default {
   placementRequests: {
     index: placementRequestsPath,
+  },
+  beds: {
+    search: bedsPath.path('/search'),
   },
 }
