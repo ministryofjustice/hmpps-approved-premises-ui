0990e8ad8ba9801d6d6525cf8fd6158cef970aeb
pezholio <pezholio@gmail.com>
2023-03-30 10:47:35 +0100

    Update the tasksController
    Now we no longer have to use the frontend to get the qualified users, we
    can make this logic much simpler, and also get rid of the
    `getQualificationsForApplication` helper

--- server/controllers/tasksController.ts
+++ server/controllers/tasksController.ts
@@ -1,16 +1,11 @@
 import type { Request, Response, TypedRequestHandler } from 'express'
 import { convertToTitleCase, sentenceCase } from '../utils/utils'
-import { ApplicationService, TaskService, UserService } from '../services'
-import { getQualificationsForApplication } from '../utils/applications/getQualificationsForApplication'
+import { ApplicationService, TaskService } from '../services'
 import { groupByAllocation } from '../utils/tasks'
 import { fetchErrorsAndUserInput } from '../utils/validation'
 
 export default class TasksController {
-  constructor(
-    private readonly taskService: TaskService,
-    private readonly applicationService: ApplicationService,
-    private readonly userService: UserService,
-  ) {}
+  constructor(private readonly taskService: TaskService, private readonly applicationService: ApplicationService) {}
 
   index(): TypedRequestHandler<Request, Response> {
     return async (req: Request, res: Response) => {
@@ -22,13 +17,8 @@ export default class TasksController {
 
   show(): TypedRequestHandler<Request, Response> {
     return async (req: Request, res: Response) => {
-      const task = await this.taskService.find(req.user.token, req.params.id, req.params.taskType)
+      const { task, users } = await this.taskService.find(req.user.token, req.params.id, req.params.taskType)
       const application = await this.applicationService.findApplication(req.user.token, req.params.id)
-      const users = await this.userService.getUsers(
-        req.user.token,
-        ['assessor'],
-        getQualificationsForApplication(application),
-      )
       const { errors, errorSummary, userInput } = fetchErrorsAndUserInput(req)
 
       res.render('tasks/show', {
