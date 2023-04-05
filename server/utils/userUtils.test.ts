bdb7c96233f2a419623f33f4d9aedb0536d54bae
pezholio <pezholio@gmail.com>
2023-04-05 09:15:34 +0100

    Add a `sectionsForUser` method
    This will act as the source of truth for all the actions a user can
    carry out.

--- server/utils/userUtils.test.ts
+++ server/utils/userUtils.test.ts
@@ -1,5 +1,5 @@
 import { userDetailsFactory } from '../testutils/factories'
-import { hasRole } from './userUtils'
+import { hasRole, sections, sectionsForUser } from './userUtils'
 
 describe('userUtils', () => {
   describe('hasRole', () => {
@@ -13,6 +13,44 @@ describe('userUtils', () => {
       const user = userDetailsFactory.build({ roles: ['assessor'] })
 
       expect(hasRole(user, 'applicant')).toEqual(false)
+    })
+  })
+
+  describe('sectionsForUser', () => {
+    it('should return Apply for a user with no roles', () => {
+      const user = userDetailsFactory.build({ roles: [] })
+
+      expect(sectionsForUser(user)).toEqual([sections.apply])
+    })
+
+    it('should return Apply and Assess sections for a user with an assessor role', () => {
+      const user = userDetailsFactory.build({ roles: ['assessor'] })
+
+      expect(sectionsForUser(user)).toEqual([sections.apply, sections.assess])
+    })
+
+    it('should return Apply and Manage sections for a user with a manager role', () => {
+      const user = userDetailsFactory.build({ roles: ['manager'] })
+
+      expect(sectionsForUser(user)).toEqual([sections.apply, sections.manage])
+    })
+
+    it('should return Apply and Workflow sections for a user with a workflow manager role', () => {
+      const user = userDetailsFactory.build({ roles: ['workflow_manager'] })
+
+      expect(sectionsForUser(user)).toEqual([sections.apply, sections.workflow])
+    })
+
+    it('should return Apply and Match sections for a user with a matcher role', () => {
+      const user = userDetailsFactory.build({ roles: ['matcher'] })
+
+      expect(sectionsForUser(user)).toEqual([sections.apply, sections.match])
+    })
+
+    it('should return all sections for a user with all roles', () => {
+      const user = userDetailsFactory.build({ roles: ['assessor', 'manager', 'matcher', 'workflow_manager'] })
+
+      expect(sectionsForUser(user)).toEqual(Object.values(sections))
     })
   })
 })
