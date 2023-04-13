e1ce99ab5341281489e1fa3a0bdde0decd2f95fe
pezholio <pezholio@gmail.com>
2023-04-13 11:31:20 +0100

    Add a searchButton util
    This is basically what we have in the table list. We will iterate on
    this so search sits under the placement request, but this will keep the
    tests passing without having to get too much in the weeds.

--- server/utils/placementRequests/utils.test.ts
+++ server/utils/placementRequests/utils.test.ts
@@ -1,11 +1,26 @@
 import { placementRequestFactory } from '../../testutils/factories'
-import { formatReleaseType } from './utils'
+import { createQueryString } from '../utils'
+import { mapPlacementRequestToBedSearchParams } from './table'
+import { formatReleaseType, searchButton } from './utils'
+import paths from '../../paths/match'
 
 describe('utils', () => {
   describe('formatReleaseType', () => {
     it('formats a release type in a human-readable format', () => {
       const placementRequest = placementRequestFactory.build({ releaseType: 'rotl' })
       expect(formatReleaseType(placementRequest)).toEqual('Release on Temporary Licence (ROTL)')
+    })
+  })
+
+  describe('searchButton', () => {
+    it('returns a link to the search query', () => {
+      const placementRequest = placementRequestFactory.build()
+
+      expect(searchButton(placementRequest)).toEqual(
+        `<a class="govuk-button" href="${paths.beds.search({})}?${createQueryString(
+          mapPlacementRequestToBedSearchParams(placementRequest),
+        )}">Search</a>`,
+      )
     })
   })
 })
--- server/utils/placementRequests/utils.ts
+++ server/utils/placementRequests/utils.ts
@@ -1,4 +1,12 @@
 import { PlacementRequest } from '@approved-premises/api'
 import { allReleaseTypes } from '../applications/releaseTypeUtils'
+import { mapPlacementRequestToBedSearchParams } from './table'
+import { createQueryString } from '../utils'
+import paths from '../../paths/match'
 
 export const formatReleaseType = (placementRequest: PlacementRequest) => allReleaseTypes[placementRequest.releaseType]
+
+export const searchButton = (placementRequest: PlacementRequest) =>
+  `<a class="govuk-button" href="${paths.beds.search({})}?${createQueryString(
+    mapPlacementRequestToBedSearchParams(placementRequest),
+  )}">Search</a>`
