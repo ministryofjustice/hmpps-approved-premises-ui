6c997cf12af8096658ffa9fcc7fe11de2753ec40
pezholio <pezholio@gmail.com>
2023-04-13 14:44:52 +0100

    Update ESAP ineligible copy

--- integration_tests/pages/apply/esapNotEligible.ts
+++ integration_tests/pages/apply/esapNotEligible.ts
@@ -3,6 +3,6 @@ import Page from '../page'
 
 export default class EsapNotEligible extends Page {
   constructor(application: Application) {
-    super(`${application.person.name} is not eligible for an ESAP placement`)
+    super(`${application.person.name} is not eligible for an Enhanced Security Approved Premises (ESAP) placement.`)
   }
 }
