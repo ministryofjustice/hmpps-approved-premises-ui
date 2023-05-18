e763d713b26af51761833bf2d867f2b1de2b9f19
pezholio <pezholio@gmail.com>
2023-05-18 10:33:55 +0100

    Add new fields to matching info view
    Also update the integration tests

--- integration_tests/pages/assess/matchingInformationPage.ts
+++ integration_tests/pages/assess/matchingInformationPage.ts
@@ -4,25 +4,30 @@ import AssessPage from './assessPage'
 import { offenceAndRiskOptions, placementRequirementOptions } from '../../../server/utils/placementCriteriaUtils'
 
 export default class MatchingInformationPage extends AssessPage {
-  pageClass = new MatchingInformation({
-    apType: 'isEsap',
-    accessibilityCriteria: ['hasBrailleSignage'],
-    specialistSupportCriteria: ['isSemiSpecialistMentalHealth'],
-    isArsonDesignated: 'essential',
-    isWheelchairDesignated: 'essential',
-    isSingleRoom: 'desirable',
-    isStepFreeDesignated: 'desirable',
-    isCatered: 'notRelevant',
-    isGroundFloor: 'notRelevant',
-    hasEnSuite: 'notRelevant',
-    isSuitableForVulnerable: 'relevant',
-    acceptsSexOffenders: 'relevant',
-    acceptsChildSexOffenders: 'relevant',
-    acceptsNonSexualChildOffenders: 'relevant',
-    acceptsHateCrimeOffenders: 'relevant',
-    isArsonSuitable: 'relevant',
-    isSuitedForSexOffenders: 'essential',
-  })
+  pageClass = new MatchingInformation(
+    {
+      apType: 'isEsap',
+      accessibilityCriteria: ['hasBrailleSignage'],
+      specialistSupportCriteria: ['isSemiSpecialistMentalHealth'],
+      isArsonDesignated: 'essential',
+      isWheelchairDesignated: 'essential',
+      isSingleRoom: 'desirable',
+      isStepFreeDesignated: 'desirable',
+      isCatered: 'notRelevant',
+      isGroundFloor: 'notRelevant',
+      hasEnSuite: 'notRelevant',
+      isSuitableForVulnerable: 'relevant',
+      acceptsSexOffenders: 'relevant',
+      acceptsChildSexOffenders: 'relevant',
+      acceptsNonSexualChildOffenders: 'relevant',
+      acceptsHateCrimeOffenders: 'relevant',
+      isArsonSuitable: 'relevant',
+      isSuitedForSexOffenders: 'essential',
+      lengthOfStayAgreed: 'yes',
+      cruInformation: 'Some info',
+    },
+    this.assessment,
+  )
 
   constructor(assessment: Assessment) {
     super(assessment, 'Matching information')
@@ -46,5 +51,9 @@ export default class MatchingInformationPage extends AssessPage {
     Object.keys(offenceAndRiskOptions).forEach(offenceAndRiskInformationKey => {
       this.checkRadioByNameAndValue(offenceAndRiskInformationKey, this.pageClass.body[offenceAndRiskInformationKey])
     })
+
+    this.checkRadioByNameAndValue('lengthOfStayAgreed', this.pageClass.body.lengthOfStayAgreed)
+
+    this.completeTextArea('cruInformation', this.pageClass.body.cruInformation)
   }
 }
