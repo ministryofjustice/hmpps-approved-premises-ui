0936df73eae18845bdd5f42719f3b36fdccf8e3e
rich <rich.james@dxw.com>
2023-05-30 13:28:05 +0100

    Add Placement Application Service
    As with Assessments and Applications we need methods to initialize and save
    each page of the form, as well as one to create the Placement Application
    at the beginning of the journey

---
+++ server/services/placementApplicationService.ts
@@ -0,0 +1,63 @@
+import { DataServices } from '@approved-premises/ui'
+import type { Request } from 'express'
+import { PlacementApplication } from '@approved-premises/api'
+import { RestClientBuilder } from '../data'
+import PlacementApplicationClient from '../data/placementApplicationClient'
+import TasklistPage, { TasklistPageInterface } from '../form-pages/tasklistPage'
+import { getBody, getPageName, getTaskName } from '../form-pages/utils'
+import { ValidationError } from '../utils/errors'
+
+export default class PlacementApplicationService {
+  constructor(private readonly placementApplicationClientFactory: RestClientBuilder<PlacementApplicationClient>) {}
+
+  async getPlacementApplication(token: string, id: string): Promise<PlacementApplication> {
+    const placementApplicationClient = this.placementApplicationClientFactory(token)
+
+    return placementApplicationClient.find(id)
+  }
+
+  async create(token: string, applicationId: string): Promise<PlacementApplication> {
+    const placementApplicationClient = this.placementApplicationClientFactory(token)
+    return placementApplicationClient.create(applicationId)
+  }
+
+  async initializePage(
+    Page: TasklistPageInterface,
+    request: Request,
+    dataServices: DataServices,
+    userInput?: Record<string, unknown>,
+  ): Promise<TasklistPage> {
+    const placementApplication = await this.getPlacementApplication(request.user.token, request.params.id)
+
+    const body = getBody(Page, placementApplication, request, userInput)
+
+    const page = Page.initialize
+      ? await Page.initialize(body, placementApplication, request.user.token, dataServices)
+      : new Page(body, placementApplication, request.session.previousPage)
+
+    return page
+  }
+
+  async save(page: TasklistPage, request: Request) {
+    const errors = page.errors()
+
+    if (Object.keys(errors).length) {
+      throw new ValidationError<typeof page>(errors)
+    } else {
+      const placementApplication = (await this.getPlacementApplication(
+        request.user.token,
+        request.params.id,
+      )) as PlacementApplication
+      const client = this.placementApplicationClientFactory(request.user.token)
+
+      const pageName = getPageName(page.constructor)
+      const taskName = getTaskName(page.constructor)
+
+      placementApplication.data = placementApplication.data || {}
+      placementApplication.data[taskName] = placementApplication.data[taskName] || {}
+      placementApplication.data[taskName][pageName] = page.body
+
+      await client.update(placementApplication)
+    }
+  }
+}
