07faa094a176239d12e3019bf8631d39272cce4a
pezholio <pezholio@gmail.com>
2023-05-30 11:25:35 +0100

    Update placementRequestData
    This updates the placementRequestData function to return all of the
    acceptance data, as well as the document and the notes. We only
    send placment dates if the start date is known (i.e. it’s not a parole
    or ROTL case)

--- server/utils/assessments/acceptanceData.test.ts
+++ server/utils/assessments/acceptanceData.test.ts
@@ -1,71 +1,107 @@
 import { createMock } from '@golevelup/ts-jest'
 import { mockOptionalQuestionResponse, mockQuestionResponse } from '../../testutils/mockQuestionResponse'
 import { MatchingInformationBody } from '../../form-pages/assess/matchingInformation/matchingInformationTask/matchingInformation'
-import { criteriaFromMatchingInformation, placementRequestData } from './placementRequestData'
+import { acceptanceData, criteriaFromMatchingInformation, placementDates, placementRequestData } from './acceptanceData'
 import { assessmentFactory } from '../../testutils/factories'
 import { pageDataFromApplicationOrAssessment } from '../../form-pages/utils'
 import { arrivalDateFromApplication } from '../applications/arrivalDateFromApplication'
 import { placementDurationFromApplication } from './placementDurationFromApplication'
+import { getResponses } from '../applications/utils'
 
 jest.mock('../../form-pages/utils')
 jest.mock('../retrieveQuestionResponseFromApplicationOrAssessment')
 jest.mock('../applications/arrivalDateFromApplication')
 jest.mock('./placementDurationFromApplication')
 
-describe('placementRequestData', () => {
+describe('acceptanceData', () => {
   const assessment = assessmentFactory.build()
-  const expectedArrival = '2020-01-01'
 
-  let matchingInformation = createMock<MatchingInformationBody>({
-    apType: 'isESAP',
-    specialistSupportCriteria: [],
-    accessibilityCriteria: [],
-  })
+  describe('acceptanceData', () => {
+    it('should return the acceptance data for the assessment', () => {
+      mockOptionalQuestionResponse({ cruInformation: 'Some notes' })
 
-  ;(pageDataFromApplicationOrAssessment as jest.Mock).mockReturnValue(matchingInformation)
-  ;(arrivalDateFromApplication as jest.Mock).mockReturnValue('2020-01-01')
-
-  it('converts matching data into a placement request', () => {
-    mockQuestionResponse({ postcodeArea: 'ABC123', type: 'normal', duration: '12' })
-    mockOptionalQuestionResponse({
-      lengthOfStayAgreedDetail: '12',
-      alternativeRadius: '100',
-      cruInformation: 'Some notes',
-    })
-
-    expect(placementRequestData(assessment)).toEqual({
-      gender: 'male',
-      type: 'esap',
-      expectedArrival,
-      duration: '12',
-      location: 'ABC123',
-      radius: '100',
-      essentialCriteria: criteriaFromMatchingInformation(matchingInformation).essentialCriteria,
-      desirableCriteria: criteriaFromMatchingInformation(matchingInformation).desirableCriteria,
-      notes: 'Some notes',
+      expect(acceptanceData(assessment)).toEqual({
+        document: getResponses(assessment),
+        requirements: placementRequestData(assessment),
+        placementDates: placementDates(assessment),
+        notes: 'Some notes',
+      })
     })
   })
 
-  it('returns a default radius if one is not present', () => {
-    mockOptionalQuestionResponse({ lengthOfStayAgreedDetail: '12', alternativeRadius: undefined })
+  describe('placementDates', () => {
+    const expectedArrival = '2020-01-01'
 
-    const result = placementRequestData(assessment)
+    it('should return the placement dates if an arrival date is provided', () => {
+      ;(arrivalDateFromApplication as jest.Mock).mockReturnValue(expectedArrival)
 
-    expect(result.radius).toEqual(50)
+      mockOptionalQuestionResponse({ lengthOfStayAgreedDetail: '12' })
+
+      const result = placementDates(assessment)
+
+      expect(result.expectedArrival).toEqual(expectedArrival)
+      expect(result.duration).toEqual(12)
+    })
+
+    it('should return the default duration if a duration is not provided', () => {
+      ;(arrivalDateFromApplication as jest.Mock).mockReturnValue(expectedArrival)
+      ;(placementDurationFromApplication as jest.Mock).mockReturnValueOnce('52')
+
+      mockOptionalQuestionResponse({ lengthOfStayAgreedDetail: undefined })
+
+      const result = placementDates(assessment)
+
+      expect(result.expectedArrival).toEqual(expectedArrival)
+      expect(result.duration).toEqual(52)
+    })
+
+    it('should return null if the arrival date is not provided', () => {
+      ;(arrivalDateFromApplication as jest.Mock).mockReturnValue(undefined)
+
+      const result = placementDates(assessment)
+
+      expect(result).toBeNull()
+    })
   })
 
-  it('returns the default placement duration if one is not present', () => {
-    ;(placementDurationFromApplication as jest.Mock).mockReturnValueOnce(52)
-    mockOptionalQuestionResponse({ duration: undefined, alternativeRadius: '100' })
+  describe('placementRequestData', () => {
+    const matchingInformation = createMock<MatchingInformationBody>({
+      apType: 'isESAP',
+      specialistSupportCriteria: [],
+      accessibilityCriteria: [],
+    })
 
-    const result = placementRequestData(assessment)
+    ;(pageDataFromApplicationOrAssessment as jest.Mock).mockReturnValue(matchingInformation)
 
-    expect(result.duration).toEqual(52)
+    it('converts matching data into a placement request', () => {
+      mockQuestionResponse({ postcodeArea: 'ABC123', type: 'normal', duration: '12' })
+      mockOptionalQuestionResponse({
+        lengthOfStayAgreedDetail: '12',
+        alternativeRadius: '100',
+      })
+
+      expect(placementRequestData(assessment)).toEqual({
+        gender: 'male',
+        type: 'esap',
+        location: 'ABC123',
+        radius: '100',
+        essentialCriteria: criteriaFromMatchingInformation(matchingInformation).essentialCriteria,
+        desirableCriteria: criteriaFromMatchingInformation(matchingInformation).desirableCriteria,
+      })
+    })
+
+    it('returns a default radius if one is not present', () => {
+      mockOptionalQuestionResponse({ lengthOfStayAgreedDetail: '12', alternativeRadius: undefined })
+
+      const result = placementRequestData(assessment)
+
+      expect(result.radius).toEqual(50)
+    })
   })
 
   describe('criteriaFromMatchingInformation', () => {
     it('returns all essential criteria for essential and relevant matching information', () => {
-      matchingInformation = createMock<MatchingInformationBody>({
+      const matchingInformation = createMock<MatchingInformationBody>({
         apType: 'isESAP',
         specialistSupportCriteria: [],
         accessibilityCriteria: [],
@@ -101,7 +137,7 @@ describe('placementRequestData', () => {
     })
 
     it('returns all desirable criteria for desirable matching information', () => {
-      matchingInformation = createMock<MatchingInformationBody>({
+      const matchingInformation = createMock<MatchingInformationBody>({
         apType: 'normal',
         isWheelchairDesignated: 'desirable',
         isStepFreeDesignated: 'desirable',
@@ -126,7 +162,7 @@ describe('placementRequestData', () => {
     })
 
     it('returns empty objects for not relevant matching information', () => {
-      matchingInformation = createMock<MatchingInformationBody>({
+      const matchingInformation = createMock<MatchingInformationBody>({
         apType: 'normal',
         isWheelchairDesignated: 'notRelevant',
         isStepFreeDesignated: 'notRelevant',
