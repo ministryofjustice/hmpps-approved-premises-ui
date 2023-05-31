b422b32fa8a4c4a13231e1d42a36dba3ce10e3ca
rich <rich.james@dxw.com>
2023-05-31 11:53:44 +0100

    Add page class for 'Reason for placement' page
    This will be there first page of the form

---
+++ server/form-pages/placement-application/request-a-placement/reason-for-placement.test.ts
@@ -0,0 +1,43 @@
+import { itShouldHaveNextValue, itShouldHavePreviousValue } from '../../shared-examples'
+
+import ReasonForPlacement from './reason-for-placement'
+
+describe('ReasonForPlacement', () => {
+  describe('title', () => {
+    expect(new ReasonForPlacement({}).title).toBe('Request a placement')
+  })
+
+  describe('body', () => {
+    it('should set the body', () => {
+      const page = new ReasonForPlacement({
+        reason: 'rotl',
+      })
+      expect(page.body).toEqual({
+        reason: 'rotl',
+      })
+    })
+  })
+
+  itShouldHaveNextValue(new ReasonForPlacement({}), '')
+  itShouldHavePreviousValue(new ReasonForPlacement({}), '')
+
+  describe('errors', () => {
+    it('if no response is given an error is returned', () => {
+      expect(new ReasonForPlacement({}).errors()).toEqual({
+        reason: 'You must state the reason for placement',
+      })
+    })
+  })
+
+  describe('response', () => {
+    it("If the response is 'no' only the response is returned", () => {
+      expect(
+        new ReasonForPlacement({
+          reason: 'rotl',
+        }).response(),
+      ).toEqual({
+        'Why are you requesting a placement?': 'Release on Temporary Licence (ROTL)',
+      })
+    })
+  })
+})
