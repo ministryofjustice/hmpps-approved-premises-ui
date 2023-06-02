ec9d2b29b3b45f173c68b13115a1ea368bf13a81
rich <rich.james@dxw.com>
2023-06-02 14:50:13 +0100

    Allow application link to take placement applications as a param
    This will be used in a subsequent commit

--- server/utils/placementRequests/utils.ts
+++ server/utils/placementRequests/utils.ts
@@ -1,4 +1,4 @@
-import { PlacementRequest } from '@approved-premises/api'
+import { PlacementApplication, PlacementRequest } from '@approved-premises/api'
 import { BedSearchParametersUi } from '@approved-premises/ui'
 import { allReleaseTypes } from '../applications/releaseTypeUtils'
 import { linkTo } from '../utils'
@@ -37,8 +37,11 @@ export const searchButton = (placementRequest: PlacementRequest) =>
     { text: 'Search', attributes: { class: 'govuk-button' } },
   )
 
-export const applicationLink = (placementRequest: PlacementRequest, text: string, hiddenText: string) =>
-  linkTo(applyPaths.applications.show, { id: placementRequest.applicationId }, { text, hiddenText })
+export const applicationLink = (
+  placementRequestOrApplication: PlacementRequest | PlacementApplication,
+  text: string,
+  hiddenText: string,
+) => linkTo(applyPaths.applications.show, { id: placementRequestOrApplication.applicationId }, { text, hiddenText })
 
 export const assessmentLink = (placementRequest: PlacementRequest, text: string, hiddenText: string) =>
   linkTo(assessPaths.assessments.show, { id: placementRequest.assessmentId }, { text, hiddenText })
