3257baec0b90791f3b975cce021720086a4b7715
rich <rich.james@dxw.com>
2023-06-02 15:32:19 +0100

    Add Check Your Answers page
    This is mainly generated from the utils we added in a previous commit.
    In order to retain the state of the answers we entered we have to add the
    placementApplicationData.json and return that from the mockApi stub

--- integration_tests/tests/match/placementApplications.cy.ts
+++ integration_tests/tests/match/placementApplications.cy.ts
@@ -5,6 +5,10 @@ import PreviousRotlPlacement from '../../pages/match/placementRequestForm/previo
 import ReasonForPlacementPage from '../../pages/match/placementRequestForm/reasonForPlacement'
 import SameAp from '../../pages/match/placementRequestForm/sameAp'
 import UpdatesToApplication from '../../pages/match/placementRequestForm/updatesToApplication'
+import CheckYourAnswers from '../../pages/match/placementRequestForm/checkYourAnswers'
+import ConfirmationPage from '../../pages/match/placementRequestForm/confirmationPage'
+import Page from '../../pages/page'
+import paths from '../../../server/paths/api'
 
 context('Placement Applications', () => {
   beforeEach(() => {
@@ -16,47 +20,55 @@ context('Placement Applications', () => {
   beforeEach(() => {
     cy.signIn()
   })
-
   it('allows me to complete form', () => {
-    // Given I have completed an application I am viewing a completed application
-    const completedApplication = applicationFactory.build({ status: 'submitted', id: '123' })
-    cy.task('stubApplicationGet', { application: completedApplication })
-    cy.task('stubApplications', [completedApplication])
+    cy.fixture('placementApplicationData.json').then(placementApplicationData => {
+      // Given I have completed an application I am viewing a completed application
+      const completedApplication = applicationFactory.build({ status: 'submitted', id: '123' })
+      cy.task('stubApplicationGet', { application: completedApplication })
+      cy.task('stubApplications', [completedApplication])
 
-    // And there is a placement application in the DB
-    const placementApplicationId = '123'
-    const placementApplication = placementApplicationFactory.build({ id: placementApplicationId })
-    cy.task('stubCreatePlacementApplication', placementApplication)
-    cy.task('stubPlacementApplicationUpdate', placementApplication)
-    cy.task('stubPlacementApplication', placementApplication)
+      // And there is a placement application in the DB
+      const placementApplicationId = '123'
+      const placementApplication = placementApplicationFactory.build({
+        id: placementApplicationId,
+        data: placementApplicationData,
+      })
+      cy.task('stubCreatePlacementApplication', placementApplication)
+      cy.task('stubPlacementApplicationUpdate', placementApplication)
+      cy.task('stubSubmitPlacementApplication', placementApplication)
+      cy.task('stubPlacementApplication', placementApplication)
 
-    // When I visit the readonly application view
-    const showPage = ShowPage.visit(completedApplication)
+      // When I visit the readonly application view
+      const showPage = ShowPage.visit(completedApplication)
 
-    // Then I should be able to click submit
-    showPage.clickSubmit()
+      // Then I should be able to click submit
+      showPage.clickSubmit()
 
-    // When I visit the placement request page
-    const placementReasonPage = ReasonForPlacementPage.visit(placementApplicationId)
+      // Given I am on the placement application form and start and application
+      const placementReasonPage = ReasonForPlacementPage.visit(placementApplicationId)
 
-    // Then I can complete the form
-    placementReasonPage.completeForm()
-    placementReasonPage.clickSubmit()
+      // When I complete the form
+      placementReasonPage.completeForm()
+      placementReasonPage.clickSubmit()
 
-    const previousRotlPlacementPage = new PreviousRotlPlacement()
-    previousRotlPlacementPage.completeForm()
-    previousRotlPlacementPage.clickSubmit()
+      const previousRotlPlacementPage = new PreviousRotlPlacement()
+      previousRotlPlacementPage.completeForm()
+      previousRotlPlacementPage.clickSubmit()
 
-    const sameApPage = new SameAp()
-    sameApPage.completeForm()
-    sameApPage.clickSubmit()
+      const sameApPage = new SameAp()
+      sameApPage.completeForm()
+      sameApPage.clickSubmit()
 
-    const datesOfPlacementPage = new DateOfPlacement()
-    datesOfPlacementPage.completeForm()
-    datesOfPlacementPage.clickSubmit()
+      const datesOfPlacementPage = new DateOfPlacement()
+      datesOfPlacementPage.completeForm()
+      datesOfPlacementPage.clickSubmit()
 
-    const updatesToApplication = new UpdatesToApplication()
-    updatesToApplication.completeForm()
-    updatesToApplication.clickSubmit()
+      const updatesToApplication = new UpdatesToApplication()
+      updatesToApplication.completeForm()
+      updatesToApplication.clickSubmit()
+
+      const checkYourAnswersPage = new CheckYourAnswers()
+      checkYourAnswersPage.completeForm()
+      checkYourAnswersPage.clickSubmit()
   })
 })
