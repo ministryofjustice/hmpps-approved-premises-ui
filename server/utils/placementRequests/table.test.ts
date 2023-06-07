d321ee6a6bbf65b5f5deae8f5ec6f411f1e5e66f
pezholio <pezholio@gmail.com>
2023-06-07 14:43:34 +0100

    Update table helpers
    This is mainly just renaming and changing the expected types, as
    the tasks contain the same subset of fields we’re using here.

--- server/utils/placementRequests/table.test.ts
+++ server/utils/placementRequests/table.test.ts
@@ -1,5 +1,5 @@
 import { add } from 'date-fns'
-import { placementRequestFactory, tierEnvelopeFactory } from '../../testutils/factories'
+import { placementRequestTaskFactory, tierEnvelopeFactory } from '../../testutils/factories'
 import { crnCell, dueDateCell, expectedArrivalDateCell, nameCell, releaseTypeCell, tableRows, tierCell } from './table'
 import { tierBadge } from '../personUtils'
 import { DateFormats } from '../dateUtils'
@@ -8,36 +8,36 @@ import { allReleaseTypes } from '../applications/releaseTypeUtils'
 describe('tableUtils', () => {
   describe('nameCell', () => {
     it('returns the name of the service user and a link', () => {
-      const placementRequest = placementRequestFactory.build()
+      const task = placementRequestTaskFactory.build()
 
-      expect(nameCell(placementRequest)).toEqual({
-        html: `<a href="/placement-requests/${placementRequest.id}" data-cy-placementRequestId="${placementRequest.id}">${placementRequest.person.name}</a>`,
+      expect(nameCell(task)).toEqual({
+        html: `<a href="/placement-requests/${task.id}" data-cy-placementRequestId="${task.id}">${task.person.name}</a>`,
       })
     })
   })
 
   describe('crnCell', () => {
     it('returns the CRN of the service user associated with the placement request', () => {
-      const placementRequest = placementRequestFactory.build()
+      const task = placementRequestTaskFactory.build()
 
-      expect(crnCell(placementRequest)).toEqual({ text: placementRequest.person.crn })
+      expect(crnCell(task)).toEqual({ text: task.person.crn })
     })
   })
 
   describe('tierCell', () => {
     it('returns the tier badge for the service user associated with the placement request', () => {
       const tier = tierEnvelopeFactory.build({ value: { level: 'A1' } })
-      const placementRequest = placementRequestFactory.build({ risks: { tier } })
+      const task = placementRequestTaskFactory.build({ risks: { tier } })
 
-      expect(tierCell(placementRequest)).toEqual({ html: tierBadge('A1') })
+      expect(tierCell(task)).toEqual({ html: tierBadge('A1') })
     })
   })
 
   describe('expectedArrivalDateCell', () => {
     it('returns a formatted arrival date', () => {
-      const placementRequest = placementRequestFactory.build({ expectedArrival: '2022-01-01' })
+      const task = placementRequestTaskFactory.build({ expectedArrival: '2022-01-01' })
 
-      expect(expectedArrivalDateCell(placementRequest)).toEqual({
+      expect(expectedArrivalDateCell(task)).toEqual({
         text: DateFormats.isoDateToUIDate('2022-01-01'),
       })
     })
@@ -46,11 +46,11 @@ describe('tableUtils', () => {
   describe('dueDateCell', () => {
     it('returns the difference in days between the arrival date and the due date', () => {
       const arrivalDate = add(new Date(), { days: 14 })
-      const placementRequest = placementRequestFactory.build({
+      const task = placementRequestTaskFactory.build({
         expectedArrival: DateFormats.dateObjToIsoDate(arrivalDate),
       })
 
-      expect(dueDateCell(placementRequest, 7)).toEqual({
+      expect(dueDateCell(task, 7)).toEqual({
         text: '7 days',
       })
     })
@@ -58,24 +58,24 @@ describe('tableUtils', () => {
 
   describe('releaseTypeCell', () => {
     it('returns the release type', () => {
-      const placementRequest = placementRequestFactory.build({ releaseType: 'rotl' })
+      const task = placementRequestTaskFactory.build({ releaseType: 'rotl' })
 
-      expect(releaseTypeCell(placementRequest)).toEqual({ text: allReleaseTypes.rotl })
+      expect(releaseTypeCell(task)).toEqual({ text: allReleaseTypes.rotl })
     })
   })
 
   describe('tableRows', () => {
     it('returns table rows for placement requests', () => {
-      const placementRequest = placementRequestFactory.build()
+      const task = placementRequestTaskFactory.build()
 
-      expect(tableRows([placementRequest])).toEqual([
+      expect(tableRows([task])).toEqual([
         [
-          nameCell(placementRequest),
-          crnCell(placementRequest),
-          tierCell(placementRequest),
-          expectedArrivalDateCell(placementRequest),
-          dueDateCell(placementRequest, 7),
-          releaseTypeCell(placementRequest),
+          nameCell(task),
+          crnCell(task),
+          tierCell(task),
+          expectedArrivalDateCell(task),
+          dueDateCell(task, 7),
+          releaseTypeCell(task),
         ],
       ])
     })
--- server/utils/placementRequests/table.ts
+++ server/utils/placementRequests/table.ts
@@ -1,5 +1,5 @@
 import { add } from 'date-fns'
-import { PlacementRequest } from '../../@types/shared'
+import { PlacementRequestTask } from '../../@types/shared'
 import { TableCell, TableRow } from '../../@types/ui'
 import paths from '../../paths/match'
 import { DateFormats } from '../dateUtils'
@@ -9,26 +9,23 @@ import { allReleaseTypes } from '../applications/releaseTypeUtils'
 
 export const DIFFERENCE_IN_DAYS_BETWEEN_DUE_DATE_AND_ARRIVAL_DATE = 7
 
-export const tableRows = (placementRequests: Array<PlacementRequest>): Array<TableRow> => {
-  return placementRequests.map((placementRequest: PlacementRequest) => {
+export const tableRows = (tasks: Array<PlacementRequestTask>): Array<TableRow> => {
+  return tasks.map((task: PlacementRequestTask) => {
     return [
-      nameCell(placementRequest),
-      crnCell(placementRequest),
-      tierCell(placementRequest),
-      expectedArrivalDateCell(placementRequest),
-      dueDateCell(placementRequest, DIFFERENCE_IN_DAYS_BETWEEN_DUE_DATE_AND_ARRIVAL_DATE),
-      releaseTypeCell(placementRequest),
+      nameCell(task),
+      crnCell(task),
+      tierCell(task),
+      expectedArrivalDateCell(task),
+      dueDateCell(task, DIFFERENCE_IN_DAYS_BETWEEN_DUE_DATE_AND_ARRIVAL_DATE),
+      releaseTypeCell(task),
     ]
   })
 }
 
-export const crnCell = (placementRequest: PlacementRequest): TableCell => ({ text: placementRequest.person.crn })
+export const crnCell = (task: PlacementRequestTask): TableCell => ({ text: task.person.crn })
 
-export const dueDateCell = (
-  placementRequest: PlacementRequest,
-  differenceBetweenDueDateAndArrivalDate: number,
-): TableCell => {
-  const dateAsObject = DateFormats.isoToDateObj(placementRequest.expectedArrival)
+export const dueDateCell = (task: PlacementRequestTask, differenceBetweenDueDateAndArrivalDate: number): TableCell => {
+  const dateAsObject = DateFormats.isoToDateObj(task.expectedArrival)
 
   return {
     text: DateFormats.differenceInDays(
@@ -38,28 +35,28 @@ export const dueDateCell = (
   }
 }
 
-export const expectedArrivalDateCell = (placementRequest: PlacementRequest): TableCell => ({
-  text: DateFormats.isoDateToUIDate(placementRequest.expectedArrival),
+export const expectedArrivalDateCell = (task: PlacementRequestTask): TableCell => ({
+  text: DateFormats.isoDateToUIDate(task.expectedArrival),
 })
 
-export const nameCell = (placementRequest: PlacementRequest): TableCell => {
+export const nameCell = (task: PlacementRequestTask): TableCell => {
   return {
     html: linkTo(
       paths.placementRequests.show,
-      { id: placementRequest.id },
-      { text: placementRequest.person.name, attributes: { 'data-cy-placementRequestId': placementRequest.id } },
+      { id: task.id },
+      { text: task.person.name, attributes: { 'data-cy-placementRequestId': task.id } },
     ),
   }
 }
 
-export const tierCell = (placementRequest: PlacementRequest) => {
+export const tierCell = (task: PlacementRequestTask) => {
   return {
-    html: tierBadge(placementRequest.risks.tier?.value?.level),
+    html: tierBadge(task.risks.tier?.value?.level),
   }
 }
 
-export const releaseTypeCell = (placementRequest: PlacementRequest) => {
+export const releaseTypeCell = (task: PlacementRequestTask) => {
   return {
-    text: allReleaseTypes[placementRequest.releaseType],
+    text: allReleaseTypes[task.releaseType],
   }
 }
