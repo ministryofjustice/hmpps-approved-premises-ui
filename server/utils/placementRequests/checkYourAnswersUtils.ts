efafbc3bfa51d909abe1644cfa8aac49235aa553
rich <rich.james@dxw.com>
2023-06-08 17:03:58 +0100

    Allow CYA utils to display documents
    Documents appear on the CYA differently as they have metadata to show.
    So to display them we will need a new util.
    This is very similar to the function in summaryListUtils.ts but with
    the link changed from an Application path to a Placement Application path

--- server/utils/placementRequests/checkYourAnswersUtils.ts
+++ server/utils/placementRequests/checkYourAnswersUtils.ts
@@ -1,15 +1,21 @@
-import { PlacementApplication } from '../../@types/shared'
-import { HtmlItem, TextItem } from '../../@types/ui'
+import { ApprovedPremisesApplication as Application, Document, PlacementApplication } from '../../@types/shared'
+import { HtmlItem, SummaryListItem, TextItem } from '../../@types/ui'
+import AdditionalDocuments from '../../form-pages/placement-application/request-a-placement/additionalDocuments'
+import paths from '../../paths/placementApplications'
 import { embeddedSummaryListItem, summaryListItemForResponse } from '../applications/summaryListUtils'
-
 import { getPage, getResponseForPage } from '../applications/utils'
+import { retrieveQuestionResponseFromFormArtifact } from '../retrieveQuestionResponseFromFormArtifact'
 
-export const mapPageForSummaryList = (placementApplication: PlacementApplication, pageName: string) => {
+export const mapPageForSummaryList = (
+  placementApplication: PlacementApplication,
+  pageName: string,
+  application: Application,
+) => {
   return {
     card: {
       title: { text: getPageTitle(placementApplication, pageName) },
     },
-    rows: pageResponsesAsSummaryListItems(placementApplication, pageName),
+    rows: pageResponsesAsSummaryListItems(placementApplication, pageName, application),
   }
 }
 
@@ -24,7 +30,15 @@ export const pagesForReview = (placementApplication: PlacementApplication) => {
   return pageNames.filter(page => page !== 'check-your-answers')
 }
 
-export const pageResponsesAsSummaryListItems = (placementApplication: PlacementApplication, pageName: string) => {
+export const pageResponsesAsSummaryListItems = (
+  placementApplication: PlacementApplication,
+  pageName: string,
+  application: Application,
+) => {
+  if (pageName === 'additional-documents') {
+    return attachDocumentsSummaryListItems(placementApplication, application, 'request-a-placement', pageName, true)
+  }
+
   const response = getResponseForPage(placementApplication, 'request-a-placement', pageName)
   return Object.keys(response).map(key => {
     const value =
@@ -34,4 +48,43 @@ export const pageResponsesAsSummaryListItems = (placementApplication: PlacementA
 
     return summaryListItemForResponse(key, value, 'request-a-placement', pageName, placementApplication, true)
   })
+}
+
+export const attachDocumentsSummaryListItems = (
+  placementApplication: PlacementApplication,
+  application: Application,
+  taskName: string,
+  pageName: string,
+  showActions: boolean,
+) => {
+  const items: Array<SummaryListItem> = []
+
+  const documents = retrieveQuestionResponseFromFormArtifact(
+    placementApplication,
+    AdditionalDocuments,
+    'selectedDocuments',
+  ) as Array<Document>
+
+  documents.forEach(document => {
+    const item: SummaryListItem = {
+      key: {
+        html: `<a href="/applications/people/${application.person.crn}/documents/${document.id}" data-cy-documentId="${document.id}">${document.fileName}</a>`,
+      },
+      value: { text: document?.description || '' },
+    }
+    if (showActions) {
+      item.actions = {
+        items: [
+          {
+            href: paths.placementApplications.pages.show({ task: taskName, page: pageName, id: application.id }),
+            text: 'Change',
+            visuallyHiddenText: document.fileName,
+          },
+        ],
+      }
+    }
+    items.push(item)
+  })
+
+  return items
 }
