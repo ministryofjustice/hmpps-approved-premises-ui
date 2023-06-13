011a72bbcf97ad59f722dcb7fe0f1db892d0aaa5
pezholio <pezholio@gmail.com>
2023-06-13 14:53:45 +0100

    Update additional placement details view and tests

--- integration_tests/pages/match/placementRequestForm/datesOfPlacement.ts
+++ integration_tests/pages/match/placementRequestForm/datesOfPlacement.ts
@@ -7,7 +7,7 @@ export default class DatesOfPlacement extends Page {
 
   completeForm() {
     this.clearAndCompleteDateInputs('arrivalDate', '2023-08-01')
-    this.clearAndCompleteTextInputById('durationDays', '5')
     this.clearAndCompleteTextInputById('durationWeeks', '12')
+    this.clearAndCompleteTextInputById('durationDays', '5')
   }
 }
