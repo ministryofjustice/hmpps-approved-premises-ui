30c30d894dee3ad67c2b966f3bfc4beea484256c
Patrick Fleming <patrick.fleming@dwx.com>
2023-06-21 15:42:55 +0100

    Adds placement application ReviewController
    Including methods to handle all pages on the review journey.

---
+++ server/controllers/placementApplications/reviewController.test.ts
@@ -0,0 +1,367 @@
+import type { NextFunction, Request, Response } from 'express'
+import { DeepMocked, createMock } from '@golevelup/ts-jest'
+
+import type { ErrorsAndUserInput, FormPages } from '@approved-premises/ui'
+import { placementApplicationFactory } from '../../testutils/factories/index'
+import ReviewController from './reviewController'
+import { PlacementApplicationService } from '../../services'
+import PlacementRequest from '../../form-pages/placement-application'
+import matchPaths from '../../paths/match'
+import {
+  catchAPIErrorOrPropogate,
+  catchValidationErrorOrPropogate,
+  fetchErrorsAndUserInput,
+  generateErrorMessages,
+  generateErrorSummary,
+} from '../../utils/validation'
+import placementApplicationPaths from '../../paths/placementApplications'
+import { PlacementApplicationReview } from '../../utils/placementApplications/review'
+
+jest.mock('../../utils/validation')
+jest.mock('../../utils/placementApplications/review')
+
+PlacementRequest.pages = {} as FormPages
+
+describe('reviewController', () => {
+  const id = 'some-uuid'
+
+  const response: DeepMocked<Response> = createMock<Response>({})
+  const next: DeepMocked<NextFunction> = jest.fn()
+
+  const placementApplicationService = createMock<PlacementApplicationService>({})
+  const placementApplication = placementApplicationFactory.build()
+
+  let request: DeepMocked<Request> = createMock<Request>({
+    headers: { referer: 'some-referrer' },
+    params: { id },
+    flash: jest.fn(),
+  })
+
+  let reviewController: ReviewController
+
+  beforeEach(() => {
+    reviewController = new ReviewController(placementApplicationService)
+    request = createMock<Request>({
+      headers: { referer: 'some-referrer' },
+      params: { id },
+      flash: jest.fn(),
+      user: {
+        token: 'token',
+      },
+    })
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('show', () => {
+    it('renders the review page', async () => {
+      ;(fetchErrorsAndUserInput as jest.Mock).mockImplementation(() => {
+        return { errors: {}, errorSummary: [], userInput: {} }
+      })
+
+      placementApplicationService.getPlacementApplication.mockResolvedValue(placementApplication)
+      ;(PlacementApplicationReview as jest.Mock).mockImplementation(() => {
+        return { applicationId: id, step: 'review' }
+      })
+
+      const requestHandler = reviewController.show('review')
+
+      await requestHandler(request, response, next)
+
+      expect(response.render).toHaveBeenCalledWith('placement-applications/pages/review/review', {
+        pageProps: {
+          pageHeading: 'Review information',
+          backLink: `${matchPaths.placementRequests.index}#placement-applications`,
+        },
+        placementApplication,
+        errors: {},
+        errorSummary: [],
+      })
+    })
+
+    it('renders the decision page', async () => {
+      ;(fetchErrorsAndUserInput as jest.Mock).mockImplementation(() => {
+        return { errors: {}, errorSummary: [], userInput: {} }
+      })
+
+      placementApplicationService.getPlacementApplication.mockResolvedValue(placementApplication)
+      ;(PlacementApplicationReview as jest.Mock).mockImplementation(() => {
+        return { applicationId: id, step: 'decision' }
+      })
+
+      const requestHandler = reviewController.show('decision')
+
+      await requestHandler(request, response, next)
+
+      expect(response.render).toHaveBeenCalledWith('placement-applications/pages/review/decision', {
+        pageProps: {
+          pageHeading: 'Make a decision',
+          backLink: placementApplicationPaths.placementApplications.show({ id }),
+        },
+        placementApplication,
+        errors: {},
+        errorSummary: [],
+      })
+    })
+
+    it('shows errors and user input on review page when returning from an error state', async () => {
+      const errorsAndUserInput = createMock<ErrorsAndUserInput>()
+      ;(fetchErrorsAndUserInput as jest.Mock).mockReturnValue(errorsAndUserInput)
+
+      placementApplicationService.getPlacementApplication.mockResolvedValue(placementApplication)
+      ;(PlacementApplicationReview as jest.Mock).mockImplementation(() => {
+        return { applicationId: id, step: 'review' }
+      })
+
+      const requestHandler = reviewController.show('review')
+
+      await requestHandler(request, response, next)
+
+      expect(response.render).toHaveBeenCalledWith('placement-applications/pages/review/review', {
+        pageProps: {
+          pageHeading: 'Review information',
+          backLink: `${matchPaths.placementRequests.index}#placement-applications`,
+        },
+        placementApplication,
+        errors: errorsAndUserInput.errors,
+        errorSummary: errorsAndUserInput.errorSummary,
+        ...errorsAndUserInput.userInput,
+      })
+    })
+
+    it('shows errors and user input on decision page when returning from an error state', async () => {
+      const errorsAndUserInput = createMock<ErrorsAndUserInput>()
+      ;(fetchErrorsAndUserInput as jest.Mock).mockReturnValue(errorsAndUserInput)
+
+      placementApplicationService.getPlacementApplication.mockResolvedValue(placementApplication)
+      ;(PlacementApplicationReview as jest.Mock).mockImplementation(() => {
+        return { applicationId: id, step: 'decision' }
+      })
+
+      const requestHandler = reviewController.show('decision')
+
+      await requestHandler(request, response, next)
+
+      expect(response.render).toHaveBeenCalledWith('placement-applications/pages/review/decision', {
+        pageProps: {
+          pageHeading: 'Make a decision',
+          backLink: placementApplicationPaths.placementApplications.show({ id }),
+        },
+        placementApplication,
+        errors: errorsAndUserInput.errors,
+        errorSummary: errorsAndUserInput.errorSummary,
+        ...errorsAndUserInput.userInput,
+      })
+    })
+  })
+
+  describe('update', () => {
+    beforeEach(() => {
+      request.session = createMock<Request['session']>({
+        placementApplicationDecisions: {},
+      })
+    })
+
+    it('redirects to the decision path on review update success', async () => {
+      const update = jest.fn()
+      ;(PlacementApplicationReview as jest.Mock).mockImplementation(() => {
+        return { applicationId: id, update }
+      })
+
+      const requestHandler = reviewController.update()
+
+      await requestHandler(
+        {
+          ...request,
+          body: {
+            summaryOfChanges: 'some changes',
+          },
+        },
+        response,
+      )
+
+      expect(update).toHaveBeenCalledTimes(1)
+      expect(response.redirect).toHaveBeenCalledWith(
+        placementApplicationPaths.placementApplications.review.decision({ id }),
+      )
+    })
+
+    it('renders errors if the changes summary is empty', async () => {
+      const err = new Error('Invalid request body')
+      const errors = { summaryOfChanges: 'some error' }
+
+      const update = jest.fn(() => {
+        throw err
+      })
+
+      ;(PlacementApplicationReview as jest.Mock).mockImplementation(() => {
+        return {
+          errors,
+          update,
+        }
+      })
+      ;(generateErrorMessages as jest.Mock).mockReturnValue('some error')
+      ;(generateErrorSummary as jest.Mock).mockReturnValue('some error summary')
+
+      const requestHandler = reviewController.update()
+
+      await requestHandler(
+        {
+          ...request,
+          body: {
+            summaryOfChanges: '',
+          },
+        },
+        response,
+      )
+
+      expect(update).toHaveBeenCalledTimes(1)
+
+      expect(generateErrorMessages).toHaveBeenCalledWith(errors)
+      expect(generateErrorSummary).toHaveBeenCalledWith(errors)
+
+      expect(request.flash).toHaveBeenCalledWith('errors', 'some error')
+      expect(request.flash).toHaveBeenCalledWith('errorSummary', 'some error summary')
+      expect(request.flash).toHaveBeenCalledWith('userInput', { summaryOfChanges: '' })
+    })
+
+    it('passes unexpected error to catchValidationErrorOrPropogate', async () => {
+      const err = new Error()
+      ;(PlacementApplicationReview as jest.Mock).mockImplementation(() => {
+        return {
+          update: jest.fn(() => {
+            throw err
+          }),
+        }
+      })
+
+      const requestHandler = reviewController.update()
+
+      await requestHandler(request, response)
+
+      expect(catchValidationErrorOrPropogate).toHaveBeenCalledWith(request, response, err, request.headers.referer)
+    })
+  })
+
+  describe('submit', () => {
+    beforeEach(() => {
+      request = createMock<Request>({
+        ...request,
+        session: {
+          placementApplicationDecisions: { [id]: { summaryOfChanges: 'some changes' } },
+        },
+      })
+    })
+
+    it('redirects to the confirm path on success', async () => {
+      const update = jest.fn()
+      ;(PlacementApplicationReview as jest.Mock).mockImplementation(() => {
+        return {
+          applicationId: id,
+          update,
+        }
+      })
+
+      placementApplicationService.submitDecision.mockResolvedValue(placementApplication)
+
+      const requestHandler = reviewController.submit()
+
+      await requestHandler(
+        {
+          ...request,
+          body: {
+            decision: 'a decision',
+            decisionSummary: 'a decision summary',
+          },
+        },
+        response,
+      )
+
+      expect(update).toHaveBeenCalled()
+      expect(placementApplicationService.submitDecision).toHaveBeenCalledWith('token', 'some-uuid', {
+        summaryOfChanges: 'some changes',
+      })
+      expect(response.redirect).toHaveBeenCalledWith(
+        placementApplicationPaths.placementApplications.review.confirm({ id }),
+      )
+    })
+
+    it('renders with errors if required fields are missing', async () => {
+      const err = new Error('Invalid request body')
+      const errors = { decision: 'some error', decisionSummary: 'some other error' }
+      ;(PlacementApplicationReview as jest.Mock).mockImplementation(() => {
+        return {
+          applicationId: id,
+          errors,
+          update: jest.fn(() => {
+            throw err
+          }),
+        }
+      })
+      ;(generateErrorMessages as jest.Mock).mockReturnValue('some error')
+      ;(generateErrorSummary as jest.Mock).mockReturnValue('some error summary')
+
+      const requestHandler = reviewController.submit()
+
+      await requestHandler(
+        {
+          ...request,
+          body: {
+            decision: '',
+            decisionSummary: '',
+          },
+        },
+        response,
+      )
+
+      expect(generateErrorMessages).toHaveBeenCalledWith(errors)
+      expect(generateErrorSummary).toHaveBeenCalledWith(errors)
+
+      expect(request.flash).toHaveBeenCalledWith('errors', 'some error')
+      expect(request.flash).toHaveBeenCalledWith('errorSummary', 'some error summary')
+      expect(request.flash).toHaveBeenCalledWith('userInput', { decision: '', decisionSummary: '' })
+    })
+
+    it('throws an error if submit decision returns an error', async () => {
+      ;(PlacementApplicationReview as jest.Mock).mockImplementation(() => {
+        return {
+          applicationId: id,
+          update: jest.fn(),
+        }
+      })
+      const err = { status: 403 }
+      placementApplicationService.submitDecision.mockImplementationOnce(() => {
+        throw err
+      })
+
+      const requestHandler = reviewController.submit()
+
+      await requestHandler(
+        {
+          ...request,
+          body: {
+            decision: 'a decision',
+            decisionSummary: 'a decision summary',
+          },
+        },
+        response,
+      )
+
+      expect(catchAPIErrorOrPropogate).toHaveBeenCalledWith(request, response, err)
+    })
+  })
+
+  describe('confirm', () => {
+    it('renders the confirm page', async () => {
+      const requestHandler = reviewController.confirm()
+
+      await requestHandler(request, response)
+
+      expect(response.render).toHaveBeenCalledWith('placement-applications/pages/review/confirm', {
+        pageHeading: 'Review complete',
+      })
+    })
+  })
+})
---
+++ server/controllers/placementApplications/reviewController.ts
@@ -0,0 +1,111 @@
+import type { Request, RequestHandler, Response } from 'express'
+import { PlacementApplicationDecisionEnvelope } from '@approved-premises/api'
+import { PlacementApplicationService } from '../../services'
+
+import {
+  catchAPIErrorOrPropogate,
+  catchValidationErrorOrPropogate,
+  fetchErrorsAndUserInput,
+  generateErrorMessages,
+  generateErrorSummary,
+} from '../../utils/validation'
+import { PlacementApplicationReview } from '../../utils/placementApplications/review'
+import paths from '../../paths/match'
+import placementApplicationPaths from '../../paths/placementApplications'
+
+export default class ReviewController {
+  constructor(private readonly placementApplicationService: PlacementApplicationService) {}
+
+  show(step: 'review' | 'decision'): RequestHandler {
+    return async (req: Request, res: Response) => {
+      const { errors, errorSummary, userInput } = fetchErrorsAndUserInput(req)
+
+      const review = new PlacementApplicationReview(req, step)
+      const placementApplication = await this.placementApplicationService.getPlacementApplication(
+        req.user.token,
+        req.params.id,
+      )
+
+      const pageProps =
+        step === 'review'
+          ? { pageHeading: 'Review information', backLink: `${paths.placementRequests.index}#placement-applications` }
+          : {
+              pageHeading: 'Make a decision',
+              backLink: placementApplicationPaths.placementApplications.show({ id: review.applicationId }),
+            }
+
+      res.render(`placement-applications/pages/review/${review.step}`, {
+        pageProps,
+        placementApplication,
+        errors,
+        errorSummary,
+        ...userInput,
+      })
+    }
+  }
+
+  update() {
+    return async (req: Request, res: Response) => {
+      const review = new PlacementApplicationReview(req, 'review')
+
+      try {
+        review.update()
+        return res.redirect(
+          placementApplicationPaths.placementApplications.review.decision({ id: review.applicationId }),
+        )
+      } catch (err) {
+        if (err.message === 'Invalid request body') {
+          const errorMessages = generateErrorMessages(review.errors)
+          const errorSummary = generateErrorSummary(review.errors)
+
+          req.flash('errors', errorMessages)
+          req.flash('errorSummary', errorSummary)
+          req.flash('userInput', req.body)
+
+          return res.redirect(req.headers.referer)
+        }
+        return catchValidationErrorOrPropogate(req, res, err, req.headers.referer)
+      }
+    }
+  }
+
+  submit() {
+    return async (req: Request, res: Response) => {
+      const review = new PlacementApplicationReview(req, 'decision')
+
+      try {
+        review.update()
+
+        await this.placementApplicationService.submitDecision(
+          req.user.token,
+          req.params.id,
+          req.session.placementApplicationDecisions[review.applicationId] as PlacementApplicationDecisionEnvelope,
+        )
+
+        return res.redirect(
+          placementApplicationPaths.placementApplications.review.confirm({ id: review.applicationId }),
+        )
+      } catch (err) {
+        if (err.message === 'Invalid request body') {
+          const errorMessages = generateErrorMessages(review.errors)
+          const errorSummary = generateErrorSummary(review.errors)
+
+          req.flash('errors', errorMessages)
+          req.flash('errorSummary', errorSummary)
+          req.flash('userInput', req.body)
+
+          return res.redirect(req.headers.referer)
+        }
+        return catchAPIErrorOrPropogate(req, res, err)
+      }
+    }
+  }
+
+  confirm() {
+    return async (req: Request, res: Response) => {
+      res.render('placement-applications/pages/review/confirm', {
+        pageHeading: 'Review complete',
+      })
+    }
+  }
+}
