85727b4cda9bad4f82d71f825ee5be6744c6c867
rich <rich.james@dxw.com>
2023-06-23 17:01:02 +0100

    Add forPagesInTask function for iterating through task pages
    This differ from the version in TA as our first task starts with an
    coniditional question, so we check that there is a response for the question
    before dealing with it. If there isn't we skip to the next question if there
    is one, if there isn't we set the pageName to an empty string.
    We also filter out the check-your-answers screen because we never care
    about the answer to that question

---
+++ server/utils/applications/forPagesInTask.ts
@@ -0,0 +1,28 @@
+import { FormArtifact, UiTask } from '../../@types/ui'
+import { journeyTypeFromArtifact } from '../journeyTypeFromArtifact'
+import { getPage } from './getPage'
+import TasklistPage from '../../form-pages/tasklistPage'
+
+export const forPagesInTask = (
+  formArtifact: FormArtifact,
+  task: UiTask,
+  callback: (page: TasklistPage, pageName: string) => void,
+): void => {
+  const pageNames = Object.keys(task.pages)
+  let pageName = pageNames?.[0]
+
+  while (pageName && pageName !== 'check-your-answers') {
+    const Page = getPage(task.id, pageName, journeyTypeFromArtifact(formArtifact))
+    const body = formArtifact?.data?.[task.id]?.[pageName]
+
+    if (body) {
+      const page = new Page(body, formArtifact)
+      callback(page, pageName)
+      pageName = page.next()
+    } else if (pageNames.indexOf(pageName) + 1 < pageNames.length) {
+      pageName = pageNames.at(pageNames.indexOf(pageName) + 1)
+    } else {
+      pageName = ''
+    }
+  }
+}
