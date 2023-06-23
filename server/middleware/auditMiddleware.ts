60acca9dfa0c3427ec6b03cd3929e8385ac948e8
pezholio <pezholio@gmail.com>
2023-06-23 11:48:33 +0100

    Send audit messages depending on redirect destination
    By reading the "Location" header from the response, we can determine
    where the request handler redirected the user to, and send audit
    messages depending on this destination. Using the path pattern, we can
    also determine the details to include in the audit message.
    
    In many of our PUT and POST routes, we follow the pattern of
    redirecting back to the source page in the event of an error, and to a
    page for viewing the edited/created entity on success. We can use this
    to send the appropriate audit event in each case.

--- server/middleware/auditMiddleware.ts
+++ server/middleware/auditMiddleware.ts
@@ -1,11 +1,16 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express'
+import { Key, pathToRegexp } from 'path-to-regexp'
 import logger from '../../logger'
 import AuditService from '../services/auditService'
 
 export type AuditEventSpec = {
   auditEvent?: string
   auditBodyParams?: Array<string>
+  redirectAuditEventSpecs?: Array<RedirectAuditEventSpec>
 }
+
+type RedirectAuditEventSpec = { path: string; auditEvent: string }
+type RedirectAuditMatcher = { keys: Array<Key>; auditEvent: string; regExp: RegExp }
 
 export const auditMiddleware = (
   handler: RequestHandler,
@@ -13,7 +18,20 @@ export const auditMiddleware = (
   auditEventSpec?: AuditEventSpec,
 ) => {
   if (auditEventSpec) {
-    return wrapHandler(handler, auditService, auditEventSpec?.auditEvent, auditEventSpec?.auditBodyParams)
+    const redirectMatchers: Array<RedirectAuditMatcher> = auditEventSpec.redirectAuditEventSpecs?.map(
+      ({ path, auditEvent: redirectAuditEvent }) => {
+        const keys: Array<Key> = []
+        return { auditEvent: redirectAuditEvent, keys, regExp: pathToRegexp(path, keys) }
+      },
+    )
+
+    return wrapHandler(
+      handler,
+      auditService,
+      auditEventSpec?.auditEvent,
+      auditEventSpec?.auditBodyParams,
+      redirectMatchers,
+    )
   }
   return handler
 }
@@ -24,8 +42,11 @@ const wrapHandler =
     auditService: AuditService,
     auditEvent: string | undefined,
     auditBodyParams: Array<string> | undefined,
+    redirectMatchers: Array<RedirectAuditMatcher> | undefined,
   ) =>
   async (req: Request, res: Response, next: NextFunction) => {
+    let redirectAuditEvent: string
+    let redirectParams: Record<string, string>
     const username = res?.locals?.user?.name
 
     if (!username) {
@@ -36,8 +57,26 @@ const wrapHandler =
 
     await handler(req, res, next)
 
+    const encodedRedirectLocation = res.get('Location')
+    if (encodedRedirectLocation && redirectMatchers) {
+      const redirectPath = decodeURI(encodedRedirectLocation)
+      redirectParams = {}
+
+      redirectMatchers.some(redirectMatcher => {
+        if (matchAuditEvent(redirectPath, redirectMatcher, redirectParams)) {
+          redirectAuditEvent = redirectMatcher.auditEvent
+          return true
+        }
+        return false
+      })
+    }
+
     if (auditEvent) {
       await auditService.sendAuditMessage(auditEvent, username, auditDetails(req, auditBodyParams))
+    }
+
+    if (redirectAuditEvent) {
+      await auditService.sendAuditMessage(redirectAuditEvent, username, redirectParams)
     }
   }
 
@@ -53,4 +92,22 @@ const auditDetails = (req: Request, auditBodyParams: Array<string> | undefined) 
       {},
     ),
   }
+}
+
+const matchAuditEvent = (
+  path: string,
+  redirectMatcher: RedirectAuditMatcher,
+  redirectParams: Record<string, string>,
+) => {
+  const matches = redirectMatcher.regExp.exec(path)
+
+  if (matches) {
+    redirectMatcher.keys.forEach((key, i) => {
+      const param = key.name
+      redirectParams[param] = decodeURIComponent(matches[i + 1])
+    })
+
+    return true
+  }
+  return false
 }
