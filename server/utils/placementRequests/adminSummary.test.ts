0f89e7b239565b20943c880c870da0bdade1505e
pezholio <pezholio@gmail.com>
2023-07-18 16:18:57 +0100

    Add an adminSummary helper
    This will help us display a summary of a Placement Request

---
+++ server/utils/placementRequests/adminSummary.test.ts
@@ -0,0 +1,155 @@
+import { ApType, ReleaseTypeOption } from '../../@types/shared'
+import { placementRequestDetailFactory } from '../../testutils/factories'
+import { allApTypes } from '../allAPTypes'
+import { allReleaseTypes } from '../applications/releaseTypeUtils'
+import { DateFormats } from '../dateUtils'
+import { placementLength } from '../matchUtils'
+import { adminSummary, apTypeCell, releaseTypeCell } from './adminSummary'
+
+describe('adminSummary', () => {
+  it('should return a summary of a placement request', () => {
+    const placementRequest = placementRequestDetailFactory.build({
+      expectedArrival: '2022-01-01',
+      duration: 16,
+    })
+
+    expect(adminSummary(placementRequest)).toEqual({
+      rows: [
+        {
+          key: {
+            text: 'CRN',
+          },
+          value: {
+            text: placementRequest.person.crn,
+          },
+        },
+        {
+          key: {
+            text: 'Tier',
+          },
+          value: {
+            text: placementRequest.risks.tier.value.level,
+          },
+        },
+        {
+          key: {
+            text: 'Arrival Date',
+          },
+          value: {
+            text: DateFormats.isoDateToUIDate('2022-01-01'),
+          },
+        },
+        {
+          key: {
+            text: 'Departure Date',
+          },
+          value: {
+            text: DateFormats.isoDateToUIDate('2022-01-17'),
+          },
+        },
+        {
+          key: {
+            text: 'Length of stay',
+          },
+          value: {
+            text: placementLength(16),
+          },
+        },
+        apTypeCell(placementRequest),
+        releaseTypeCell(placementRequest),
+      ],
+    })
+  })
+
+  it('should return N/A if there is no tier', () => {
+    const placementRequest = placementRequestDetailFactory.build({
+      expectedArrival: '2022-01-01',
+      duration: 16,
+      risks: undefined,
+    })
+
+    expect(adminSummary(placementRequest)).toEqual({
+      rows: [
+        {
+          key: {
+            text: 'CRN',
+          },
+          value: {
+            text: placementRequest.person.crn,
+          },
+        },
+        {
+          key: {
+            text: 'Tier',
+          },
+          value: {
+            text: 'N/A',
+          },
+        },
+        {
+          key: {
+            text: 'Arrival Date',
+          },
+          value: {
+            text: DateFormats.isoDateToUIDate('2022-01-01'),
+          },
+        },
+        {
+          key: {
+            text: 'Departure Date',
+          },
+          value: {
+            text: DateFormats.isoDateToUIDate('2022-01-17'),
+          },
+        },
+        {
+          key: {
+            text: 'Length of stay',
+          },
+          value: {
+            text: placementLength(16),
+          },
+        },
+        apTypeCell(placementRequest),
+        releaseTypeCell(placementRequest),
+      ],
+    })
+  })
+
+  describe('apTypeCell', () => {
+    it.each(Object.keys(allApTypes))('should return the correct type for AP Type %s', (apType: ApType) => {
+      const placementRequest = placementRequestDetailFactory.build({
+        type: apType,
+      })
+
+      expect(apTypeCell(placementRequest)).toEqual({
+        key: {
+          text: 'Type of AP',
+        },
+        value: {
+          text: allApTypes[apType],
+        },
+      })
+    })
+  })
+
+  describe('releaseTypeCell', () => {
+    it.each(Object.keys(allReleaseTypes))(
+      'should return the correct type for release type %s',
+      (releaseType: ReleaseTypeOption) => {
+        const placementRequest = placementRequestDetailFactory.build({
+          releaseType,
+        })
+
+        expect(releaseTypeCell(placementRequest)).toEqual({
+          key: {
+            text: 'Release Type',
+          },
+          value: {
+            text: allReleaseTypes[releaseType],
+          },
+        })
+      },
+    )
+  })
+})
---
+++ server/utils/placementRequests/adminSummary.ts
@@ -0,0 +1,78 @@
+import { PlacementRequestDetail } from '../../@types/shared'
+import { SummaryList } from '../../@types/ui'
+import { allApTypes } from '../allAPTypes'
+import { allReleaseTypes } from '../applications/releaseTypeUtils'
+import { DateFormats } from '../dateUtils'
+import { placementDates, placementLength } from '../matchUtils'
+
+export const adminSummary = (placementRequest: PlacementRequestDetail): SummaryList => {
+  const dates = placementDates(placementRequest.expectedArrival, String(placementRequest.duration))
+  return {
+    rows: [
+      {
+        key: {
+          text: 'CRN',
+        },
+        value: {
+          text: placementRequest.person.crn,
+        },
+      },
+      {
+        key: {
+          text: 'Tier',
+        },
+        value: {
+          text: placementRequest.risks?.tier?.value?.level || 'N/A',
+        },
+      },
+      {
+        key: {
+          text: 'Arrival Date',
+        },
+        value: {
+          text: DateFormats.isoDateToUIDate(dates.startDate),
+        },
+      },
+      {
+        key: {
+          text: 'Departure Date',
+        },
+        value: {
+          text: DateFormats.isoDateToUIDate(dates.endDate),
+        },
+      },
+      {
+        key: {
+          text: 'Length of stay',
+        },
+        value: {
+          text: placementLength(dates.placementLength),
+        },
+      },
+      apTypeCell(placementRequest),
+      releaseTypeCell(placementRequest),
+    ],
+  }
+}
+
+export const apTypeCell = (placementRequest: PlacementRequestDetail) => {
+  return {
+    key: {
+      text: 'Type of AP',
+    },
+    value: {
+      text: allApTypes[placementRequest.type],
+    },
+  }
+}
+
+export const releaseTypeCell = (placementRequest: PlacementRequestDetail) => {
+  return {
+    key: {
+      text: 'Release Type',
+    },
+    value: {
+      text: allReleaseTypes[placementRequest.releaseType],
+    },
+  }
+}
