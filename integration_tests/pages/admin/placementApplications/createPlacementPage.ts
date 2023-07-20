01b89d384609f7b504dc6801187f494ae9dc3afa
pezholio <pezholio@gmail.com>
2023-07-20 13:27:08 +0100

    Add view and integration tests to create a booking

---
+++ integration_tests/pages/admin/placementApplications/createPlacementPage.ts
@@ -0,0 +1,25 @@
+import { PlacementRequestDetail } from '../../../../server/@types/shared'
+import { placementDates } from '../../../../server/utils/matchUtils'
+import Page from '../../page'
+
+export default class CreatePlacementPage extends Page {
+  constructor(private readonly placementRequest: PlacementRequestDetail) {
+    super('Create a placement')
+  }
+
+  dateInputsShouldBePrepopulated(): void {
+    const dates = placementDates(this.placementRequest.expectedArrival, String(this.placementRequest.duration))
+
+    this.dateInputsShouldContainDate('arrivalDate', dates.startDate)
+    this.dateInputsShouldContainDate('departureDate', dates.endDate)
+  }
+
+  completeForm(startDate: string, endDate: string, premisesId: string): void {
+    this.clearDateInputs('arrivalDate')
+    this.completeDateInputs('arrivalDate', startDate)
+
+    this.clearDateInputs('departureDate')
+    this.completeDateInputs('departureDate', endDate)
+    this.getSelectInputByIdAndSelectAnEntry('premisesId', premisesId)
+  }
+}
