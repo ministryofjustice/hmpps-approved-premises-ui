12267066de1c8856b0c4ba5437fd9ac02a7627bc
rich <rich.james@dxw.com>
2023-12-06 14:13:07 +0000

    Move stub data
    We need to retain these reference data jsons for the Cypress tests

---
+++ server/testutils/referenceData/stubs/referenceDataStubs.ts
@@ -0,0 +1,94 @@
+/* istanbul ignore file */
+
+import departureReasonsJson from './departure-reasons.json'
+import moveOnCategoriesJson from './move-on-categories.json'
+import destinationProvidersJson from './destination-providers.json'
+import cancellationReasonsJson from './cancellation-reasons.json'
+import lostBedReasonsJson from './lost-bed-reasons.json'
+import keyWorkersJson from './keyworkers.json'
+
+const departureReasons = {
+  request: {
+    method: 'GET',
+    url: '/reference-data/departure-reasons',
+  },
+  response: {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+    jsonBody: departureReasonsJson,
+  },
+}
+
+const moveOnCategories = {
+  request: {
+    method: 'GET',
+    url: '/reference-data/move-on-categories',
+  },
+  response: {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+    jsonBody: moveOnCategoriesJson,
+  },
+}
+
+const destinationProviders = {
+  request: {
+    method: 'GET',
+    url: '/reference-data/destination-providers',
+  },
+  response: {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+    jsonBody: destinationProvidersJson,
+  },
+}
+
+const cancellationReasons = {
+  request: {
+    method: 'GET',
+    url: '/reference-data/cancellation-reasons',
+  },
+  response: {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+    jsonBody: cancellationReasonsJson,
+  },
+}
+
+const lostBedReasons = {
+  request: {
+    method: 'GET',
+    url: '/reference-data/lost-bed-reasons',
+  },
+  response: {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+    jsonBody: lostBedReasonsJson,
+  },
+}
+
+const keyWorkers = {
+  request: {
+    method: 'GET',
+    url: '/reference-data/key-workers',
+  },
+  response: {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
+    jsonBody: keyWorkersJson,
+  },
+}
+
+export { departureReasons, moveOnCategories, destinationProviders, cancellationReasons, lostBedReasons, keyWorkers }
