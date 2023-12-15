40d5d57bd5a9c871e5c8f8866f61d10d70fac969
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2023-12-15 09:28:25 +0000

    Add placement request report
    Sort report names on label instead of key
    
    ignore intellij iml project file at any directory level (including top level)

--- server/utils/reportUtils.ts
+++ server/utils/reportUtils.ts
@@ -6,10 +6,11 @@ export const reportNames = {
   applications: 'Applications',
   referrals: 'Referrals',
   'lost-beds': 'Lost Beds',
+  'placement-applications': 'Placement Requests',
 } as const
 
 export type ReportType = (keyof typeof reportNames)[number]
 
-export const reportOptions = Object.keys(reportNames)
-  .sort()
-  .map(k => ({ value: k, text: reportNames[k] }))
+export const reportOptions = Object.entries(reportNames)
+  .sort((a, b) => (a[1] > b[1] ? 1 : -1))
+  .map(e => ({ value: e[0], text: e[1] }))
