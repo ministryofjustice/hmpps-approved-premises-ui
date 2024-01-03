8c0d4b6cb3bfd2eb838c645a478324457d68f954
pezholio <pezholio@gmail.com>
2024-01-03 16:10:11 +0000

    Update task view and integration tests
    This also adds an integration test to ensure sorting works correctly.

--- integration_tests/tests/tasks/list.cy.ts
+++ integration_tests/tests/tasks/list.cy.ts
@@ -85,4 +85,49 @@ context('Tasks', () => {
     // And I should see the tasks that are allocated
     listPage.shouldShowAllocatedTasks(allocatedTasksPage9)
   })
+
+  it('supports sorting', () => {
+    cy.task('stubAuthUser')
+
+    // Given I am logged in
+    cy.signIn()
+
+    const tasks = taskFactory.buildList(10)
+
+    cy.task('stubReallocatableTasks', {
+      tasks,
+      allocatedFilter: 'allocated',
+      page: '1',
+      sortDirection: 'asc',
+      sortField: 'createdAt',
+    })
+    cy.task('stubReallocatableTasks', {
+      tasks,
+      allocatedFilter: 'allocated',
+      sortDirection: 'desc',
+      sortField: 'createdAt',
+    })
+
+    // When I visit the tasks dashboard
+    const listPage = ListPage.visit(tasks, [])
+
+    // Then I should see the tasks that are allocated
+    listPage.shouldShowAllocatedTasks()
+
+    // When I sort by created at in ascending order
+    listPage.clickSortBy('createdAt')
+
+    // Then the dashboard should be sorted by created at
+    listPage.shouldBeSortedByField('createdAt', 'descending')
+
+    // And the API should have received a request for the correct sort order
+    cy.task('verifyTasksRequests', {
+      allocatedFilter: 'allocated',
+      page: '1',
+      sortDirection: 'desc',
+      sortField: 'createdAt',
+    }).then(requests => {
+      expect(requests).to.have.length(1)
+    })
+  })
 })
