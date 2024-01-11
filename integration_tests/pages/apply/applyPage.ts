e83398f97edb26706a7a8b32cd31cd74b9518b8a
rich <rich.james@dxw.com>
2024-01-11 14:33:16 +0000

    Raise Formpage functionality
    We will soon be adding an AssessPage that inherits most of this functionality.

--- integration_tests/pages/apply/applyPage.ts
+++ integration_tests/pages/apply/applyPage.ts
@@ -1,10 +1,10 @@
 import { ApprovedPremisesApplication } from '@approved-premises/api'
 
 import Apply from '../../../server/form-pages/apply'
-import TasklistPage from '../../../server/form-pages/tasklistPage'
-import Page from '../page'
+import TasklistPage, { TasklistPageInterface } from '../../../server/form-pages/tasklistPage'
+import FormPage from '../formPage'
 
-export default class ApplyPage extends Page {
+export default class ApplyPage extends FormPage {
   tasklistPage: TasklistPage
 
   constructor(
@@ -14,43 +14,10 @@ export default class ApplyPage extends Page {
     pageName: string,
     backLink?: string,
   ) {
-    super(title)
+    super(title, backLink)
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const Class = Apply.pages[taskName][pageName] as any
+    const Class = Apply.pages[taskName][pageName] as TasklistPageInterface
 
     this.tasklistPage = new Class(application.data?.[taskName]?.[pageName], application)
-
-    if (backLink) {
-      this.checkForBackButton(backLink)
-    }
-
-    this.checkPhaseBanner('Give us your feedback')
-  }
-
-  checkRadioButtonFromPageBody(fieldName: string) {
-    this.checkRadioByNameAndValue(fieldName, this.tasklistPage.body[fieldName] as string)
-  }
-
-  completeTextInputFromPageBody(fieldName: string) {
-    this.getTextInputByIdAndEnterDetails(fieldName, this.tasklistPage.body[fieldName] as string)
-  }
-
-  checkCheckboxesFromPageBody(
-    fieldName: string,
-    options: { addArrayNotationToInputName: boolean } = { addArrayNotationToInputName: false },
-  ) {
-    ;(this.tasklistPage.body[fieldName] as Array<string>).forEach(need => {
-      this.checkCheckboxByNameAndValue(options.addArrayNotationToInputName ? `${fieldName}[]` : fieldName, need)
-    })
-  }
-
-  completeDateInputsFromPageBody(fieldName: string) {
-    const date = this.tasklistPage.body[fieldName] as string
-    this.completeDateInputs(fieldName, date)
-  }
-
-  selectSelectOptionFromPageBody(fieldName: string) {
-    this.getSelectInputByIdAndSelectAnEntry(fieldName, this.tasklistPage.body[fieldName] as string)
   }
 }
