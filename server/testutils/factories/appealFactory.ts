d465f591ed28425f6333b762f421e1e7b8471e02
pezholio <pezholio@gmail.com>
2024-02-22 11:54:10 +0000

    Update types

--- server/testutils/factories/appealFactory.ts
+++ server/testutils/factories/appealFactory.ts
@@ -2,16 +2,16 @@ import { faker } from '@faker-js/faker'
 import { Factory } from 'fishery'
 import { Appeal } from '../../@types/shared'
 import { DateFormats } from '../../utils/dateUtils'
+import userFactory from './user'
 
 export default Factory.define<Appeal>(() => ({
   id: faker.string.uuid(),
   appealDate: DateFormats.dateObjToIsoDate(faker.date.past()),
   appealDetail: faker.lorem.sentence(),
-  reviewer: faker.person.fullName(),
   decision: 'accepted',
   decisionDetail: faker.lorem.sentence(),
   createdAt: DateFormats.dateObjToIsoDateTime(faker.date.past()),
   applicationId: faker.string.uuid(),
   assessmentId: faker.string.uuid(),
-  createdByUserId: faker.string.uuid(),
+  createdByUser: userFactory.build(),
 }))
