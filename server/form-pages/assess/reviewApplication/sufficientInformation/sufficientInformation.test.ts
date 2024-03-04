5b65ec22e4458a5b3984f36c9e144cdfd7d7bbfb
rich <rich.james@dxw.com>
2024-03-04 08:58:54 +0000

    Remove initialize method on SufficientInformation page
    This method was just adding a user object to the page.
    This user object wasn't being used anywhere.
    
    This is unrelated to the other changes in this PR.

--- server/form-pages/assess/reviewApplication/sufficientInformation/sufficientInformation.test.ts
+++ server/form-pages/assess/reviewApplication/sufficientInformation/sufficientInformation.test.ts
@@ -1,17 +1,8 @@
-import { createMock } from '@golevelup/ts-jest'
-
-import { fromPartial } from '@total-typescript/shoehorn'
-import { UserService } from '../../../../services'
 import { itShouldHaveNextValue, itShouldHavePreviousValue } from '../../../shared-examples'
 
 import SufficientInformation from './sufficientInformation'
 
-import { assessmentFactory, userFactory } from '../../../../testutils/factories'
-
 describe('SufficientInformation', () => {
-  const user = userFactory.build()
-  const assessment = assessmentFactory.build()
-
   describe('title', () => {
     expect(new SufficientInformation({}).title).toBe(
       'Is there enough information in the application for you to make a decision?',
@@ -22,25 +13,6 @@ describe('SufficientInformation', () => {
     it('should set the body', () => {
       const page = new SufficientInformation({ sufficientInformation: 'yes' })
       expect(page.body).toEqual({ sufficientInformation: 'yes' })
-    })
-  })
-
-  describe('initialize', () => {
-    it('should fetch the user and return the page', async () => {
-      const getUserByIdMock = jest.fn()
-
-      const userService = createMock<UserService>({
-        getUserById: getUserByIdMock,
-      })
-
-      getUserByIdMock.mockResolvedValue(user)
-
-      const page = await SufficientInformation.initialize({}, assessment, 'some-token', fromPartial({ userService }))
-
-      expect(page).toBeInstanceOf(SufficientInformation)
-      expect(page.user).toEqual(user)
-
-      expect(userService.getUserById).toHaveBeenCalledWith('some-token', assessment.application.createdByUserId)
     })
   })
 
