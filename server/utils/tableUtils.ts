efac98473826c9473d37c2a345f2f9edf2bb2aa9
pezholio <pezholio@gmail.com>
2024-03-06 09:05:46 +0000

    Add a shared `daysUntilDueCell` function
    This can be used by `AssessmentSummary`s and `Task`s to get the due
    date of a task.

--- server/utils/tableUtils.ts
+++ server/utils/tableUtils.ts
@@ -1,6 +1,10 @@
 import { RiskTierEnvelope } from '../@types/shared'
 import { TableCell } from '../@types/ui'
+import { DateFormats } from './dateUtils'
 import { tierBadge } from './personUtils'
+import { pluralize } from './utils'
+
+const DUE_DATE_APPROACHING_DAYS_WINDOW = 3
 
 export const nameCell = (item: { personName?: string }): TableCell => ({ text: item.personName })
 
@@ -13,3 +17,32 @@ export const tierCell = (item: { tier?: RiskTierEnvelope }) => {
 }
 
 export const emailCell = (item: { email?: string }): TableCell => ({ text: item.email })
+
+export const daysUntilDueCell = (item: { dueAt: string }, warningClass: string): TableCell => {
+  const dueDate = DateFormats.isoToDateObj(item.dueAt)
+  const daysUntilDue = DateFormats.differenceInBusinessDays(dueDate, new Date())
+  const formattedDifference = pluralize('Day', daysUntilDue)
+
+  let html: string
+
+  switch (true) {
+    case daysUntilDue === 0:
+      html = `<strong class="${warningClass}">Today</strong>`
+      break
+    case daysUntilDue < 0:
+      html = `<strong class="${warningClass}">${formattedDifference}<span class="govuk-visually-hidden"> (Overdue by ${pluralize('day', Math.abs(daysUntilDue))})</span></strong>`
+      break
+    case daysUntilDue < DUE_DATE_APPROACHING_DAYS_WINDOW:
+      html = `<strong class="${warningClass}">${formattedDifference}<span class="govuk-visually-hidden"> (Approaching due date)</span></strong>`
+      break
+    default:
+      html = formattedDifference
+  }
+
+  return {
+    html,
+    attributes: {
+      'data-sort-value': daysUntilDue,
+    },
+  }
+}
