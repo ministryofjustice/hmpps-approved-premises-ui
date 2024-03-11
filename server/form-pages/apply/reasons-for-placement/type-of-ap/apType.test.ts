dcfe71de7cdbea8fb6b661c3f50fd4f76789468f
Ynda Jas <yndajas@gmail.com>
2024-03-11 18:51:49 +0000

    Use correct class name in import

--- server/form-pages/apply/reasons-for-placement/type-of-ap/apType.test.ts
+++ server/form-pages/apply/reasons-for-placement/type-of-ap/apType.test.ts
@@ -2,40 +2,40 @@ import { itShouldHaveNextValue, itShouldHavePreviousValue } from '../../../share
 import { convertKeyValuePairToRadioItems } from '../../../../utils/formUtils'
 
 // eslint-disable-next-line import/no-named-as-default
-import ApType from './apType'
+import SelectApType from './apType'
 
 jest.mock('../../../../utils/formUtils')
 
 describe('ApType', () => {
-  itShouldHavePreviousValue(new ApType({}), 'dashboard')
+  itShouldHavePreviousValue(new SelectApType({}), 'dashboard')
 
   describe('when type is set to pipe', () => {
-    itShouldHaveNextValue(new ApType({ type: 'pipe' }), 'pipe-referral')
+    itShouldHaveNextValue(new SelectApType({ type: 'pipe' }), 'pipe-referral')
   })
 
   describe('when type is set to esap', () => {
-    itShouldHaveNextValue(new ApType({ type: 'esap' }), 'managed-by-national-security-division')
+    itShouldHaveNextValue(new SelectApType({ type: 'esap' }), 'managed-by-national-security-division')
   })
 
   describe('when type is set to standard', () => {
-    itShouldHaveNextValue(new ApType({ type: 'standard' }), '')
+    itShouldHaveNextValue(new SelectApType({ type: 'standard' }), '')
   })
 
   describe('errors', () => {
     it('should return an empty object if the type is populated', () => {
-      const page = new ApType({ type: 'standard' })
+      const page = new SelectApType({ type: 'standard' })
       expect(page.errors()).toEqual({})
     })
 
     it('should return an errors if the type is not populated', () => {
-      const page = new ApType({})
+      const page = new SelectApType({})
       expect(page.errors()).toEqual({ type: 'You must specify an AP type' })
     })
   })
 
   describe('items', () => {
     it('it calls convertKeyValuePairToRadioItems', () => {
-      const page = new ApType({})
+      const page = new SelectApType({})
       page.items()
 
       expect(convertKeyValuePairToRadioItems).toHaveBeenCalled()
@@ -44,7 +44,7 @@ describe('ApType', () => {
 
   describe('response', () => {
     it('should return a translated version of the response', () => {
-      const page = new ApType({ type: 'pipe' })
+      const page = new SelectApType({ type: 'pipe' })
 
       expect(page.response()).toEqual({
         [page.title]: 'Psychologically Informed Planned Environment (PIPE)',
