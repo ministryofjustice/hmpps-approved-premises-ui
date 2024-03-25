caf82c1fdb4529a6c71f8d6b5287de756a2bbfee
pezholio <pezholio@gmail.com>
2024-03-25 13:46:50 +0000

    Add a feature flag service

---
+++ server/services/featureFlagService.test.ts
@@ -0,0 +1,89 @@
+import { createMock } from '@golevelup/ts-jest'
+import { FliptClient } from '@flipt-io/flipt'
+import { when } from 'jest-when'
+import { BooleanEvaluationResponse } from '@flipt-io/flipt/dist/evaluation/models'
+import config from '../config'
+import FeatureFlagService from './featureFlagService'
+import logger from '../../logger'
+
+jest.mock('@flipt-io/flipt')
+jest.mock('../../logger')
+
+describe('FeatureFlagService', () => {
+  let featureFlagService: FeatureFlagService
+
+  describe('when flipt is enabled', () => {
+    const mockClient = createMock<FliptClient>({
+      evaluation: {
+        boolean: jest.fn(),
+      },
+    })
+
+    beforeEach(() => {
+      config.fliptEnabled = true
+      config.apis.flipt = {
+        namespace: 'some-namespace',
+        token: 'some-token',
+        url: 'http://localhost',
+      }
+      ;(FliptClient as jest.Mock).mockReturnValue(mockClient)
+      featureFlagService = new FeatureFlagService()
+    })
+
+    afterEach(() => {
+      jest.resetAllMocks()
+    })
+
+    describe('getBooleanFlag', () => {
+      it.each([true, false])(
+        'returns the enabled response from the flipt client when enabled is %s',
+        async (enabled: boolean) => {
+          const booleanEvaluationResponse = createMock<BooleanEvaluationResponse>({
+            enabled,
+          })
+
+          when(mockClient.evaluation.boolean)
+            .calledWith({
+              namespaceKey: featureFlagService.namespaceKey,
+              flagKey: 'some-flag',
+              entityId: '',
+              context: {},
+            })
+            .mockResolvedValue(booleanEvaluationResponse)
+
+          const response = await featureFlagService.getBooleanFlag('some-flag')
+          expect(response).toEqual(enabled)
+        },
+      )
+
+      it('returns false and logs an error when the feature flag does not exist', async () => {
+        when(mockClient.evaluation.boolean)
+          .calledWith({
+            namespaceKey: featureFlagService.namespaceKey,
+            flagKey: 'some-flag',
+            entityId: '',
+            context: {},
+          })
+          .mockImplementation(() => {
+            throw new Error('Feature flag not found')
+          })
+
+        const response = await featureFlagService.getBooleanFlag('some-flag')
+
+        expect(response).toEqual(false)
+        expect(logger.error).toHaveBeenCalledWith('Feature flag some-flag not found, defaulting to false')
+      })
+    })
+  })
+
+  describe('when flipt is not enabled', () => {
+    beforeEach(() => {
+      config.fliptEnabled = false
+    })
+
+    it('should return true', async () => {
+      const response = await featureFlagService.getBooleanFlag('some-flag')
+      expect(response).toEqual(true)
+    })
+  })
+})
---
+++ server/services/featureFlagService.ts
@@ -0,0 +1,42 @@
+import { ClientTokenAuthentication, FliptClient } from '@flipt-io/flipt'
+import config from '../config'
+import logger from '../../logger'
+
+export default class FeatureFlagService {
+  fliptClient: FliptClient | null
+
+  namespaceKey: string | null
+
+  constructor() {
+    if (config.fliptEnabled) {
+      this.fliptClient = new FliptClient({
+        url: config.apis.flipt.url,
+        authenticationStrategy: new ClientTokenAuthentication(config.apis.flipt.token),
+      })
+      this.namespaceKey = config.apis.flipt.namespace
+    }
+  }
+
+  async getBooleanFlag(flag: string): Promise<boolean> {
+    if (!config.fliptEnabled) {
+      return true
+    }
+
+    try {
+      const response = await this.fliptClient.evaluation.boolean({
+        namespaceKey: this.namespaceKey,
+        flagKey: flag,
+        entityId: '',
+        context: {},
+      })
+
+      return response.enabled
+    } catch (err) {
+      if (err.message?.includes('not found')) {
+        logger.error(`Feature flag ${flag} not found, defaulting to false`)
+        return false
+      }
+      throw err
+    }
+  }
+}
