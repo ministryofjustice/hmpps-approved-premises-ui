820ebac9e723842b0c977f3c85d71b13fb168405
Ed Davey <ed.davey@digital.justice.gov.uk>
2024-06-06 17:13:25 +0100

    Convert existing tests to use new inclusion matcher
    Asserting on the presence of (or absence of) each
    action gives us a clear specification:
    
    ```
    premisesActions
      for users with the role "workflow_manager"
        ✓ includes the VIEW CALENDAR action
        ✓ includes the CREATE PLACEMENT action
        ✓ includes the MANAGE BEDS action
      for users with the role "legacy_manager"
        ✓ includes the MANAGE BEDS action
        ✓ does NOT include the VIEW CALENDAR action
        ✓ does NOT include the CREATE PLACEMENT action
      for users with the role "future_manager"
        ✓ includes the MANAGE BEDS action
        ✓ does NOT include the VIEW CALENDAR action
        ✓ does NOT include the CREATE PLACEMENT action
    ```
    
    which will allow us to make changes more easily.

--- server/utils/premises/premisesActions.test.ts
+++ server/utils/premises/premisesActions.test.ts
@@ -6,59 +6,88 @@ import userDetails from '../../testutils/factories/userDetails'
 describe('premisesActions', () => {
   describe('for users with the role "workflow_manager"', () => {
     const user = userDetails.build({ roles: ['workflow_manager'] })
+    const premises = premisesFactory.build()
 
-    it('returns all the actions', () => {
-      const premises = premisesFactory.build()
+    it('includes the VIEW CALENDAR action', () => {
+      expect(premisesActions(user, premises)).toContainManageAction({
+        text: 'View calendar',
+        classes: 'govuk-button--secondary',
+        href: paths.premises.calendar({ premisesId: premises.id }),
+      })
+    })
 
-      expect(premisesActions(user, premises)).toEqual([
-        {
-          text: 'View calendar',
-          classes: 'govuk-button--secondary',
-          href: paths.premises.calendar({ premisesId: premises.id }),
-        },
-        {
-          text: 'Create a placement',
-          classes: 'govuk-button--secondary',
-          href: paths.bookings.new({ premisesId: premises.id }),
-        },
-        {
-          text: 'Manage beds',
-          classes: 'govuk-button--secondary',
-          href: paths.premises.beds.index({ premisesId: premises.id }),
-        },
-      ])
+    it('includes the CREATE PLACEMENT action', () => {
+      expect(premisesActions(user, premises)).toContainManageAction({
+        text: 'Create a placement',
+        classes: 'govuk-button--secondary',
+        href: paths.bookings.new({ premisesId: premises.id }),
+      })
+    })
+
+    it('includes the MANAGE BEDS action', () => {
+      expect(premisesActions(user, premises)).toContainManageAction({
+        text: 'Manage beds',
+        classes: 'govuk-button--secondary',
+        href: paths.premises.beds.index({ premisesId: premises.id }),
+      })
     })
   })
 
   describe('for users with the role "legacy_manager"', () => {
     const user = userDetails.build({ roles: ['legacy_manager'] })
+    const premises = premisesFactory.build()
 
-    it('returns only "Manage beds"', () => {
-      const premises = premisesFactory.build()
+    it('includes the MANAGE BEDS action', () => {
+      expect(premisesActions(user, premises)).toContainManageAction({
+        text: 'Manage beds',
+        classes: 'govuk-button--secondary',
+        href: paths.premises.beds.index({ premisesId: premises.id }),
+      })
+    })
 
-      expect(premisesActions(user, premises)).toEqual([
-        {
-          text: 'Manage beds',
-          classes: 'govuk-button--secondary',
-          href: paths.premises.beds.index({ premisesId: premises.id }),
-        },
-      ])
+    it('does NOT include the VIEW CALENDAR action', () => {
+      expect(premisesActions(user, premises)).not.toContainManageAction({
+        text: 'View calendar',
+        classes: 'govuk-button--secondary',
+        href: paths.premises.calendar({ premisesId: premises.id }),
+      })
+    })
+
+    it('does NOT include the CREATE PLACEMENT action', () => {
+      expect(premisesActions(user, premises)).not.toContainManageAction({
+        text: 'Create a placement',
+        classes: 'govuk-button--secondary',
+        href: paths.bookings.new({ premisesId: premises.id }),
+      })
     })
   })
 
   describe('for users with the role "future_manager"', () => {
     const user = userDetails.build({ roles: ['future_manager'] })
+    const premises = premisesFactory.build()
 
-    it('returns only "Manage beds"', () => {
-      const premises = premisesFactory.build()
+    it('includes the MANAGE BEDS action', () => {
+      expect(premisesActions(user, premises)).toContainManageAction({
+        text: 'Manage beds',
+        classes: 'govuk-button--secondary',
+        href: paths.premises.beds.index({ premisesId: premises.id }),
+      })
+    })
 
-      expect(premisesActions(user, premises)).toEqual([
-        {
-          text: 'Manage beds',
-          classes: 'govuk-button--secondary',
-          href: paths.premises.beds.index({ premisesId: premises.id }),
-        },
-      ])
+    it('does NOT include the VIEW CALENDAR action', () => {
+      expect(premisesActions(user, premises)).not.toContainManageAction({
+        text: 'View calendar',
+        classes: 'govuk-button--secondary',
+        href: paths.premises.calendar({ premisesId: premises.id }),
+      })
+    })
+
+    it('does NOT include the CREATE PLACEMENT action', () => {
+      expect(premisesActions(user, premises)).not.toContainManageAction({
+        text: 'Create a placement',
+        classes: 'govuk-button--secondary',
+        href: paths.bookings.new({ premisesId: premises.id }),
+      })
     })
   })
 })
