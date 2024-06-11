a8bd4d1b19dcfba677ca3cf9db8e1f7290f3d09a
rich <rich.james@dxw.com>
2024-06-11 13:10:38 +0100

    Allow setRoles to accept Role as argument
    Previously we only allowed Qualifications but we now want to be able to
    accept Roles (which also includes Qualifications)

--- e2e/steps/admin.ts
+++ e2e/steps/admin.ts
@@ -1,14 +1,10 @@
 import { Page } from '@playwright/test'
 import { TestOptions } from '@approved-premises/e2e'
-import { EditUser, Qualification } from '../pages/admin/editUser'
+import { EditUser, Role } from '../pages/admin/editUser'
 import { visitDashboard } from './apply'
 import { UserList } from '../pages/admin/listUsers'
 
-export const setRoles = async (
-  page: Page,
-  username: TestOptions['user']['name'],
-  roles: ReadonlyArray<Qualification>,
-) => {
+export const setRoles = async (page: Page, username: TestOptions['user']['name'], roles: ReadonlyArray<Role>) => {
   const dashboard = await visitDashboard(page)
   dashboard.clickUserMangement()
 
