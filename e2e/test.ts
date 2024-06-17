6fc28cc7678af5e0225b5015ee5639adc43fdbeb
rich <rich.james@dxw.com>
2024-06-17 16:17:47 +0100

    Prefix env vars with CAS1
    These env vars will need to be stored in the API pipeline so for clarity
    it's best we prefix them with the service name. As CAS2 do.

--- e2e/test.ts
+++ e2e/test.ts
@@ -18,7 +18,12 @@ export const test = base.extend<TestOptions>({
     },
     { option: true },
   ],
-  userToAddAndDelete: [{ name: process.env.E2E_DELIUS_USERNAME_TO_ADD_AND_DELETE as string }, { option: true }],
+  userToAddAndDelete: [{ name: process.env.CAS1_E2E_DELIUS_USERNAME_TO_ADD_AND_DELETE as string }, { option: true }],
+  futureManager: [
+    { username: process.env.CAS1_E2E_FUTURE_MANAGER_USERNAME, password: process.env.CAS1_E2E_FUTURE_MANAGER_PASSWORD },
+    { option: true },
+  ],
+
   oasysSections: [['3. Accommodation', '13. Health', '4. Education, training and employment'], { option: true }],
-  emergencyApplicationUser: [process.env.E2E_EMERGENCY_USER_TO_ALLOCATE_TO, { option: true }],
+  emergencyApplicationUser: [process.env.CAS1_E2E_EMERGENCY_ASSESSOR_NAME_TO_ALLOCATE_TO, { option: true }],
 })
