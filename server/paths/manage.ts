47068b298ffdac9ed05a23a7f2ba73f980917065
rich <rich.james@dxw.com>
2024-07-09 12:23:56 +0100

    Correct update path
    Previously we didn't use the update OoS bed path in v2Manage so the path
    wasn't updated. But now we need it to work.

--- server/paths/manage.ts
+++ server/paths/manage.ts
@@ -132,8 +132,8 @@ const v2Manage = {
     create: outOfServiceBedsPath,
     premisesIndex: v2SinglePremisesPath.path('out-of-service-beds').path(':temporality'),
     index: outOfServiceBedsIndexPath.path(':temporality'),
-    update: singlePremisesPath.path('out-of-service-beds').path(':id'),
     show: outOfServiceBedPath.path(':tab'),
+    update: outOfServiceBedPath.path('/update'),
     cancel: singlePremisesPath.path('out-of-service-beds').path(':id').path('cancellations'),
   },
 }
