471b4dc76b46d9498fec48d2833cf7579f019714
bala.vura <bala.vura@digital.justice.gov.uk>
2024-08-07 09:18:25 +0100

    APS-1051: LAO E2E integration tests

--- integration_tests/tests/apply/list.cy.ts
+++ integration_tests/tests/apply/list.cy.ts
@@ -1,6 +1,10 @@
 import { ListPage, StartPage } from '../../pages/apply'
 
-import { applicationSummaryFactory, placementApplicationFactory } from '../../../server/testutils/factories'
+import {
+  applicationSummaryFactory,
+  placementApplicationFactory,
+  restrictedPersonFactory,
+} from '../../../server/testutils/factories'
 import Page from '../../pages/page'
 import ReasonForPlacementPage from '../../pages/match/placementRequestForm/reasonForPlacement'
 
@@ -17,6 +21,55 @@ context('Applications dashboard', () => {
 
     // And there are applications in the database
     const inProgressApplications = applicationSummaryFactory.buildList(5, { status: 'started' })
+    const submittedApplications = applicationSummaryFactory.buildList(5, { status: 'submitted' })
+    const requestedFurtherInformationApplications = applicationSummaryFactory.buildList(5, {
+      status: 'requestedFurtherInformation',
+    })
+    const awaitingPlacementApplications = applicationSummaryFactory.buildList(5, { status: 'awaitingPlacement' })
+
+    cy.task(
+      'stubApplications',
+      [
+        inProgressApplications,
+        submittedApplications,
+        requestedFurtherInformationApplications,
+        awaitingPlacementApplications,
+      ].flat(),
+    )
+
+    // When I visit the Previous Applications page
+    const page = ListPage.visit(inProgressApplications, submittedApplications, requestedFurtherInformationApplications)
+
+    // Then I should see all of the in progress applications
+    page.shouldShowInProgressApplications()
+
+    // And I click on the Further Information Requested tab
+    page.clickFurtherInformationRequestedTab()
+
+    // Then I should see the applications where further information has been requested
+    page.shouldShowFurtherInformationRequestedApplications()
+
+    // And I click on the submitted tab
+    page.clickSubmittedTab()
+
+    // Then I should see the applications that have been submitted
+    page.shouldShowSubmittedApplications()
+
+    // And I the button should take me to the application start page
+    page.clickSubmit()
+
+    Page.verifyOnPage(StartPage)
+  })
+
+  it('shows the dashboard lao', () => {
+    // Given I am logged in
+    cy.signIn()
+
+    // And there are applications in the database
+    const inProgressApplications = applicationSummaryFactory.buildList(1, {
+      status: 'started',
+      person: restrictedPersonFactory.build(),
+    })
     const submittedApplications = applicationSummaryFactory.buildList(5, { status: 'submitted' })
     const requestedFurtherInformationApplications = applicationSummaryFactory.buildList(5, {
       status: 'requestedFurtherInformation',
