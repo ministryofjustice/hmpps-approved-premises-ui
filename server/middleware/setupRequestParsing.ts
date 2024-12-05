f42032b07281e69250faa3bcbae72760e1225e2c
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2024-12-05 13:41:35 +0000

    Increase max POST body size and parameter count

--- server/middleware/setupRequestParsing.ts
+++ server/middleware/setupRequestParsing.ts
@@ -4,7 +4,7 @@ import trimInput from './trimInput'
 export default function setUpWebRequestParsing(): Router {
   const router = express.Router()
   router.use(express.json())
-  router.use(express.urlencoded({ extended: true }))
+  router.use(express.urlencoded({ extended: true, limit: '1mb', parameterLimit: 2500 }))
   router.use(trimInput())
   return router
 }
