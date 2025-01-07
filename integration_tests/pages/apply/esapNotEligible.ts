b46148d1bbe9c12f84675c8a74051bc4b1f55c86
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2025-01-07 18:14:39 +0000

    Prevent infinite loop for ineligible application
    This prevents an infinite loop when building the task list for an
    application that has the ESAP AP type selected but no agreement with
    CHPP. The loop was the result of the ineligibility page returning the user
    to the AP type selection page on submission. This works if the user then saves a
    different AP type straight away; however, should the user then exit the
    application, any further attempt to load the application would result in
    the loop.
    
    To prevent this, the ESAP ineligibility page no longer posts, but simply
    offers a link to the AP Type selection.
    
    In addition, to ensure the AP Type task is shown as 'In progress' in the
    above scenario, the ESAP ineligibility page always returns an error.
    This error is never shown, but prevents the loop from continuing with
    processing any further task.

--- integration_tests/pages/apply/esapNotEligible.ts
+++ integration_tests/pages/apply/esapNotEligible.ts
@@ -4,4 +4,8 @@ export default class EsapNotEligible extends Page {
   constructor() {
     super(`The person is not eligible for an Enhanced Security Approved Premises (ESAP) placement.`)
   }
+
+  clickContinueWithApplication() {
+    cy.get('a[role="button"]').contains('Continue with application').click()
+  }
 }
--- server/form-pages/apply/reasons-for-placement/type-of-ap/esapNotEligible.ts
+++ server/form-pages/apply/reasons-for-placement/type-of-ap/esapNotEligible.ts
@@ -1,14 +1,17 @@
 import { ApprovedPremisesApplication as Application } from '@approved-premises/api'
+import type { TaskListErrors } from '@approved-premises/ui'
 import { Page } from '../../../utils/decorators'
 
 import TasklistPage from '../../../tasklistPage'
 
-@Page({ name: 'not-esap-eligible', bodyProperties: [] })
+@Page({ name: 'not-esap-eligible', bodyProperties: ['esapNotEligible'] })
 export default class EsapNotEligible implements TasklistPage {
   title = `The person is not eligible for an Enhanced Security Approved Premises (ESAP) placement.`
 
   constructor(
-    readonly body: Record<string, never>,
+    readonly body: Partial<{
+      esapNotEligible: never
+    }>,
     readonly application: Application,
   ) {}
 
@@ -21,10 +24,14 @@ export default class EsapNotEligible implements TasklistPage {
   }
 
   next() {
-    return 'ap-type'
+    return ''
   }
 
   errors() {
-    return {}
+    const errors: TaskListErrors<this> = {}
+
+    errors.esapNotEligible = this.title
+
+    return errors
   }
 }
