ministryofjustice/approved-premises-ui

Update booking confirmation screen
This does a few things:
- Ensures the booking confirmation flow uses the arrival date and
  departure date submitted, rather than relying on filter start date and
  duration
- Removes the `apType` parameter as it is not used
- Updates the booking confirmation template to show relevent information
- Refactors a lot of the flow to present occupancy view with retained
  filters where relevant

integration_tests/pages/match/bookASpacePage.ts
@@ -1,54 +1,58 @@
 import {
-  ApType,
+  Cas1PremisesSummary,
   Cas1SpaceBookingCharacteristic,
-  Cas1SpaceCharacteristic,
-  PlacementDates,
   PlacementRequestDetail,
   Premises,
 } from '@approved-premises/api'
+import { differenceInDays } from 'date-fns'
 import Page from '../page'
 import paths from '../../../server/paths/match'
-import { createQueryString, sentenceCase } from '../../../server/utils/utils'
-import { DateFormats } from '../../../server/utils/dateUtils'
-import { placementDates, placementLength as placementLengthInDaysAndWeeks } from '../../../server/utils/match'
-import { placementCriteriaLabels } from '../../../server/utils/placementCriteriaUtils'
-import { apTypeLabels } from '../../../server/utils/apTypeLabels'
+import { createQueryString } from '../../../server/utils/utils'
+import { DateFormats, daysToWeeksAndDays } from '../../../server/utils/dateUtils'
+import { requirementsHtmlString } from '../../../server/utils/match'
+import { allReleaseTypes } from '../../../server/utils/applications/releaseTypeUtils'
 
 export default class BookASpacePage extends Page {
-  constructor(premisesName: string) {
-    super(`Book space in ${premisesName}`)
+  constructor() {
+    super(`Confirm booking`)
   }
 
   static visit(
     placementRequest: PlacementRequestDetail,
-    startDate: string,
-    durationDays: PlacementDates['duration'],
-    premisesName: Premises['name'],
     premisesId: Premises['id'],
-    apType: ApType,
+    arrivalDate: string,
+    departureDate: string,
     criteria: Array<Cas1SpaceBookingCharacteristic>,
   ) {
-    const queryString = createQueryString({ startDate, durationDays, premisesName, premisesId, apType, criteria })
-    const path = `${paths.v2Match.placementRequests.spaceBookings.new({ id: placementRequest.id })}?${queryString}`
+    const queryString = createQueryString({ arrivalDate, departureDate, criteria })
+    const path = `${paths.v2Match.placementRequests.spaceBookings.new({
+      id: placementRequest.id,
+      premisesId,
+    })}?${queryString}`
+
     cy.visit(path)
-    return new BookASpacePage(premisesName)
+
+    return new BookASpacePage()
   }
 
   shouldShowBookingDetails(
     placementRequest: PlacementRequestDetail,
-    startDate: string,
-    duration: PlacementDates['duration'],
-    apType: ApType,
-    criteria?: Array<Cas1SpaceCharacteristic>,
+    premises: Cas1PremisesSummary,
+    arrivalDate: string,
+    departureDate: string,
+    criteria: Array<Cas1SpaceBookingCharacteristic>,
   ): void {
-    const { endDate, placementLength } = placementDates(startDate, duration.toString())
-    cy.get('dd').contains(apTypeLabels[apType])
-    cy.get('dd').contains(DateFormats.isoDateToUIDate(startDate))
-    cy.get('dd').contains(DateFormats.isoDateToUIDate(endDate))
-    cy.get('dd').contains(placementLengthInDaysAndWeeks(placementLength))
-    cy.get('dd').contains(sentenceCase(placementRequest.gender))
-    ;(criteria || []).forEach(requirement => {
-      cy.get('li').contains(placementCriteriaLabels[requirement])
-    })
+    this.shouldContainSummaryListItems([
+      { key: { text: 'Approved Premises' }, value: { text: premises.name } },
+      // { key: { text: 'Address' }, value: { text: premises.fullAddress } },
+      { key: { text: 'Space type' }, value: { html: requirementsHtmlString(criteria) } },
+      { key: { text: 'Arrival date' }, value: { text: DateFormats.isoDateToUIDate(arrivalDate) } },
+      { key: { text: 'Departure date' }, value: { text: DateFormats.isoDateToUIDate(departureDate) } },
+      {
+        key: { text: 'Length of stay' },
+        value: { text: DateFormats.formatDuration(daysToWeeksAndDays(differenceInDays(departureDate, arrivalDate))) },
+      },
+      { key: { text: 'Release type' }, value: { text: allReleaseTypes[placementRequest.releaseType] } },
+    ])
   }
 }

server/utils/match/index.test.ts
@@ -9,6 +9,7 @@ import { when } from 'jest-when'
 import type { SummaryListItem } from '@approved-premises/ui'
 import paths from '../../paths/match'
 import {
+  cas1PremisesSummaryFactory,
   personFactory,
   placementRequestDetailFactory,
   premisesFactory,
@@ -54,6 +55,7 @@ import {
   redirectToSpaceBookingsNew,
   releaseTypeRow,
   requirementsHtmlString,
+  spaceBookingConfirmationSummaryListRows,
   spaceBookingPersonNeedsSummaryCardRows,
   spaceBookingPremisesSummaryCardRows,
   spaceRequirementsRow,
@@ -62,7 +64,6 @@ import {
   totalCapacityRow,
 } from '.'
 import { placementCriteriaLabels } from '../placementCriteriaUtils'
-import { createQueryString } from '../utils'
 import * as formUtils from '../formUtils'
 import { retrieveOptionalQuestionResponseFromFormArtifact } from '../retrieveQuestionResponseFromFormArtifact'
 import PreferredAps from '../../form-pages/apply/risk-and-need-factors/location-factors/preferredAps'
@@ -71,6 +72,7 @@ import { textValue } from '../applications/helpers'
 import { preferredApsRow } from '../placementRequests/preferredApsRow'
 import { placementRequirementsRow } from '../placementRequests/placementRequirementsRow'
 import applicationFactory from '../../testutils/factories/application'
+import { allReleaseTypes } from '../applications/releaseTypeUtils'
 
 jest.mock('../retrieveQuestionResponseFromFormArtifact')
 
@@ -258,7 +260,6 @@ describe('matchUtils', () => {
     it('returns a link to the occupancy view page', () => {
       const placementRequestId = '123'
       const premisesId = 'abc'
-      const apType = 'pipe'
       const startDate = '2025-04-14'
       const durationDays = '84'
       const spaceCharacteristics: Array<Cas1SpaceBookingCharacteristic> = ['isWheelchairDesignated', 'isSingle']
@@ -266,7 +267,6 @@ describe('matchUtils', () => {
       const result = occupancyViewLink({
         placementRequestId,
         premisesId,
-        apType,
         startDate,
         durationDays,
         spaceCharacteristics,
@@ -276,14 +276,13 @@ describe('matchUtils', () => {
         `${paths.v2Match.placementRequests.search.occupancy({
           id: placementRequestId,
           premisesId,
-        })}?apType=pipe&startDate=2025-04-14&durationDays=84&criteria=isWheelchairDesignated&criteria=isSingle`,
+        })}?startDate=2025-04-14&durationDays=84&criteria=isWheelchairDesignated&criteria=isSingle`,
       )
     })
 
     it('filters out non booking-specific search criteria', () => {
       const placementRequestId = '123'
       const premisesId = 'abc'
-      const apType = 'pipe'
       const startDate = '2025-04-14'
       const durationDays = '84'
       const spaceCharacteristics: Array<PlacementCriteria> = [
@@ -302,7 +301,6 @@ describe('matchUtils', () => {
       const result = occupancyViewLink({
         placementRequestId,
         premisesId,
-        apType,
         startDate,
         durationDays,
         spaceCharacteristics: filterToSpaceBookingCharacteristics(spaceCharacteristics),
@@ -312,43 +310,39 @@ describe('matchUtils', () => {
         `${paths.v2Match.placementRequests.search.occupancy({
           id: placementRequestId,
           premisesId,
-        })}?apType=pipe&startDate=2025-04-14&durationDays=84&criteria=isWheelchairDesignated&criteria=isSingle&criteria=hasEnSuite&criteria=isArsonSuitable`,
+        })}?startDate=2025-04-14&durationDays=84&criteria=isWheelchairDesignated&criteria=isSingle&criteria=hasEnSuite&criteria=isArsonSuitable`,
       )
     })
   })
 
   describe('redirectToSpaceBookingsNew', () => {
-    it('returns a link to the confirm page with the premises name and bed', () => {
+    it('returns a link to the confirm booking page with dates, criteria and existing query parameters', () => {
       const placementRequestId = '123'
-      const premisesName = 'Hope House'
       const premisesId = 'abc'
-      const apType = 'pipe'
-      const startDate = '2022-01-01'
-      const durationDays = '1'
-      const criteria: Array<Cas1SpaceBookingCharacteristic> = ['hasEnSuite', 'isArsonSuitable']
+      const arrivalDate = '2022-01-01'
+      const departureDate = '2022-03-05'
+      const criteria: Array<Cas1SpaceBookingCharacteristic> = ['hasEnSuite', 'isWheelchairDesignated']
+      const existingQuery = {
+        foo: 'bar',
+      }
 
       const result = redirectToSpaceBookingsNew({
         placementRequestId,
-        premisesName,
         premisesId,
-        apType,
-        startDate,
-        durationDays,
+        arrivalDate,
+        departureDate,
         criteria,
+        ...existingQuery,
       })
 
+      const expectedQueryString =
+        'arrivalDate=2022-01-01&departureDate=2022-03-05&criteria=hasEnSuite&criteria=isWheelchairDesignated&foo=bar'
+
       expect(result).toEqual(
-        `${paths.v2Match.placementRequests.spaceBookings.new({ id: placementRequestId })}${createQueryString(
-          {
-            premisesName: 'Hope House',
-            premisesId: 'abc',
-            apType: 'pipe',
-            startDate: '2022-01-01',
-            durationDays: '1',
-            criteria: ['hasEnSuite', 'isArsonSuitable'],
-          },
-          { addQueryPrefix: true, arrayFormat: 'repeat' },
-        )}`,
+        `${paths.v2Match.placementRequests.spaceBookings.new({
+          id: placementRequestId,
+          premisesId,
+        })}?${expectedQueryString}`,
       )
     })
   })
@@ -520,6 +514,31 @@ describe('matchUtils', () => {
       expect(spaceBookingPremisesSummaryCardRows(premisesName, apType)).toEqual([
         premisesNameRow(premisesName),
         apTypeRow(apType),
+      ])
+    })
+  })
+
+  describe('spaceBookingConfirmationSummaryListRows', () => {
+    it('returns summary list items for the space booking confirmation screen', () => {
+      const placementRequest = placementRequestDetailFactory.build()
+      const premises = cas1PremisesSummaryFactory.build()
+      const arrivalDate = '2025-05-23'
+      const departureDate = '2025-07-18'
+      const criteria: Array<Cas1SpaceBookingCharacteristic> = ['hasEnSuite', 'isArsonSuitable']
+
+      expect(
+        spaceBookingConfirmationSummaryListRows(placementRequest, premises, arrivalDate, departureDate, criteria),
+      ).toEqual([
+        { key: { text: 'Approved Premises' }, value: { text: premises.name } },
+        // { key: { text: 'Address' }, value: { text: premises.fullAddress } },
+        {
+          key: { text: 'Space type' },
+          value: { html: '<ul class="govuk-list"><li>En-suite bathroom</li><li>Arson offences</li></ul>' },
+        },
+        { key: { text: 'Arrival date' }, value: { text: 'Fri 23 May 2025' } },
+        { key: { text: 'Departure date' }, value: { text: 'Fri 18 Jul 2025' } },
+        { key: { text: 'Length of stay' }, value: { text: '8 weeks' } },
+        { key: { text: 'Release type' }, value: { text: allReleaseTypes[placementRequest.releaseType] } },
       ])
     })
   })

server/utils/match/index.ts
@@ -1,7 +1,8 @@
-import { addDays } from 'date-fns'
+import { addDays, differenceInDays } from 'date-fns'
 import type {
   ApType,
   ApprovedPremisesApplication,
+  Cas1PremisesSummary,
   Cas1SpaceBookingCharacteristic,
   Gender,
   PlacementCriteria,
@@ -17,6 +18,7 @@ import type {
   SpaceSearchParametersUi,
   SummaryListItem,
 } from '@approved-premises/ui'
+import { ParsedQs } from 'qs'
 import { DateFormats, daysToWeeksAndDays } from '../dateUtils'
 import { createQueryString, sentenceCase } from '../utils'
 import matchPaths from '../../paths/match'
@@ -87,14 +89,12 @@ export const placementLength = (lengthInDays: number): string => {
 export const occupancyViewLink = ({
   placementRequestId,
   premisesId,
-  apType,
   startDate,
   durationDays,
   spaceCharacteristics = [],
 }: {
   placementRequestId: string
   premisesId: string
-  apType: string
   startDate: string
   durationDays: string
   spaceCharacteristics: Array<Cas1SpaceBookingCharacteristic>
@@ -104,7 +104,6 @@ export const occupancyViewLink = ({
     premisesId,
   })}${createQueryString(
     {
-      apType,
       startDate,
       durationDays,
       criteria: spaceCharacteristics,
@@ -114,31 +113,27 @@ export const occupancyViewLink = ({
 
 export const redirectToSpaceBookingsNew = ({
   placementRequestId,
-  premisesName,
   premisesId,
-  apType,
-  startDate,
-  durationDays,
+  arrivalDate,
+  departureDate,
   criteria,
-}: {
+  ...existingQuery
+}: ParsedQs & {
   placementRequestId: string
-  premisesName: string
   premisesId: string
-  apType: string
-  startDate: string
-  durationDays: string
+  arrivalDate: string
+  departureDate: string
   criteria: Array<Cas1SpaceBookingCharacteristic>
 }): string => {
-  return `${matchPaths.v2Match.placementRequests.spaceBookings.new({ id: placementRequestId })}${createQueryString(
+  return `${matchPaths.v2Match.placementRequests.spaceBookings.new({
+    id: placementRequestId,
+    premisesId,
+  })}${createQueryString(
+    { arrivalDate, departureDate, criteria, ...existingQuery },
     {
-      premisesName,
-      premisesId,
-      apType,
-      startDate,
-      durationDays,
-      criteria,
+      addQueryPrefix: true,
+      arrayFormat: 'repeat',
     },
-    { addQueryPrefix: true, arrayFormat: 'repeat' },
   )}`
 }
 
@@ -192,6 +187,37 @@ export const occupancyViewSummaryListForMatchingDetails = (
     totalCapacityRow(totalCapacity),
     apManagerDetailsRow(managerDetails),
     spaceRequirementsRow(essentialCharacteristics),
+  ]
+}
+
+export const textRow = (label: string, value: string): SummaryListItem => ({
+  key: { text: label },
+  value: { text: value },
+})
+
+export const htmlRow = (label: string, value: string): SummaryListItem => ({
+  key: { text: label },
+  value: { html: value },
+})
+
+export const spaceBookingConfirmationSummaryListRows = (
+  placementRequest: PlacementRequestDetail,
+  premises: Cas1PremisesSummary,
+  arrivalDate: string,
+  departureDate: string,
+  criteria: Array<Cas1SpaceBookingCharacteristic>,
+): Array<SummaryListItem> => {
+  return [
+    textRow('Approved Premises', premises.name),
+    // textRow('Address', premises.fullAddress),
+    htmlRow('Space type', requirementsHtmlString(criteria)),
+    textRow('Arrival date', DateFormats.isoDateToUIDate(arrivalDate)),
+    textRow('Departure date', DateFormats.isoDateToUIDate(departureDate)),
+    textRow(
+      'Length of stay',
+      DateFormats.formatDuration(daysToWeeksAndDays(differenceInDays(departureDate, arrivalDate))),
+    ),
+    textRow('Release type', allReleaseTypes[placementRequest.releaseType]),
   ]
 }
 

ministryofjustice/hmpps-approved-premises-ui

integration_tests/pages/match/bookASpacePage.ts
@@ -1,54 +1,53 @@
-import {
-  ApType,
-  Cas1SpaceBookingCharacteristic,
-  Cas1SpaceCharacteristic,
-  PlacementDates,
-  PlacementRequestDetail,
-  Premises,
-} from '@approved-premises/api'
+import { Cas1Premises, Cas1SpaceBookingCharacteristic, PlacementRequestDetail, Premises } from '@approved-premises/api'
+import { differenceInDays } from 'date-fns'
 import Page from '../page'
 import paths from '../../../server/paths/match'
-import { createQueryString, sentenceCase } from '../../../server/utils/utils'
-import { DateFormats } from '../../../server/utils/dateUtils'
-import { placementDates, placementLength as placementLengthInDaysAndWeeks } from '../../../server/utils/match'
-import { placementCriteriaLabels } from '../../../server/utils/placementCriteriaUtils'
-import { apTypeLabels } from '../../../server/utils/apTypeLabels'
+import { createQueryString } from '../../../server/utils/utils'
+import { DateFormats, daysToWeeksAndDays } from '../../../server/utils/dateUtils'
+import { requirementsHtmlString } from '../../../server/utils/match'
+import { allReleaseTypes } from '../../../server/utils/applications/releaseTypeUtils'
 
 export default class BookASpacePage extends Page {
-  constructor(premisesName: string) {
-    super(`Book space in ${premisesName}`)
+  constructor() {
+    super(`Confirm booking`)
   }
 
   static visit(
     placementRequest: PlacementRequestDetail,
-    startDate: string,
-    durationDays: PlacementDates['duration'],
-    premisesName: Premises['name'],
     premisesId: Premises['id'],
-    apType: ApType,
+    arrivalDate: string,
+    departureDate: string,
     criteria: Array<Cas1SpaceBookingCharacteristic>,
   ) {
-    const queryString = createQueryString({ startDate, durationDays, premisesName, premisesId, apType, criteria })
-    const path = `${paths.v2Match.placementRequests.spaceBookings.new({ id: placementRequest.id })}?${queryString}`
+    const queryString = createQueryString({ arrivalDate, departureDate, criteria })
+    const path = `${paths.v2Match.placementRequests.spaceBookings.new({
+      id: placementRequest.id,
+      premisesId,
+    })}?${queryString}`
+
     cy.visit(path)
-    return new BookASpacePage(premisesName)
+
+    return new BookASpacePage()
   }
 
   shouldShowBookingDetails(
     placementRequest: PlacementRequestDetail,
-    startDate: string,
-    duration: PlacementDates['duration'],
-    apType: ApType,
-    criteria?: Array<Cas1SpaceCharacteristic>,
+    premises: Cas1Premises,
+    arrivalDate: string,
+    departureDate: string,
+    criteria: Array<Cas1SpaceBookingCharacteristic>,
   ): void {
-    const { endDate, placementLength } = placementDates(startDate, duration.toString())
-    cy.get('dd').contains(apTypeLabels[apType])
-    cy.get('dd').contains(DateFormats.isoDateToUIDate(startDate))
-    cy.get('dd').contains(DateFormats.isoDateToUIDate(endDate))
-    cy.get('dd').contains(placementLengthInDaysAndWeeks(placementLength))
-    cy.get('dd').contains(sentenceCase(placementRequest.gender))
-    ;(criteria || []).forEach(requirement => {
-      cy.get('li').contains(placementCriteriaLabels[requirement])
-    })
+    this.shouldContainSummaryListItems([
+      { key: { text: 'Approved Premises' }, value: { text: premises.name } },
+      // { key: { text: 'Address' }, value: { text: premises.fullAddress } },
+      { key: { text: 'Space type' }, value: { html: requirementsHtmlString(criteria) } },
+      { key: { text: 'Arrival date' }, value: { text: DateFormats.isoDateToUIDate(arrivalDate) } },
+      { key: { text: 'Departure date' }, value: { text: DateFormats.isoDateToUIDate(departureDate) } },
+      {
+        key: { text: 'Length of stay' },
+        value: { text: DateFormats.formatDuration(daysToWeeksAndDays(differenceInDays(departureDate, arrivalDate))) },
+      },
+      { key: { text: 'Release type' }, value: { text: allReleaseTypes[placementRequest.releaseType] } },
+    ])
   }
 }

server/utils/match/index.test.ts
@@ -9,6 +9,7 @@ import { when } from 'jest-when'
 import type { SummaryListItem } from '@approved-premises/ui'
 import paths from '../../paths/match'
 import {
+  cas1PremisesFactory,
   personFactory,
   placementRequestDetailFactory,
   premisesFactory,
@@ -51,6 +52,7 @@ import {
   redirectToSpaceBookingsNew,
   releaseTypeRow,
   requirementsHtmlString,
+  spaceBookingConfirmationSummaryListRows,
   spaceBookingPersonNeedsSummaryCardRows,
   spaceBookingPremisesSummaryCardRows,
   spaceRequirementsRow,
@@ -59,7 +61,6 @@ import {
   totalCapacityRow,
 } from '.'
 import { placementCriteriaLabels } from '../placementCriteriaUtils'
-import { createQueryString } from '../utils'
 import * as formUtils from '../formUtils'
 import { retrieveOptionalQuestionResponseFromFormArtifact } from '../retrieveQuestionResponseFromFormArtifact'
 import PreferredAps from '../../form-pages/apply/risk-and-need-factors/location-factors/preferredAps'
@@ -68,6 +69,7 @@ import { textValue } from '../applications/helpers'
 import { preferredApsRow } from '../placementRequests/preferredApsRow'
 import { placementRequirementsRow } from '../placementRequests/placementRequirementsRow'
 import applicationFactory from '../../testutils/factories/application'
+import { allReleaseTypes } from '../applications/releaseTypeUtils'
 
 jest.mock('../retrieveQuestionResponseFromFormArtifact')
 
@@ -230,7 +232,6 @@ describe('matchUtils', () => {
     it('returns a link to the occupancy view page', () => {
       const placementRequestId = '123'
       const premisesId = 'abc'
-      const apType = 'pipe'
       const startDate = '2025-04-14'
       const durationDays = '84'
       const spaceCharacteristics: Array<Cas1SpaceBookingCharacteristic> = ['isWheelchairDesignated', 'isSingle']
@@ -238,7 +239,6 @@ describe('matchUtils', () => {
       const result = occupancyViewLink({
         placementRequestId,
         premisesId,
-        apType,
         startDate,
         durationDays,
         spaceCharacteristics,
@@ -248,14 +248,13 @@ describe('matchUtils', () => {
         `${paths.v2Match.placementRequests.search.occupancy({
           id: placementRequestId,
           premisesId,
-        })}?apType=pipe&startDate=2025-04-14&durationDays=84&criteria=isWheelchairDesignated&criteria=isSingle`,
+        })}?startDate=2025-04-14&durationDays=84&criteria=isWheelchairDesignated&criteria=isSingle`,
       )
     })
 
     it('filters out non booking-specific search criteria', () => {
       const placementRequestId = '123'
       const premisesId = 'abc'
-      const apType = 'pipe'
       const startDate = '2025-04-14'
       const durationDays = '84'
       const spaceCharacteristics: Array<PlacementCriteria> = [
@@ -274,7 +273,6 @@ describe('matchUtils', () => {
       const result = occupancyViewLink({
         placementRequestId,
         premisesId,
-        apType,
         startDate,
         durationDays,
         spaceCharacteristics: filterToSpaceBookingCharacteristics(spaceCharacteristics),
@@ -284,43 +282,39 @@ describe('matchUtils', () => {
         `${paths.v2Match.placementRequests.search.occupancy({
           id: placementRequestId,
           premisesId,
-        })}?apType=pipe&startDate=2025-04-14&durationDays=84&criteria=isWheelchairDesignated&criteria=isSingle&criteria=hasEnSuite&criteria=isArsonSuitable`,
+        })}?startDate=2025-04-14&durationDays=84&criteria=isWheelchairDesignated&criteria=isSingle&criteria=hasEnSuite&criteria=isArsonSuitable`,
       )
     })
   })
 
   describe('redirectToSpaceBookingsNew', () => {
-    it('returns a link to the confirm page with the premises name and bed', () => {
+    it('returns a link to the confirm booking page with dates, criteria and existing query parameters', () => {
       const placementRequestId = '123'
-      const premisesName = 'Hope House'
       const premisesId = 'abc'
-      const apType = 'pipe'
-      const startDate = '2022-01-01'
-      const durationDays = '1'
-      const criteria: Array<Cas1SpaceBookingCharacteristic> = ['hasEnSuite', 'isArsonSuitable']
+      const arrivalDate = '2022-01-01'
+      const departureDate = '2022-03-05'
+      const criteria: Array<Cas1SpaceBookingCharacteristic> = ['hasEnSuite', 'isWheelchairDesignated']
+      const existingQuery = {
+        foo: 'bar',
+      }
 
       const result = redirectToSpaceBookingsNew({
         placementRequestId,
-        premisesName,
         premisesId,
-        apType,
-        startDate,
-        durationDays,
+        arrivalDate,
+        departureDate,
         criteria,
+        ...existingQuery,
       })
 
+      const expectedQueryString =
+        'arrivalDate=2022-01-01&departureDate=2022-03-05&criteria=hasEnSuite&criteria=isWheelchairDesignated&foo=bar'
+
       expect(result).toEqual(
-        `${paths.v2Match.placementRequests.spaceBookings.new({ id: placementRequestId })}${createQueryString(
-          {
-            premisesName: 'Hope House',
-            premisesId: 'abc',
-            apType: 'pipe',
-            startDate: '2022-01-01',
-            durationDays: '1',
-            criteria: ['hasEnSuite', 'isArsonSuitable'],
-          },
-          { addQueryPrefix: true, arrayFormat: 'repeat' },
-        )}`,
+        `${paths.v2Match.placementRequests.spaceBookings.new({
+          id: placementRequestId,
+          premisesId,
+        })}?${expectedQueryString}`,
       )
     })
   })
@@ -492,6 +486,31 @@ describe('matchUtils', () => {
       expect(spaceBookingPremisesSummaryCardRows(premisesName, apType)).toEqual([
         premisesNameRow(premisesName),
         apTypeRow(apType),
+      ])
+    })
+  })
+
+  describe('spaceBookingConfirmationSummaryListRows', () => {
+    it('returns summary list items for the space booking confirmation screen', () => {
+      const placementRequest = placementRequestDetailFactory.build()
+      const premises = cas1PremisesFactory.build()
+      const arrivalDate = '2025-05-23'
+      const departureDate = '2025-07-18'
+      const criteria: Array<Cas1SpaceBookingCharacteristic> = ['hasEnSuite', 'isArsonSuitable']
+
+      expect(
+        spaceBookingConfirmationSummaryListRows(placementRequest, premises, arrivalDate, departureDate, criteria),
+      ).toEqual([
+        { key: { text: 'Approved Premises' }, value: { text: premises.name } },
+        // { key: { text: 'Address' }, value: { text: premises.fullAddress } },
+        {
+          key: { text: 'Space type' },
+          value: { html: '<ul class="govuk-list"><li>En-suite bathroom</li><li>Arson offences</li></ul>' },
+        },
+        { key: { text: 'Arrival date' }, value: { text: 'Fri 23 May 2025' } },
+        { key: { text: 'Departure date' }, value: { text: 'Fri 18 Jul 2025' } },
+        { key: { text: 'Length of stay' }, value: { text: '8 weeks' } },
+        { key: { text: 'Release type' }, value: { text: allReleaseTypes[placementRequest.releaseType] } },
       ])
     })
   })

server/utils/match/index.ts
@@ -1,7 +1,8 @@
-import { addDays } from 'date-fns'
+import { addDays, differenceInDays } from 'date-fns'
 import type {
   ApType,
   ApprovedPremisesApplication,
+  Cas1Premises,
   Cas1SpaceBookingCharacteristic,
   Gender,
   PlacementCriteria,
@@ -17,6 +18,7 @@ import type {
   SpaceSearchParametersUi,
   SummaryListItem,
 } from '@approved-premises/ui'
+import { ParsedQs } from 'qs'
 import { DateFormats, daysToWeeksAndDays } from '../dateUtils'
 import { createQueryString, sentenceCase } from '../utils'
 import matchPaths from '../../paths/match'
@@ -68,14 +70,12 @@ export const placementLength = (lengthInDays: number): string => {
 export const occupancyViewLink = ({
   placementRequestId,
   premisesId,
-  apType,
   startDate,
   durationDays,
   spaceCharacteristics = [],
 }: {
   placementRequestId: string
   premisesId: string
-  apType: string
   startDate: string
   durationDays: string
   spaceCharacteristics: Array<Cas1SpaceBookingCharacteristic>
@@ -85,7 +85,6 @@ export const occupancyViewLink = ({
     premisesId,
   })}${createQueryString(
     {
-      apType,
       startDate,
       durationDays,
       criteria: spaceCharacteristics,
@@ -95,31 +94,27 @@ export const occupancyViewLink = ({
 
 export const redirectToSpaceBookingsNew = ({
   placementRequestId,
-  premisesName,
   premisesId,
-  apType,
-  startDate,
-  durationDays,
+  arrivalDate,
+  departureDate,
   criteria,
-}: {
+  ...existingQuery
+}: ParsedQs & {
   placementRequestId: string
-  premisesName: string
   premisesId: string
-  apType: string
-  startDate: string
-  durationDays: string
+  arrivalDate: string
+  departureDate: string
   criteria: Array<Cas1SpaceBookingCharacteristic>
 }): string => {
-  return `${matchPaths.v2Match.placementRequests.spaceBookings.new({ id: placementRequestId })}${createQueryString(
+  return `${matchPaths.v2Match.placementRequests.spaceBookings.new({
+    id: placementRequestId,
+    premisesId,
+  })}${createQueryString(
+    { arrivalDate, departureDate, criteria, ...existingQuery },
     {
-      premisesName,
-      premisesId,
-      apType,
-      startDate,
-      durationDays,
-      criteria,
+      addQueryPrefix: true,
+      arrayFormat: 'repeat',
     },
-    { addQueryPrefix: true, arrayFormat: 'repeat' },
   )}`
 }
 
@@ -173,6 +168,37 @@ export const occupancyViewSummaryListForMatchingDetails = (
     totalCapacityRow(totalCapacity),
     apManagerDetailsRow(managerDetails),
     spaceRequirementsRow(essentialCharacteristics),
+  ]
+}
+
+export const textRow = (label: string, value: string): SummaryListItem => ({
+  key: { text: label },
+  value: { text: value },
+})
+
+export const htmlRow = (label: string, value: string): SummaryListItem => ({
+  key: { text: label },
+  value: { html: value },
+})
+
+export const spaceBookingConfirmationSummaryListRows = (
+  placementRequest: PlacementRequestDetail,
+  premises: Cas1Premises,
+  arrivalDate: string,
+  departureDate: string,
+  criteria: Array<Cas1SpaceBookingCharacteristic>,
+): Array<SummaryListItem> => {
+  return [
+    textRow('Approved Premises', premises.name),
+    // textRow('Address', premises.fullAddress),
+    htmlRow('Space type', requirementsHtmlString(criteria)),
+    textRow('Arrival date', DateFormats.isoDateToUIDate(arrivalDate)),
+    textRow('Departure date', DateFormats.isoDateToUIDate(departureDate)),
+    textRow(
+      'Length of stay',
+      DateFormats.formatDuration(daysToWeeksAndDays(differenceInDays(departureDate, arrivalDate))),
+    ),
+    textRow('Release type', allReleaseTypes[placementRequest.releaseType]),
   ]
 }
 