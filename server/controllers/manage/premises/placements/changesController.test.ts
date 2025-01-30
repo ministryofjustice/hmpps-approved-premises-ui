984a18707ba737b2da4476efa708093970a4b0a0
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2025-01-30 14:50:45 +0000

    Add filter form

--- server/controllers/manage/premises/placements/changesController.test.ts
+++ server/controllers/manage/premises/placements/changesController.test.ts
@@ -1,5 +1,7 @@
 import { DeepMocked, createMock } from '@golevelup/ts-jest'
 import type { NextFunction, Request, Response } from 'express'
+import { Cas1SpaceBookingCharacteristic } from '@approved-premises/api'
+import { differenceInDays } from 'date-fns'
 import { PlacementService, PremisesService } from '../../../../services'
 import {
   cas1PremiseCapacityFactory,
@@ -12,7 +14,10 @@ import { occupancyCalendar } from '../../../../utils/match/occupancyCalendar'
 import paths from '../../../../paths/match'
 import { placementOverviewSummary } from '../../../../utils/placements'
 import { filterRoomLevelCriteria } from '../../../../utils/match/spaceSearch'
-import { createQueryString } from '../../../../utils/utils'
+import { createQueryString, makeArrayOfType } from '../../../../utils/utils'
+import { durationSelectOptions, occupancyCriteriaMap } from '../../../../utils/match/occupancy'
+import { convertKeyValuePairToCheckBoxItems } from '../../../../utils/formUtils'
+import { DateFormats } from '../../../../utils/dateUtils'
 
 describe('changesController', () => {
   const token = 'SOME_TOKEN'
@@ -38,19 +43,21 @@ describe('changesController', () => {
       user: { token },
       params: { premisesId: premises.id, placementId: placement.id },
       flash: jest.fn(),
+      query: {},
     })
   })
 
-  it('renders the change placement screen with current booking setup', async () => {
+  it('renders the change placement screen with current booking information', async () => {
     const requestHandler = changesController.new()
     await requestHandler(request, response, next)
 
-    const filteredCriteria = filterRoomLevelCriteria(placement.requirements.essentialCharacteristics)
-    const placeholderDetailsUrl = `${paths.v2Match.placementRequests.search.dayOccupancy({
+    const expectedCriteria = filterRoomLevelCriteria(placement.requirements.essentialCharacteristics)
+    const expectedPlaceholderDayUrl = `${paths.v2Match.placementRequests.search.dayOccupancy({
       id: placement.requestForPlacementId,
       premisesId: premises.id,
       date: ':date',
-    })}?${createQueryString({ criteria: filteredCriteria, excludeSpaceBookingId: placement.id })}`
+    })}?${createQueryString({ criteria: expectedCriteria, excludeSpaceBookingId: placement.id })}`
+    const expectedDuration = differenceInDays(placement.expectedDepartureDate, placement.expectedArrivalDate)
 
     expect(placementService.getPlacement).toHaveBeenCalledWith(token, placement.id)
     expect(premisesService.getCapacity).toHaveBeenCalledWith(token, premises.id, {
@@ -62,10 +69,85 @@ describe('changesController', () => {
       pageHeading: 'Change placement dates',
       placement,
       placementSummary: placementOverviewSummary(placement),
-      summary: occupancySummary(capacity.capacity, filteredCriteria),
-      calendar: occupancyCalendar(capacity.capacity, placeholderDetailsUrl, filteredCriteria),
+      durationOptions: durationSelectOptions(expectedDuration),
+      criteriaOptions: convertKeyValuePairToCheckBoxItems(occupancyCriteriaMap, expectedCriteria),
+      startDate: placement.expectedArrivalDate,
+      ...DateFormats.isoDateToDateInputs(placement.expectedArrivalDate, 'startDate'),
+      durationDays: expectedDuration,
+      summary: occupancySummary(capacity.capacity, expectedCriteria),
+      calendar: occupancyCalendar(capacity.capacity, expectedPlaceholderDayUrl, expectedCriteria),
       errorSummary: [],
       errors: {},
+    })
+  })
+
+  describe('when filtering the view', () => {
+    it('renders the change placement screen with a filtered view', async () => {
+      const query = {
+        'startDate-day': '12',
+        'startDate-month': '5',
+        'startDate-year': '2025',
+        durationDays: '7',
+        criteria: 'hasEnSuite',
+      }
+
+      const filterCriteria = makeArrayOfType<Cas1SpaceBookingCharacteristic>(query.criteria)
+
+      const requestHandler = changesController.new()
+      await requestHandler({ ...request, query }, response, next)
+
+      const placeholderDetailsUrl = `${paths.v2Match.placementRequests.search.dayOccupancy({
+        id: placement.requestForPlacementId,
+        premisesId: premises.id,
+        date: ':date',
+      })}?${createQueryString({ criteria: filterCriteria, excludeSpaceBookingId: placement.id })}`
+
+      expect(premisesService.getCapacity).toHaveBeenCalledWith(token, premises.id, {
+        startDate: '2025-05-12',
+        endDate: '2025-05-19',
+        excludeSpaceBookingId: placement.id,
+      })
+      expect(response.render.mock.lastCall[1]).toEqual(
+        expect.objectContaining({
+          summary: occupancySummary(capacity.capacity, filterCriteria),
+          calendar: occupancyCalendar(capacity.capacity, placeholderDetailsUrl, filterCriteria),
+          durationOptions: durationSelectOptions(Number(query.durationDays)),
+          criteriaOptions: convertKeyValuePairToCheckBoxItems(occupancyCriteriaMap, filterCriteria),
+          'startDate-day': '12',
+          'startDate-month': '5',
+          'startDate-year': '2025',
+        }),
+      )
+    })
+
+    it('shows an error and does not display summary or calendar if the filter date is invalid', async () => {
+      const query = {
+        'startDate-day': '12',
+        'startDate-year': '2025',
+        durationDays: '7',
+      }
+
+      const requestHandler = changesController.new()
+      await requestHandler({ ...request, query }, response, next)
+
+      expect(premisesService.getCapacity).not.toHaveBeenCalled()
+      expect(response.render.mock.lastCall[1]).toEqual(
+        expect.objectContaining({
+          summary: undefined,
+          calendar: undefined,
+          durationOptions: durationSelectOptions(Number(query.durationDays)),
+          criteriaOptions: convertKeyValuePairToCheckBoxItems(occupancyCriteriaMap, []),
+          errorSummary: [{ text: 'Enter a valid date', href: '#startDate' }],
+          errors: {
+            startDate: {
+              attributes: { 'data-cy-error-startDate': true },
+              text: 'Enter a valid date',
+            },
+          },
+          'startDate-day': '12',
+          'startDate-year': '2025',
+        }),
+      )
     })
   })
 })
--- server/controllers/manage/premises/placements/changesController.ts
+++ server/controllers/manage/premises/placements/changesController.ts
@@ -1,13 +1,31 @@
 import { type Request, RequestHandler, type Response } from 'express'
 import { Cas1SpaceBookingCharacteristic } from '@approved-premises/api'
+import { ObjectWithDateParts } from '@approved-premises/ui'
+import { addDays, differenceInDays } from 'date-fns'
 import { PlacementService, PremisesService } from '../../../../services'
-import { fetchErrorsAndUserInput } from '../../../../utils/validation'
+import { fetchErrorsAndUserInput, generateErrorMessages, generateErrorSummary } from '../../../../utils/validation'
 import { occupancySummary } from '../../../../utils/match'
 import paths from '../../../../paths/match'
-import { occupancyCalendar } from '../../../../utils/match/occupancyCalendar'
+import { Calendar, occupancyCalendar } from '../../../../utils/match/occupancyCalendar'
 import { placementOverviewSummary } from '../../../../utils/placements'
 import { filterRoomLevelCriteria } from '../../../../utils/match/spaceSearch'
-import { createQueryString } from '../../../../utils/utils'
+import { createQueryString, makeArrayOfType } from '../../../../utils/utils'
+import { DateFormats, dateAndTimeInputsAreValidDates } from '../../../../utils/dateUtils'
+import { CriteriaQuery } from '../../../match/placementRequests/occupancyViewController'
+import { convertKeyValuePairToCheckBoxItems } from '../../../../utils/formUtils'
+import { durationSelectOptions, occupancyCriteriaMap } from '../../../../utils/match/occupancy'
+import { OccupancySummary } from '../../../../utils/match/occupancySummary'
+
+interface ViewRequest extends Request {
+  params: {
+    premisesId: string
+    placementId: string
+  }
+  query: ObjectWithDateParts<'startDate'> & {
+    durationDays: string
+    criteria: CriteriaQuery
+  }
+}
 
 export default class ChangesController {
   constructor(
@@ -16,40 +34,76 @@ export default class ChangesController {
   ) {}
 
   new(): RequestHandler {
-    return async (req: Request, res: Response) => {
+    return async (req: ViewRequest, res: Response) => {
       const { token } = req.user
       const { premisesId, placementId } = req.params
+      const { durationDays: queryDurationDays, criteria, ...startDateParts } = req.query
       const { errors, errorSummary, userInput } = fetchErrorsAndUserInput(req)
 
       const placement = await this.placementService.getPlacement(token, placementId)
-      const capacity = await this.premisesService.getCapacity(token, premisesId, {
-        startDate: placement.expectedArrivalDate,
-        endDate: placement.expectedDepartureDate,
-        excludeSpaceBookingId: placement.id,
-      })
-      const bookingCriteria: Array<Cas1SpaceBookingCharacteristic> = filterRoomLevelCriteria(
+
+      let startDate = placement.expectedArrivalDate
+      let endDate = placement.expectedDepartureDate
+      let durationDays = differenceInDays(endDate, startDate)
+      let bookingCriteria: Array<Cas1SpaceBookingCharacteristic> = filterRoomLevelCriteria(
         placement.requirements.essentialCharacteristics,
       )
-      const placeholderDetailsUrl = `${paths.v2Match.placementRequests.search.dayOccupancy({
-        id: placement.requestForPlacementId,
-        premisesId,
-        date: ':date',
-      })}${createQueryString(
-        {
-          criteria: bookingCriteria,
+
+      if (queryDurationDays) {
+        durationDays = Number(queryDurationDays)
+        bookingCriteria = makeArrayOfType(criteria)
+
+        if (dateAndTimeInputsAreValidDates(startDateParts, 'startDate')) {
+          startDate = DateFormats.dateAndTimeInputsToIsoString(startDateParts, 'startDate').startDate
+          endDate = DateFormats.dateObjToIsoDate(addDays(startDate, durationDays))
+        } else {
+          const startDateError = {
+            startDate: 'Enter a valid date',
+          }
+          errorSummary.push(...generateErrorSummary(startDateError))
+          Object.assign(errors, generateErrorMessages(startDateError))
+        }
+      }
+
+      let summary: OccupancySummary
+      let calendar: Calendar
+
+      if (!errors.startDate) {
+        const capacity = await this.premisesService.getCapacity(token, premisesId, {
+          startDate,
+          endDate,
           excludeSpaceBookingId: placement.id,
-        },
-        { arrayFormat: 'repeat', addQueryPrefix: true },
-      )}`
+        })
+        const placeholderDetailsUrl = `${paths.v2Match.placementRequests.search.dayOccupancy({
+          id: placement.requestForPlacementId,
+          premisesId,
+          date: ':date',
+        })}${createQueryString(
+          {
+            criteria: bookingCriteria,
+            excludeSpaceBookingId: placement.id,
+          },
+          { arrayFormat: 'repeat', addQueryPrefix: true },
+        )}`
+
+        summary = occupancySummary(capacity.capacity, bookingCriteria)
+        calendar = occupancyCalendar(capacity.capacity, placeholderDetailsUrl, bookingCriteria)
+      }
 
       return res.render('manage/premises/placements/changes', {
         pageHeading: 'Change placement dates',
         placement,
+        startDate,
+        ...DateFormats.isoDateToDateInputs(startDate, 'startDate'),
+        durationDays,
         placementSummary: placementOverviewSummary(placement),
-        summary: occupancySummary(capacity.capacity, bookingCriteria),
-        calendar: occupancyCalendar(capacity.capacity, placeholderDetailsUrl, bookingCriteria),
+        durationOptions: durationSelectOptions(durationDays),
+        criteriaOptions: convertKeyValuePairToCheckBoxItems(occupancyCriteriaMap, bookingCriteria),
+        summary,
+        calendar,
         errors,
         errorSummary,
+        ...startDateParts,
         ...userInput,
       })
     }
