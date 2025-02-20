ministryofjustice/approved-premises-ui

APS-1912 Extend booking

integration_tests/tests/manage/placements/changes.cy.ts
@@ -17,6 +17,8 @@ import { DateFormats } from '../../../../server/utils/dateUtils'
 import ChangePlacementConfirmPage from '../../../pages/manage/placements/changes/confirm'
 import { occupancyCriteriaMap } from '../../../../server/utils/match/occupancy'
 import apiPaths from '../../../../server/paths/api'
+import { Cas1SpaceBooking } from '../../../../server/@types/shared/models/Cas1SpaceBooking'
+import { Cas1UpdateSpaceBooking } from '../../../../server/@types/shared/models/Cas1UpdateSpaceBooking'
 
 context('Change Placement', () => {
   const expectedArrivalDate = DateFormats.dateObjToIsoDate(faker.date.soon())
@@ -24,18 +26,28 @@ context('Change Placement', () => {
 
   const person = fullPersonFactory.build()
   const premises = cas1PremisesFactory.build()
-  const placement = cas1SpaceBookingFactory.upcoming().build({
-    premises: { name: premises.name, id: premises.id },
-    expectedArrivalDate,
-    expectedDepartureDate,
-  })
-  const placementRequestDetail = placementRequestDetailFactory.build({
-    person,
-    status: 'matched',
-    booking: bookingSummaryFactory.fromSpaceBooking(placement).build(),
-  })
-  placement.requestForPlacementId = placementRequestDetail.id
-  const capacity = cas1PremiseCapacityFactory.build()
+
+  const setupMocks = (placement: Cas1SpaceBooking) => {
+    const placementRequestDetail = placementRequestDetailFactory.build({
+      person,
+      status: 'matched',
+      booking: bookingSummaryFactory.fromSpaceBooking(placement).build(),
+    })
+    placement.requestForPlacementId = placementRequestDetail.id
+    const capacity = cas1PremiseCapacityFactory.build()
+    cy.task('stubSinglePremises', premises)
+    cy.task('stubPremiseCapacity', {
+      premisesId: premises.id,
+      premiseCapacity: capacity,
+      startDate: placement.actualArrivalDateOnly || placement.expectedArrivalDate,
+      endDate: placement.expectedDepartureDate,
+      excludeSpaceBookingId: placement.id,
+    })
+    cy.task('stubPlacementRequest', placementRequestDetail)
+    cy.task('stubSpaceBookingGetWithoutPremises', placement)
+    cy.task('stubSpaceBookingUpdate', { premisesId: premises.id, placementId: placement.id })
+    return { placement, placementRequestDetail }
+  }
 
   beforeEach(() => {
     cy.task('reset')
@@ -44,21 +56,15 @@ context('Change Placement', () => {
 
     // Given I am signed in
     signIn(['workflow_manager', 'future_manager'], ['cas1_space_booking_create'])
-
-    cy.task('stubSinglePremises', premises)
-    cy.task('stubPremiseCapacity', {
-      premisesId: premises.id,
-      premiseCapacity: capacity,
-      startDate: placement.expectedArrivalDate,
-      endDate: placement.expectedDepartureDate,
-      excludeSpaceBookingId: placement.id,
-    })
-    cy.task('stubPlacementRequest', placementRequestDetail)
-    cy.task('stubSpaceBookingGetWithoutPremises', placement)
-    cy.task('stubSpaceBookingUpdate', { premisesId: premises.id, placementId: placement.id })
   })
 
   it('allows me to change the dates and criteria of a space booking', () => {
+    const placement = cas1SpaceBookingFactory.upcoming().build({
+      premises: { name: premises.name, id: premises.id },
+      expectedArrivalDate,
+      expectedDepartureDate,
+    })
+    const { placementRequestDetail } = setupMocks(placement)
     // When I visit a placement request
     const placementRequestPage = ShowPage.visit(placementRequestDetail)
 
@@ -72,7 +78,7 @@ context('Change Placement', () => {
     changePlacementPage.shouldShowPlacementOverview()
 
     // And I should see the filter form with default populated values from the placement
-    const selectedCriteria = filterRoomLevelCriteria(placement.requirements.essentialCharacteristics)
+    const selectedCriteria = filterRoomLevelCriteria(placement.characteristics)
     const selectedCriteriaLabels = selectedCriteria.map(criterion => occupancyCriteriaMap[criterion])
     changePlacementPage.shouldShowFilters(placement.expectedArrivalDate, 'Up to 12 weeks', selectedCriteriaLabels)
 
@@ -82,11 +88,11 @@ context('Change Placement', () => {
     // And I can see the current placement dates in the hints
     changePlacementPage.shouldShowDateFieldHint(
       'arrivalDate',
-      `Expected arrival date: ${DateFormats.isoDateToUIDate(placement.expectedArrivalDate, { format: 'dateFieldHint' })}`,
+      `Current arrival date: ${DateFormats.isoDateToUIDate(placement.expectedArrivalDate, { format: 'dateFieldHint' })}`,
     )
     changePlacementPage.shouldShowDateFieldHint(
       'departureDate',
-      `Expected departure date: ${DateFormats.isoDateToUIDate(placement.expectedDepartureDate, { format: 'dateFieldHint' })}`,
+      `Current departure date: ${DateFormats.isoDateToUIDate(placement.expectedDepartureDate, { format: 'dateFieldHint' })}`,
     )
 
     // When I submit the filters with an invalid date
@@ -152,6 +158,73 @@ context('Change Placement', () => {
         departureDate,
         characteristics: criteria,
       })
+    })
+  })
+
+  it('allows me to extend the end date of placement after arrival', () => {
+    const actualArrivalDateOnly = DateFormats.dateObjToIsoDate(addDays(expectedArrivalDate, 20))
+    const arrivedPlacement = cas1SpaceBookingFactory.upcoming().build({
+      premises: { name: premises.name, id: premises.id },
+      expectedArrivalDate,
+      expectedDepartureDate: DateFormats.dateObjToIsoDate(addDays(expectedArrivalDate, 45)),
+      actualArrivalDateOnly,
+    })
+    const newDepartureDate = DateFormats.dateObjToIsoDate(addDays(arrivedPlacement.expectedDepartureDate, 5))
+
+    const { placementRequestDetail } = setupMocks(arrivedPlacement)
+    // When I visit a placement request
+
+    const placementRequestPage = ShowPage.visit(placementRequestDetail)
+
+    // When I click on the amend booking button
+    placementRequestPage.clickAction('Change placement')
+
+    // Then I should see the Change Placement page
+    const page = Page.verifyOnPage(ChangePlacementPage, arrivedPlacement)
+
+    // And I should see an overview of the placement
+    page.shouldShowPlacementOverview()
+
+    // And I can see the calendar for the rounded-up period in the duration selector, from the actual arrival date
+    page.shouldShowCalendarHeading(actualArrivalDateOnly, 56)
+
+    // And I can see the current placement dates in the departure date hint
+    page.shouldShowDateFieldHint(
+      'departureDate',
+      `Current departure date: ${DateFormats.isoDateToUIDate(arrivedPlacement.expectedDepartureDate, { format: 'dateFieldHint' })}`,
+    )
+    // And I should only be able to change the departure date
+    cy.get('h2').contains('Change placement')
+
+    // When I set a new departure date and submit
+    page.clearAndCompleteDateInputs('departureDate', newDepartureDate)
+    page.clickContinue()
+
+    // Then I should see the confirmation page with the correct dates
+    const confirmPage = Page.verifyOnPage(ChangePlacementConfirmPage, premises, {
+      arrivalDate: arrivedPlacement.expectedArrivalDate,
+      departureDate: DateFormats.dateObjToIsoDate(addDays(arrivedPlacement.expectedDepartureDate, 5)),
+      criteria: arrivedPlacement.characteristics.filter(characteristic => occupancyCriteriaMap[characteristic]),
+    })
+    confirmPage.shouldShowProposedChanges()
+
+    // When I submit the confirmation page
+    confirmPage.clickSubmit()
+
+    // Then I should see the placement request page again with a success banner
+    Page.verifyOnPage(ShowPage, placementRequestDetail)
+
+    placementRequestPage.shouldShowBanner('Booking changed successfully')
+
+    // And the booking changes should have been sent to the API
+    cy.task('verifyApiPatch', apiPaths.premises.placements.show).then(body => {
+      const { characteristics, arrivalDate, departureDate } = body as Cas1UpdateSpaceBooking
+
+      expect(arrivalDate).equal(undefined)
+      expect(departureDate).equal(newDepartureDate)
+      expect(characteristics.sort()).to.deep.equal(
+        arrivedPlacement.characteristics.filter(characteristic => occupancyCriteriaMap[characteristic]).sort(),
+      )
     })
   })
 })

server/utils/placements/index.ts
@@ -138,13 +138,10 @@ export const placementOverviewSummary = (placement: Cas1SpaceBooking): SummaryLi
     summaryRow('Approved premises', placement.premises.name),
     summaryRow('Date of match', formatDate(placement.createdAt)),
     summaryRow('Expected arrival date', formatDate(placement.expectedArrivalDate)),
+    summaryRow('Actual arrival date', formatDate(placement.actualArrivalDateOnly)),
     summaryRow('Expected departure date', formatDate(placement.expectedDepartureDate)),
-    summaryListItem(
-      'Room criteria',
-      requirementsHtmlString(placement.requirements.essentialCharacteristics, occupancyCriteriaMap),
-      'html',
-    ),
-  ],
+    summaryListItem('Room criteria', requirementsHtmlString(placement.characteristics, occupancyCriteriaMap), 'html'),
+  ].filter(Boolean),
 })
 
 export const arrivalInformation = (placement: Cas1SpaceBooking): SummaryList => {
@@ -188,7 +185,7 @@ export const departureInformation = (placement: Cas1SpaceBooking): SummaryList =
 }
 
 export const requirementsInformation = (placement: Cas1SpaceBooking): SummaryList => {
-  const requirements = placement.requirements.essentialCharacteristics
+  const requirements = placement.characteristics
   const apType =
     apTypeCriteriaLabels[requirements.find(requirement => specialistApTypeCriteria.includes(requirement)) || 'normal']
   const apRequirements = filterApLevelCriteria(requirements)

ministryofjustice/hmpps-approved-premises-ui

integration_tests/tests/manage/placements/changes.cy.ts
@@ -17,6 +17,8 @@ import { DateFormats } from '../../../../server/utils/dateUtils'
 import ChangePlacementConfirmPage from '../../../pages/manage/placements/changes/confirm'
 import apiPaths from '../../../../server/paths/api'
 import { roomCharacteristicMap } from '../../../../server/utils/characteristicsUtils'
+import { Cas1SpaceBooking } from '../../../../server/@types/shared/models/Cas1SpaceBooking'
+import { Cas1UpdateSpaceBooking } from '../../../../server/@types/shared/models/Cas1UpdateSpaceBooking'
 
 context('Change Placement', () => {
   const expectedArrivalDate = DateFormats.dateObjToIsoDate(faker.date.soon())
@@ -24,18 +26,27 @@ context('Change Placement', () => {
 
   const person = fullPersonFactory.build()
   const premises = cas1PremisesFactory.build()
-  const placement = cas1SpaceBookingFactory.upcoming().build({
-    premises: { name: premises.name, id: premises.id },
-    expectedArrivalDate,
-    expectedDepartureDate,
-  })
-  const placementRequestDetail = placementRequestDetailFactory.build({
-    person,
-    status: 'matched',
-    booking: bookingSummaryFactory.fromSpaceBooking(placement).build(),
-  })
-  placement.requestForPlacementId = placementRequestDetail.id
-  const capacity = cas1PremiseCapacityFactory.build()
+
+  const setupMocks = (placement: Cas1SpaceBooking, startDate?: string, endDate?: string) => {
+    const placementRequestDetail = placementRequestDetailFactory.build({
+      person,
+      status: 'matched',
+      booking: bookingSummaryFactory.fromSpaceBooking(placement).build(),
+    })
+    placement.requestForPlacementId = placementRequestDetail.id
+    const capacity = cas1PremiseCapacityFactory.build()
+    cy.task('stubSinglePremises', premises)
+    cy.task('stubPremiseCapacity', {
+      premisesId: premises.id,
+      premiseCapacity: capacity,
+      startDate: startDate || placement.expectedArrivalDate,
+      endDate: endDate || placement.expectedDepartureDate,
+    })
+    cy.task('stubPlacementRequest', placementRequestDetail)
+    cy.task('stubSpaceBookingGetWithoutPremises', placement)
+    cy.task('stubSpaceBookingUpdate', { premisesId: premises.id, placementId: placement.id })
+    return { placement, placementRequestDetail }
+  }
 
   beforeEach(() => {
     cy.task('reset')
@@ -44,21 +55,15 @@ context('Change Placement', () => {
 
     // Given I am signed in
     signIn(['workflow_manager', 'future_manager'], ['cas1_space_booking_create'])
-
-    cy.task('stubSinglePremises', premises)
-    cy.task('stubPremiseCapacity', {
-      premisesId: premises.id,
-      premiseCapacity: capacity,
-      startDate: placement.expectedArrivalDate,
-      endDate: placement.expectedDepartureDate,
-      excludeSpaceBookingId: placement.id,
-    })
-    cy.task('stubPlacementRequest', placementRequestDetail)
-    cy.task('stubSpaceBookingGetWithoutPremises', placement)
-    cy.task('stubSpaceBookingUpdate', { premisesId: premises.id, placementId: placement.id })
   })
 
   it('allows me to change the dates and criteria of a space booking', () => {
+    const placement = cas1SpaceBookingFactory.upcoming().build({
+      premises: { name: premises.name, id: premises.id },
+      expectedArrivalDate,
+      expectedDepartureDate,
+    })
+    const { placementRequestDetail } = setupMocks(placement)
     // When I visit a placement request
     const placementRequestPage = ShowPage.visit(placementRequestDetail)
 
@@ -72,7 +77,7 @@ context('Change Placement', () => {
     changePlacementPage.shouldShowPlacementOverview()
 
     // And I should see the filter form with default populated values from the placement
-    const selectedCriteria = filterRoomLevelCriteria(placement.requirements.essentialCharacteristics)
+    const selectedCriteria = filterRoomLevelCriteria(placement.characteristics)
     const selectedCriteriaLabels = selectedCriteria.map(criterion => roomCharacteristicMap[criterion])
     changePlacementPage.shouldShowFilters(placement.expectedArrivalDate, 'Up to 12 weeks', selectedCriteriaLabels)
 
@@ -82,11 +87,11 @@ context('Change Placement', () => {
     // And I can see the current placement dates in the hints
     changePlacementPage.shouldShowDateFieldHint(
       'arrivalDate',
-      `Expected arrival date: ${DateFormats.isoDateToUIDate(placement.expectedArrivalDate, { format: 'dateFieldHint' })}`,
+      `Current arrival date: ${DateFormats.isoDateToUIDate(placement.expectedArrivalDate, { format: 'dateFieldHint' })}`,
     )
     changePlacementPage.shouldShowDateFieldHint(
       'departureDate',
-      `Expected departure date: ${DateFormats.isoDateToUIDate(placement.expectedDepartureDate, { format: 'dateFieldHint' })}`,
+      `Current departure date: ${DateFormats.isoDateToUIDate(placement.expectedDepartureDate, { format: 'dateFieldHint' })}`,
     )
 
     // When I submit the filters with an invalid date
@@ -152,6 +157,77 @@ context('Change Placement', () => {
         departureDate,
         characteristics: criteria,
       })
+    })
+  })
+
+  it('allows me to extend the end date of placement after arrival', () => {
+    const actualArrivalDateOnly = DateFormats.dateObjToIsoDate(addDays(expectedArrivalDate, 20))
+    const arrivedPlacement = cas1SpaceBookingFactory.upcoming().build({
+      premises: { name: premises.name, id: premises.id },
+      expectedArrivalDate,
+      expectedDepartureDate: DateFormats.dateObjToIsoDate(addDays(expectedArrivalDate, 45)),
+      actualArrivalDateOnly,
+    })
+    const newDepartureDate = DateFormats.dateObjToIsoDate(addDays(arrivedPlacement.expectedDepartureDate, 5))
+
+    const { placementRequestDetail } = setupMocks(
+      arrivedPlacement,
+      actualArrivalDateOnly,
+      DateFormats.dateObjToIsoDate(addDays(actualArrivalDateOnly, 56)),
+    )
+    // When I visit a placement request
+
+    const placementRequestPage = ShowPage.visit(placementRequestDetail)
+
+    // When I click on the amend booking button
+    placementRequestPage.clickAction('Change placement')
+
+    // Then I should see the Change Placement page
+    const page = Page.verifyOnPage(ChangePlacementPage, arrivedPlacement)
+
+    // And I should see an overview of the placement
+    page.shouldShowPlacementOverview()
+
+    // And I can see the calendar for the rounded-up period in the duration selector, from the actual arrival date
+    page.shouldShowCalendarHeading(actualArrivalDateOnly, 56)
+
+    // And I can see the current placement dates in the departure date hint
+    page.shouldShowDateFieldHint(
+      'departureDate',
+      `Current departure date: ${DateFormats.isoDateToUIDate(arrivedPlacement.expectedDepartureDate, { format: 'dateFieldHint' })}`,
+    )
+    // And I should only be able to change the departure date
+    cy.get('h2').contains('Change placement')
+
+    // When I set a new departure date and submit
+    page.clearAndCompleteDateInputs('departureDate', newDepartureDate)
+    page.clickContinue()
+
+    // Then I should see the confirmation page with the correct dates
+    const confirmPage = Page.verifyOnPage(ChangePlacementConfirmPage, premises, {
+      arrivalDate: arrivedPlacement.expectedArrivalDate,
+      departureDate: DateFormats.dateObjToIsoDate(addDays(arrivedPlacement.expectedDepartureDate, 5)),
+      criteria: arrivedPlacement.characteristics.filter(characteristic => roomCharacteristicMap[characteristic]),
+    })
+    confirmPage.shouldShowProposedChanges()
+
+    // When I submit the confirmation page
+    confirmPage.clickSubmit()
+
+    // Then I should see the placement request page again with a success banner
+    Page.verifyOnPage(ShowPage, placementRequestDetail)
+
+    placementRequestPage.shouldShowBanner('Booking changed successfully')
+
+    // And the booking changes should have been sent to the API
+    cy.task('verifyApiPatch', apiPaths.premises.placements.show).then(body => {
+      const { characteristics, arrivalDate, departureDate } = body as Cas1UpdateSpaceBooking
+
+      expect(arrivalDate).equal(undefined)
+      expect(departureDate).equal(newDepartureDate)
+      expect(characteristics.sort()).to.deep.equal(
+        arrivedPlacement.characteristics.filter(characteristic => roomCharacteristicMap[characteristic]).sort(),
+      )
     })
   })
 })

server/utils/placements/index.ts
@@ -138,13 +138,10 @@ export const placementOverviewSummary = (placement: Cas1SpaceBooking): SummaryLi
     summaryRow('Approved premises', placement.premises.name),
     summaryRow('Date of match', formatDate(placement.createdAt)),
     summaryRow('Expected arrival date', formatDate(placement.expectedArrivalDate)),
+    summaryRow('Actual arrival date', formatDate(placement.actualArrivalDateOnly)),
     summaryRow('Expected departure date', formatDate(placement.expectedDepartureDate)),
-    summaryListItem(
-      'Room criteria',
-      requirementsHtmlString(placement.requirements.essentialCharacteristics, roomCharacteristicMap),
-      'html',
-    ),
-  ],
+    summaryListItem('Room criteria', requirementsHtmlString(placement.characteristics, roomCharacteristicMap), 'html'),
+  ].filter(Boolean),
 })
 
 export const arrivalInformation = (placement: Cas1SpaceBooking): SummaryList => {
@@ -188,7 +185,7 @@ export const departureInformation = (placement: Cas1SpaceBooking): SummaryList =
 }
 
 export const requirementsInformation = (placement: Cas1SpaceBooking): SummaryList => {
-  const requirements = placement.requirements.essentialCharacteristics
+  const requirements = placement.characteristics
   const apType =
     apTypeCriteriaLabels[requirements.find(requirement => specialistApTypeCriteria.includes(requirement)) || 'normal']
   const apRequirements = filterApLevelCriteria(requirements)