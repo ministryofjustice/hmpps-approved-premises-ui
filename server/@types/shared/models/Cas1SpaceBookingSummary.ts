9ca414e2cf27b9fd90aa2be13ca511d3333a7541
davidatkinsuk <22135634+davidatkinsuk@users.noreply.github.com>
2025-02-21 10:56:33 +0000

    Updating hmpps-approved-premises-api models from OpenAPI specification

--- server/@types/shared/models/Cas1SpaceBookingSummary.ts
+++ server/@types/shared/models/Cas1SpaceBookingSummary.ts
@@ -4,6 +4,7 @@
 /* eslint-disable */
 import type { Cas1KeyWorkerAllocation } from './Cas1KeyWorkerAllocation';
 import type { Cas1SpaceBookingSummaryStatus } from './Cas1SpaceBookingSummaryStatus';
+import type { Cas1SpaceCharacteristic } from './Cas1SpaceCharacteristic';
 import type { PersonSummary } from './PersonSummary';
 export type Cas1SpaceBookingSummary = {
     id: string;
@@ -17,10 +18,31 @@ export type Cas1SpaceBookingSummary = {
      */
     canonicalDepartureDate: string;
     /**
+     * expected arrival date
+     */
+    expectedArrivalDate: string;
+    /**
+     * actual arrival date if known
+     */
+    actualArrivalDate?: string;
+    /**
+     * expected departure date
+     */
+    expectedDepartureDate: string;
+    /**
+     * actual departure date if known
+     */
+    actualDepartureDate?: string;
+    isNonArrival?: boolean;
+    /**
      * Risk rating tier level of corresponding application
      */
     tier?: string;
     keyWorkerAllocation?: Cas1KeyWorkerAllocation;
     status?: Cas1SpaceBookingSummaryStatus;
+    /**
+     * Room and premise characteristics
+     */
+    characteristics: Array<Cas1SpaceCharacteristic>;
 };
 
