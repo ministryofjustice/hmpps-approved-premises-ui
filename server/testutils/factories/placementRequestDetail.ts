c6828fc9af27468e0d30b6ad930c6c4af050365b
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2025-02-25 12:42:45 +0000

    Update placement request factories

--- server/testutils/factories/placementRequestDetail.ts
+++ server/testutils/factories/placementRequestDetail.ts
@@ -3,12 +3,20 @@ import { PlacementRequestDetail } from '../../@types/shared'
 
 import cancellationFactory from './cancellation'
 import { placementRequestFactory } from './placementRequest'
-import bookingSummaryFactory from './placementRequestBookingSummary'
 import applicationFactory from './application'
+import cas1SpaceBookingSummaryFactory from './cas1SpaceBookingSummary'
+import bookingSummaryFactory from './placementRequestBookingSummary'
 
-export default Factory.define<PlacementRequestDetail>(() => ({
-  ...placementRequestFactory.build(),
-  cancellations: cancellationFactory.buildList(2),
-  booking: bookingSummaryFactory.build(),
-  application: applicationFactory.build(),
-}))
+export default Factory.define<PlacementRequestDetail>(() => {
+  const spaceBooking = cas1SpaceBookingSummaryFactory.build()
+  const bookingSummary = bookingSummaryFactory.fromSpaceBooking(spaceBooking).build()
+
+  return {
+    ...placementRequestFactory.build(),
+    cancellations: cancellationFactory.buildList(2),
+    booking: bookingSummary,
+    legacyBooking: undefined,
+    spaceBookings: [spaceBooking],
+    application: applicationFactory.build(),
+  }
+})
