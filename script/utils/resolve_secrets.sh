bb47bfe5c2517860407c3713b9929c2058368588
davidatkinsuk <david.atkins1@digital.justice.gov.uk>
2025-04-04 16:15:44 +0100

    fixes

--- script/utils/resolve_secrets.sh
+++ script/utils/resolve_secrets.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e
 # shellcheck disable=SC3040
 set -o pipefail
@@ -11,16 +11,19 @@ set -o pipefail
 # $1 source template file to update
 # $2 where to write the resulting file
 # $3 kubernetes namespace
-# $4 kubernetes secret name
+# $4 kubernetes secret names (array)
 resolve_secrets() {
   echo "==> Resolve Secrets"
 
-  source=$1
-  target=$2
-  k8s_namespace=$3
-  k8s_secret_name=$4
+  source="$1"
+  shift
+  target="$1"
+  shift
+  k8s_namespace="$1"
+  shift
+  secretNames=("$@")
 
-  echo "Rendering template '$source' to '$target' using secrets '$k8s_secret_name' in namespace '$k8s_namespace'"
+  echo "Rendering template '$source' to '$target' in namespace '$k8s_namespace'"
 
   # shellcheck disable=SC3020
   if ! command -v jq &> /dev/null
@@ -29,11 +32,17 @@ resolve_secrets() {
       exit 1
   fi
 
-  secrets=$(kubectl get secrets "$k8s_secret_name" --namespace "$k8s_namespace" -o json | jq ".data | map_values(@base64d)")
-  # get value in format 'key=value' which can then be used with the 'export' command, setting them as env vars
-  for secret in $(echo "$secrets" | jq -r "to_entries | map(\"\(.key)=\(.value|tostring)\") | .[]" ); do
-    # shellcheck disable=SC2163
-    export "$secret"
+  for secretName in "${secretNames[@]}"; do
+
+    echo "Loading secrets for $secretName"
+
+    secrets=$(kubectl get secrets "$secretName" --namespace "$k8s_namespace" -o json | jq ".data | map_values(@base64d)")
+    # get value in format 'key=value' which can then be used with the 'export' command, setting them as env vars
+    for secret in $(echo "$secrets" | jq -r "to_entries | map(\"\(.key)=\(.value|tostring)\") | .[]" ); do
+      # shellcheck disable=SC2163
+      export "$secret"
+    done
+
   done
 
   rm -f "$target"
