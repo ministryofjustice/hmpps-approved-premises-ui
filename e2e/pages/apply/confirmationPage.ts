8c90f72b954c258cd3ff4af34f33205c6abb37fb
Fred Marecesche <fred.marecesche@justice.gov.uk>
2025-05-22 16:58:38 +0100

    Fix feedback survey link on success page

--- e2e/pages/apply/confirmationPage.ts
+++ e2e/pages/apply/confirmationPage.ts
@@ -5,4 +5,11 @@ export class ConfirmationPage extends BasePage {
   async shouldShowSuccessMessage() {
     await expect(this.page.locator('h1.govuk-panel__title')).toContainText('Application confirmation')
   }
+
+  async shouldShowLinkToSurvey() {
+    await expect(this.page.locator('a', { hasText: 'What did you think of this service?' })).toHaveAttribute(
+      'href',
+      'https://forms.office.com/e/jSiRQFF82r',
+    )
+  }
 }
--- e2e/tests/feedback-banner.spec.ts
+++ e2e/tests/feedback-banner.spec.ts
@@ -7,6 +7,11 @@ test('feedback banner', async ({ page, userWithoutRoles }) => {
 
   await page.goto('/')
 
+  await expect(page.getByRole('link', { name: 'email us' })).toHaveAttribute(
+    'href',
+    'mailto:APServiceSupport@digital.justice.gov.uk',
+  )
+
   await page.getByRole('link', { name: 'Give us your feedback' }).click()
 
   await expect(page).toHaveTitle('Satisfaction survey - Approved Premises (AP) also known as CAS1')
