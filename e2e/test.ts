13b9b20b6cb098b115c4678b48568693ab7e6f4b
Bob Meredith <robert.meredith@digital.justice.gov.uk>
2025-06-11 17:05:32 +0100

    Fix e2e tests

--- e2e/test.ts
+++ e2e/test.ts
@@ -53,6 +53,6 @@ export const test = base.extend<TestOptions>({
     },
     { option: true },
   ],
-  oasysSections: [['3. Accommodation', '13. Health'], { option: true }],
+  oasysSections: [['3. Accommodation'], { option: true }],
   emergencyApplicationUser: [process.env.CAS1_E2E_EMERGENCY_ASSESSOR_NAME_TO_ALLOCATE_TO, { option: true }],
 })
