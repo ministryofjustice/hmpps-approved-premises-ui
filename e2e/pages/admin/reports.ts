d9ed8ea7c3d902e88059e855a5ac701319c2b9d8
Fred Marecesche <fred.marecesche@justice.gov.uk>
2025-06-12 15:39:40 +0100

    Add type to avoid repetition

--- e2e/pages/admin/reports.ts
+++ e2e/pages/admin/reports.ts
@@ -1,6 +1,8 @@
 import { Page, expect } from '@playwright/test'
 import { BasePage } from '../basePage'
 
+type DateRangeArgs = { startDate: string; endDate: string }
+
 export class ReportsPage extends BasePage {
   static async initialize(page: Page, title: string) {
     await expect(page.locator('h1')).toContainText(title)
@@ -8,17 +10,17 @@ export class ReportsPage extends BasePage {
     return new ReportsPage(page)
   }
 
-  async downloadOutOfServiceBedsReports({ startDate, endDate }: { startDate: string; endDate: string }) {
+  async downloadOutOfServiceBedsReports({ startDate, endDate }: DateRangeArgs) {
     await this.checkRadio('Out of service beds')
     return this.downloadReports({ startDate, endDate })
   }
 
-  async downloadDailyMetricsReports({ startDate, endDate }: { startDate: string; endDate: string }) {
+  async downloadDailyMetricsReports({ startDate, endDate }: DateRangeArgs) {
     await this.checkRadio('Daily metrics')
     return this.downloadReports({ startDate, endDate })
   }
 
-  async downloadReports({ startDate, endDate }: { startDate: string; endDate: string }) {
+  async downloadReports({ startDate, endDate }: DateRangeArgs) {
     await this.page.getByRole('textbox', { name: 'Start date' }).fill(startDate)
     await this.page.getByRole('textbox', { name: 'End date' }).fill(endDate)
 
