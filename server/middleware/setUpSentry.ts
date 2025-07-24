50482ad1ff9c3ea1349cf61941ae436330bb648e
Fred Marecesche <fred.marecesche@justice.gov.uk>
2025-07-24 17:00:54 +0100

    Upgrade Sentry to latest
    Sentry recommend calling Sentry.init() as early as possible, so it has
    been moved to the entry file.

--- server/middleware/setUpSentry.ts
+++ server/middleware/setUpSentry.ts
@@ -1,10 +1,9 @@
 import * as Sentry from '@sentry/node'
-import * as Tracing from '@sentry/tracing'
 import express from 'express'
-import applicationVersion from '../applicationVersion'
 import config from '../config'
+import applicationVersion from '../applicationVersion'
 
-export function setUpSentryRequestHandler(app: express.Express): void {
+export function setUpSentryRequestHandler(): void {
   if (config.sentry.dsn) {
     // Prevent usernames which are PII from being sent to Sentry
     // https://docs.sentry.io/platforms/python/guides/logging/data-management/sensitive-data/#examples
@@ -15,19 +14,8 @@ export function setUpSentryRequestHandler(app: express.Express): void {
       dsn: config.sentry.dsn,
       environment: config.environment,
       release: applicationVersion.gitRef,
-      integrations: [
-        // enable HTTP calls tracing
-        new Sentry.Integrations.Http({ tracing: true }),
-        // enable Express.js middleware tracing
-        new Tracing.Integrations.Express({ app }),
-      ],
-      tracesSampler: samplingContext => {
-        const transactionName = samplingContext?.transactionContext?.name
-        if (
-          transactionName?.includes('ping') ||
-          transactionName?.includes('health') ||
-          transactionName?.includes('assets')
-        ) {
+      tracesSampler: ({ name }) => {
+        if (name.includes('ping') || name.includes('health') || name.includes('assets')) {
           return 0
         }
 
@@ -39,18 +27,11 @@ export function setUpSentryRequestHandler(app: express.Express): void {
         return 0.05
       },
     })
-    app.use(
-      Sentry.Handlers.requestHandler({
-        ip: false,
-        user: false,
-      }) as express.RequestHandler,
-    )
-    app.use(Sentry.Handlers.tracingHandler())
   }
 }
 
 export function setUpSentryErrorHandler(app: express.Express): void {
   if (config.sentry.dsn) {
-    app.use(Sentry.Handlers.errorHandler() as express.ErrorRequestHandler)
+    Sentry.setupExpressErrorHandler(app)
   }
 }
