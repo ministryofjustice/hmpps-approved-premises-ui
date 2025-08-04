81108fd4263e15bcd521f46f7b18e4c763f92a42
Fred Marecesche <fred.marecesche@justice.gov.uk>
2025-08-04 11:03:48 +0100

    Add error throwing routes for Sentry debugging

--- server/controllers/dashboardController.ts
+++ server/controllers/dashboardController.ts
@@ -6,6 +6,7 @@ export default class DashboardController {
     return (_req: Request, res: Response) => {
       const sections = sectionsForUser(res.locals.user)
       const hideNav = true
+
       res.render('dashboard/index', {
         pageHeading: 'Approved Premises',
         hideNav,
