9f60ceb05741dbf4873d11678d8a317f139638c2
Patrick Fleming <patrick.fleming1@digital.justice.gov.uk>
2025-09-11 10:24:05 +0100

    Call dashboard controller method on index route
    We will want to abstract some of the handler logic later to ensure consistent request handling, but that’s out of scope for this PR.
    
    We can also remove the tests on the index route that we inherited from the TS template.

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,15 +2,18 @@ import { Router } from 'express'
 
 import type { Services } from '../services'
 import { Page } from '../services/auditService'
+import { Controllers } from '../controllers'
 
-export default function routes({ auditService, exampleService }: Services): Router {
+export default function routes(controllers: Controllers, { auditService }: Services): Router {
   const router = Router()
+
+  const { dashboardController } = controllers
 
   router.get('/', async (req, res, next) => {
     await auditService.logPageView(Page.EXAMPLE_PAGE, { who: res.locals.user.username, correlationId: req.id })
 
-    const exampleData = await exampleService.getExampleData(res.locals.user.username)
-    return res.render('pages/index', { exampleData })
+    const handler = dashboardController.index()
+    await handler(req, res, next)
   })
 
   return router
