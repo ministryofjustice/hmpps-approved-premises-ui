c46300f4375ad5399e281300302008483eb7e2f1
Patrick Fleming <patrick.fleming1@digital.justice.gov.uk>
2025-09-11 10:19:21 +0100

    Pass controllers into app on setup

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,8 @@
 import createApp from './app'
 import { services } from './services'
+import { controllers } from './controllers'
 
-const app = createApp(services())
+const serviceList = services()
+const app = createApp(controllers(serviceList), serviceList)
 
 export default app
