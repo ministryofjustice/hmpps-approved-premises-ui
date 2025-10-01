212701ca7ce72ca1b8d0a813b1d58ef53b96209d
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-10-01 15:51:16 +0100

    Add appointment update route

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -4,11 +4,12 @@ import type { Services } from '../services'
 import { Page } from '../services/auditService'
 import { Controllers } from '../controllers'
 import sessionRoutes from './session'
+import appointmentRoutes from './appointment'
 
 export default function routes(controllers: Controllers, { auditService }: Services): Router {
   const router = Router()
 
-  const { dashboardController, sessionsController } = controllers
+  const { dashboardController, sessionsController, appointmentsController } = controllers
 
   router.get('/', async (req, res, next) => {
     await auditService.logPageView(Page.EXAMPLE_PAGE, { who: res.locals.user.username, correlationId: req.id })
@@ -17,6 +18,7 @@ export default function routes(controllers: Controllers, { auditService }: Servi
     await handler(req, res, next)
   })
 
+  appointmentRoutes(appointmentsController, router)
   sessionRoutes(sessionsController, router)
 
   return router
