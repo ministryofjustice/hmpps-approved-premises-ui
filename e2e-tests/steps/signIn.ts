2cd260d3e3348d21bc365e7a51b45252315536d0
Liz <50752284+lookupdaily@users.noreply.github.com>
2025-10-03 12:14:38 +0100

    Update  user login in e2e tests
    This is a workaround to prevent any password being logged in the test report. See https://github.com/microsoft/playwright/issues/35848

--- e2e-tests/steps/signIn.ts
+++ e2e-tests/steps/signIn.ts
@@ -5,7 +5,13 @@ export default async (page: Page, user: { username: string; password: string }):
   const homePage = new HomePage(page)
   await homePage.visit()
   await page.getByLabel('Username').fill(user.username)
-  await page.getByLabel('Password').fill(user.password)
+
+  // Don't enter password via user input to prevent it being logged in the report
+  await page.getByLabel('Password').evaluate((input: HTMLInputElement, password: string) => {
+    /* eslint no-param-reassign: "off" */
+    input.value = password
+  }, user.password)
+
   await page.getByRole('button', { name: 'Sign in' }).click()
   await homePage.expect.toBeOnThePage()
   return homePage
