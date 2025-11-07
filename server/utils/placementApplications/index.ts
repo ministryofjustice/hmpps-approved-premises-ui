c29046116f3355f863ffde9cf8175ddf9bfd7ea8
Bob Meredith <robert.meredith@digital.justice.gov.uk>
2025-11-07 15:28:39 +0000

    APS-2828 Default placementApplication releaseType

--- server/utils/placementApplications/index.ts
+++ server/utils/placementApplications/index.ts
@@ -3,6 +3,17 @@ import { YesOrNo } from '@approved-premises/ui'
 
 export * as tableUtils from './table'
 
+export const substituteReleaseType = (sentenceType: SentenceTypeOption, releaseType: ReleaseTypeOption) => {
+  if (sentenceType === 'nonStatutory') {
+    return 'not_applicable'
+  }
+
+  if (sentenceType === 'communityOrder' || sentenceType === 'bailPlacement') {
+    return 'in_community'
+  }
+  return releaseType
+}
+
 export const getSentenceType = (
   placementApplication: PlacementApplication,
 ): {
@@ -12,27 +23,32 @@ export const getSentenceType = (
   situation: SituationOption
 } => {
   const pageData = placementApplication.data?.['request-a-placement']
+  const { sentenceTypeCheck } = pageData?.['sentence-type-check'] || {}
+
+  if (sentenceTypeCheck === 'yes') {
+    const { releaseType }: { releaseType: ReleaseTypeOption } = pageData?.['release-type'] || {}
+    const { sentenceType }: { sentenceType: SentenceTypeOption } = pageData?.['sentence-type'] || {}
+    const { situation }: { situation: SituationOption } = pageData?.situation || {}
+    return {
+      sentenceTypeCheck,
+      releaseType: substituteReleaseType(sentenceType, releaseType),
+      sentenceType,
+      situation,
+    }
+  }
   const {
-    applicationReleaseType,
-    sentenceTypeCheck,
-    applicationSentenceType,
+    applicationReleaseType: releaseType,
+    applicationSentenceType: sentenceType,
     situation,
   }: {
-    sentenceTypeCheck: YesOrNo
     applicationReleaseType: ReleaseTypeOption
     applicationSentenceType: SentenceTypeOption
     situation: SituationOption
   } = pageData?.['sentence-type-check'] || {}
-  const { releaseType: updatedReleaseType }: { releaseType: ReleaseTypeOption } = pageData?.['release-type'] || {}
-  const { sentenceType: updatedSentenceType }: { sentenceType: SentenceTypeOption } = pageData?.['sentence-type'] || {}
-  const { situation: updatedSituation }: { situation: SituationOption } = pageData?.situation || {}
-
-  const updated = sentenceTypeCheck === 'yes'
-
   return {
     sentenceTypeCheck,
-    releaseType: updated ? updatedReleaseType : applicationReleaseType,
-    sentenceType: updated ? updatedSentenceType : applicationSentenceType,
-    situation: updated ? updatedSituation : situation,
+    releaseType,
+    sentenceType,
+    situation,
   }
 }
