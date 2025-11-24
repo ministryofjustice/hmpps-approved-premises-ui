4395cd2d5223474349182a6f50e49b7d1fbf13e8
Bob Meredith <robert.meredith@digital.justice.gov.uk>
2025-11-24 14:00:33 +0000

    integration tests

---
+++ integration_tests/tests/manage/placements/residentProfile.cy.ts
@@ -0,0 +1,47 @@
+import { FullPerson } from '@approved-premises/api'
+import { signIn } from '../../signIn'
+import { cas1PremisesBasicSummaryFactory, cas1SpaceBookingFactory } from '../../../../server/testutils/factories'
+import ResidentProfilePage from '../../../pages/manage/placements/residentProfile'
+import { AND, GIVEN, THEN, WHEN } from '../../../helpers'
+
+context('ResidentProfile', () => {
+  describe('show', () => {
+    const setup = (placementParameters = {}) => {
+      const premises = cas1PremisesBasicSummaryFactory.build()
+      const placement = cas1SpaceBookingFactory.upcoming().build({
+        ...placementParameters,
+        premises: { name: premises.name, id: premises.id },
+      })
+
+      cy.task('stubSpaceBookingGetWithoutPremises', placement)
+      return {
+        placement,
+      }
+    }
+
+    beforeEach(() => {
+      cy.task('reset')
+    })
+
+    it('should render the resident profile page', () => {
+      GIVEN(' that I am signed in as a user with access resident profile')
+      signIn(['applicant', 'change_request_dev'])
+      GIVEN('there is an existing placement')
+      const { placement } = setup()
+      WHEN('I visit the resident profile page')
+      const page = ResidentProfilePage.visit(placement)
+      THEN('I should see the person information in the header')
+      page.checkHeader()
+      AND('the Personal tab should be selected')
+      page.shouldHaveActiveTab('Personal')
+    })
+
+    it('should not allow access to the page if user lacks permission', () => {
+      const { placement } = setup()
+      GIVEN(' that I am signed in as a user with access resident profile')
+      signIn(['applicant'])
+      WHEN('I visit the resident profile page')
+      ResidentProfilePage.visitUnauthorised(placement)
+    })
+  })
+})
