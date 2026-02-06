749b16c6f66f23f39dc093cd807f9252bb703959
AvraamK <avraam.kokkinis@justice.gov.uk>
2026-02-06 11:15:16 +0000

    increase cookie maxAge

--- server/controllers/staticController.ts
+++ server/controllers/staticController.ts
@@ -8,8 +8,8 @@ export default class StaticController {
     return async (req: Request, res: Response) => {
       let backLink: string
       if (template === 'whatsNew') {
-        // Note to potentially to further discuss-improve: For the cookies added for now a maxAge (1 year, although it can be anything really) in order to make the cookie to persist or else it will become browser-session only.
-        res.cookie(TECHNICAL_BANNER_COOKIE_NAME, TECHNICAL_BANNER_VERSION, { maxAge: 1000 * 60 * 60 * 24 * 365 })
+        // Note to potentially to further discuss-improve: For the cookies added for now a maxAge in ms (10 years, although it can be anything really) in order to make the cookie to persist or else it will become browser-session only.
+        res.cookie(TECHNICAL_BANNER_COOKIE_NAME, TECHNICAL_BANNER_VERSION, { maxAge: 1000 * 60 * 60 * 24 * 365 * 10 })
         backLink = req.headers.referer ? new URL(req.headers.referer).pathname : '/'
       }
       return res.render(`static/${template}.njk`, { backLink })
