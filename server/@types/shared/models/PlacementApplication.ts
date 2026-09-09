00d2f61b0a482dce3050cb9961526a3e2820f54f
davidatkinsuk <22135634+davidatkinsuk@users.noreply.github.com>
2026-09-09 10:05:55 +0000

    Updating hmpps-approved-premises-api models from OpenAPI specification

--- server/@types/shared/models/PlacementApplication.ts
+++ server/@types/shared/models/PlacementApplication.ts
@@ -4,7 +4,6 @@
 /* eslint-disable */
 import type { Cas1RequestedPlacementPeriod } from './Cas1RequestedPlacementPeriod';
 import type { PlacementApplicationType } from './PlacementApplicationType';
-import type { PlacementDates } from './PlacementDates';
 import type { WithdrawPlacementRequestReason } from './WithdrawPlacementRequestReason';
 /**
  * The API model used when constructing, submitting and approving a PlacementApplication. Once approved this is represented by a RequestForPlacement type
@@ -21,22 +20,12 @@ export type PlacementApplication = {
     createdAt: string;
     createdByUserId: string;
     data?: any;
-    /**
-     * Use requestedPlacementPeriod
-     * @deprecated
-     */
-    dates?: PlacementDates;
     document?: any;
     /**
      * If type is 'Additional', provides the PlacementApplication ID. If type is 'Initial' this field provides a PlacementRequest ID.
      */
     id: string;
     isWithdrawn: boolean;
-    /**
-     * Use requestedPlacementPeriod
-     * @deprecated
-     */
-    placementDates: Array<PlacementDates>;
     requestedPlacementPeriod?: Cas1RequestedPlacementPeriod;
     submittedAt?: string;
     type: PlacementApplicationType;
